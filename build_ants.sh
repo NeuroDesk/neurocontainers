89b80a50639c4f0470ad1b715a4e1184561c65aa
Steffen <stebo85@web.de>
2018-08-03 11:32:56 +1000

    added ants

---
+++ build_ants.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+set -e
+
+imageName='ants_2p2p0'
+buildDate=`date +%Y%m%d`
+
+echo "building $imageName"
+
+
+#install neurodocker
+#pip3 install --no-cache-dir https://github.com/kaczmarj/neurodocker/tarball/master --user
+
+#upgrade neurodocker
+#pip install --no-cache-dir https://github.com/kaczmarj/neurodocker/tarball/master --upgrade
+#or
+#pip install --no-cache-dir https://github.com/stebo85/neurodocker/tarball/master --upgrade
+
+
+neurodocker generate docker \
+   --base=ubuntu:xenial \
+   --pkg-manager apt \
+   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+   --run="chmod +x /usr/bin/ll" \
+   --copy globalMountPointList.txt /globalMountPointList.txt \
+   --run="mkdir \`cat /globalMountPointList.txt\`" \
+   --ants version=2.2.0 \
+   --user=neuro \
+   --env DEPLOY_PATH=/opt/ants-2.2.0/ \
+   > Dockerfile.${imageName}
+
+
+docker build -t ${imageName}:$buildDate -f  Dockerfile.${imageName} .
+
+#test:
+docker run -it ${imageName}:$buildDate
+#exit 0
+
+
+
+docker tag ${imageName}:$buildDate caid/${imageName}:$buildDate
+#docker login
+docker push caid/${imageName}:$buildDate
+docker tag ${imageName}:$buildDate caid/${imageName}:latest
+docker push caid/${imageName}:latest
+
+echo "BootStrap:docker" > Singularity.${imageName}
+echo "From:caid/${imageName}" >> Singularity.${imageName}
+
+sudo singularity build ${imageName}_${buildDate}.simg Singularity.${imageName}
+
+source ../setupSwift.sh
+swift upload singularityImages ${imageName}_${buildDate}.simg
+
+git commit -am 'auto commit after build run'
+git push
