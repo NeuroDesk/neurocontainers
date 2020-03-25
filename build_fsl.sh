08668a8d66dab988c53e21628c2c1527f2701a76
Steffen <stebo85@web.de>
2020-03-25 08:23:05 +0000

    fsl not working yet

--- build_fsl.sh
+++ build_fsl.sh
@@ -1,26 +1,46 @@
 #!/usr/bin/env bash
+set -e
 
-imageName='fsl_5p0p11'
+imageName='fsl_6p0p3'
 buildDate=`date +%Y%m%d`
 
+echo "building $imageName"
+
+
 #install neurodocker
-#pip3 install --no-cache-dir https://github.com/kaczmarj/neurodocker/tarball/master --user
+#pip install --no-cache-dir https://github.com/kaczmarj/neurodocker/tarball/master --user
 
 #upgrade neurodocker
 #pip install --no-cache-dir https://github.com/kaczmarj/neurodocker/tarball/master --upgrade
 
+# install development version
+pip install --no-cache-dir https://github.com/stebo85/neurodocker/tarball/master --upgrade
+
 neurodocker generate singularity \
    --base debian:wheezy \
    --pkg-manager apt \
-   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
-   --run="chmod +x /usr/bin/ll" \
-   --run="mkdir /90days /30days /QRISdata /RDS /data /short /proc_temp /TMPDIR /nvme /local /gpfs1" \
-   --install libdbus-glib-1-2 libjpeg62 libgtk2.0-0  libpng12-0 \
-   --fsl version=5.0.11 \
-   -e FSLOUTPUTTYPE=NIFTI_GZ \
-   --user=neuro \
-   > Singularity.${imageName}
+   --fsl version=6.0.3 \
+  > Singularity.${imageName}
+
+#   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+#   --run="chmod +x /usr/bin/ll" \
+#   --copy globalMountPointList.txt /globalMountPointList.txt \
+#   --run="mkdir \`cat /globalMountPointList.txt\`" \
+#   --env FSLOUTPUTTYPE=NIFTI_GZ \
+#   --env DEPLOY_PATH=/opt/fsl-6.0.3/bin/ \
+#   --user=neuro \
+
+if [ -f ${imageName}_${buildDate}.simg ] ; then
+       rm ${imageName}_${buildDate}.simg
+fi
 
 sudo singularity build ${imageName}_${buildDate}.simg Singularity.${imageName}
 
-singularity shell --bind $PWD:/data ${imageName}_${buildDate}.simg
+# test:
+sudo singularity shell --bind $PWD:/data ${imageName}_${buildDate}.simg
+
+source ../setupSwift.sh
+swift upload singularityImages ${imageName}_${buildDate}.simg
+
+git commit -am 'auto commit after build run'
+git push
