3c3c9d16c977adbe381d4f7ef03fd6bf2c31d695
Steffen <steffen.bollmann@cai.uq.edu.au>
2020-04-27 10:01:22 +1000

    trying ar

--- minc/build_minc.sh
+++ minc/build_minc.sh
@@ -2,13 +2,13 @@
 set -e
 
 export toolName='minc'
-export toolVersion=1.9.15
+export toolVersion=1.9.17
 
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base centos:6 \
-   --pkg-manager yum \
+   --base debian:stretch \
+   --pkg-manager apt \
    --run="mkdir ${mountPointList}" \
    --${toolName} version=${toolVersion} \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
