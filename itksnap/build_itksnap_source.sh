7251b49aa3aad8061de6d1a156fe9305fc9dd09e
Dr Steffen Bollmann <uqsbollm@cai-arwana.cai.uq.edu.au>
2020-05-05 22:08:24 +1000

    itksnap source testing

--- itksnap/build_itksnap_source.sh
+++ itksnap/build_itksnap_source.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
-export toolName='freesurfer'
-export toolVersion=7.0.0
+export toolName='itksnap'
+export toolVersion='3.8.0'
 
 source ../main_setup.sh
 
-export localSingularityBuild='false'
-export localSingularityBuildWritable='true'
+# export localSingularityBuild='false'
+# export localSingularityBuildWritable='true'
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base centos:6 \
-   --pkg-manager yum \
+   --base ubuntu:16.04 \
+   --pkg-manager apt \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
-   --${toolName} version=${toolVersion} \
-   --copy ../../license.txt /opt/${toolName}-${toolVersion}/ \
+   --${toolName} version=master method=source \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
+   --entrypoint /opt/${toolName}-${toolVersion}/bin/itksnap \
    --user=neuro \
   > recipe.${imageName}
 
