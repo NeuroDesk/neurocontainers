0bb26bbbca1529e884b3e72b1d3853de3b6d8158
Steffen <steffen.bollmann@cai.uq.edu.au>
2020-05-05 18:02:34 +1000

    added freesurfer, dke and itksnap

---
+++ itksnap/build_itksnap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+export toolName='itksnap'
+export toolVersion='3.8.0'
+
+source ../main_setup.sh
+
+neurodocker generate ${neurodocker_buildMode} \
+   --base debian:stretch \
+   --pkg-manager apt \
+   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+   --run="chmod +x /usr/bin/ll" \
+   --run="mkdir ${mountPointList}" \
+   --${toolName} version=${toolVersion} \
+   --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
+   --user=neuro \
+  > recipe.${imageName}
+
+./../main_build.sh
