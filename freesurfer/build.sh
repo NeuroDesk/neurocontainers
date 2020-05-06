3f9e3e0d8e2597c817568d7b8fcc56a62e335ca4
Steffen <steffen.bollmann@cai.uq.edu.au>
2020-05-06 21:25:12 +1000

    trying centos 6 for 7.0.0

--- freesurfer/build.sh
+++ freesurfer/build.sh
@@ -6,12 +6,12 @@ export toolVersion=7.0.0
 
 source ../main_setup.sh
 
-# export localSingularityBuild='false'
-# export localSingularityBuildWritable='true'
+export localSingularityBuild='false'
+export localSingularityBuildWritable='true'
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base ubuntu:18.04 \
-   --pkg-manager apt \
+   --base centos:6 \
+   --pkg-manager yum \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
