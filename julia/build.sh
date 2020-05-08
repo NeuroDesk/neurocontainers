13d046ddece6cf109af2aa97c432bff1c4afb0b1
Steffen <steffen.bollmann@cai.uq.edu.au>
2020-05-08 11:30:43 +1000

    added cleanup script for swift storage

--- julia/build.sh
+++ julia/build.sh
@@ -15,7 +15,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="mkdir ${mountPointList}" \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
-   --install libzstd1 $toolName \
+   --install zlib1g-dev libzstd1 $toolName \
    --env DEPLOY_PATH=/usr/bin/julia \
    --entrypoint /usr/bin/$toolName \
    --user=neuro \
