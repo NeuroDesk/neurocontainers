e305bba0f4e1c2d88e8bf344611ed2d891c968e9
thomshaw92 <thomasbasilshaw@gmail.com>
2020-05-28 15:14:17 +1000

    Change build context for ASHS

--- LASHiS/build.sh
+++ LASHiS/build.sh
@@ -16,7 +16,7 @@ neurodocker generate ${neurodocker_buildMode} \
 	--run="chmod +x /usr/bin/ll" \
 	--run="mkdir ${mountPointList}" \
 	--install libxt6 libxext6 libxtst6 libgl1-mesa-glx libc6 libice6 libsm6 libx11-6 \
-	--copy ../../ashs-fastashs_beta /ashs-fastashs_beta \
+	--copy ./ashs-fastashs_beta /ashs-fastashs_beta \
 	--env ASHS_ROOT="/ashs-fastashs_beta" \
 	--ants version=2.3.0 \
 	--copy antsJointLabelFusion2.sh /opt/ants-2.3.0/antsJointLabelFusion2.sh \
