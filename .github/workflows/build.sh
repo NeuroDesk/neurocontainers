20ed4e88a212e6249a11f5d33da4a35b94c20759
Aswin Narayanan <aswin@excelsol.com>
2020-07-01 02:11:03 +1000

    Add push to dockerhub/vnmd

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -20,9 +20,11 @@ git push github HEAD:${GITHUB_REF}
 # Loop through Local Dockerfiles
 # Build and Push Dockerfile images
 for dockerfile in ./*.Dockerfile; do
-  IMAGEID=docker.pkg.github.com/$GITHUB_REPOSITORY/$(basename $dockerfile .Dockerfile)
+  IMAGENAME=$(basename $dockerfile .Dockerfile)
+  IMAGEID=docker.pkg.github.com/$GITHUB_REPOSITORY/$IMAGENAME
   IMAGEID=$(echo $IMAGEID | tr '[A-Z]' '[a-z]')
   docker pull $IMAGEID:latest || true
   docker build . --file $dockerfile --tag $IMAGEID:latest --cache-from $IMAGEID:latest
   docker push $IMAGEID:latest
+  docker push vnmd/$IMAGENAME:latest
 done
