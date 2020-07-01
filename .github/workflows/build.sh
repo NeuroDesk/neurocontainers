419b021622815bdee4d692759b86e8ce93388e76
Dr Steffen Bollmann <uqsbollm@cai-arwana.cai.uq.edu.au>
2020-07-01 21:23:59 +1000

    no ff

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -42,7 +42,7 @@ for dockerfile in ./*.Dockerfile; do
   docker push vnmd/$IMAGENAME:$BUILDDATE
 
   # Write Container List
-  git pull github ${GITHUB_REF} --ff-only
+  git pull github ${GITHUB_REF}
   echo $IMAGENAME >> ../../container_list.txt
   git add ../../container_list.txt
   git commit -m "$GITHUB_SHA"
