d6e421e1accc70098bb3dc9b3c2f9e64539dc3b1
Steffen <stebo85@web.de>
2020-07-02 13:39:40 +1000

    try to clean disk space in builder before attempting large fsl build

--- recipes/fsl/build.sh
+++ recipes/fsl/build.sh
@@ -2,12 +2,12 @@
 set -e
 
 export toolName='fsl'
-export toolVersion='6.0.1'
+export toolVersion='6.0.3'
 
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base debian:stretch \
+   --base ubuntu:16.04 \
    --pkg-manager apt \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
