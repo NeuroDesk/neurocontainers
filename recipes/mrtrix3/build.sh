70bbb3d7bf5fb52b17d2e964e64f50cab6e8fe9f
Steffen <stebo85@web.de>
2020-07-02 10:15:34 +1000

    removed mount point creation, because it is already in our base image

--- recipes/mrtrix3/build.sh
+++ recipes/mrtrix3/build.sh
@@ -9,9 +9,6 @@ source ../main_setup.sh
 neurodocker generate ${neurodocker_buildMode} \
    --base vnmd/ants_2.3.1:latest \
    --pkg-manager apt \
-   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
-   --run="chmod +x /usr/bin/ll" \
-   --run="mkdir ${mountPointList}" \
    --${toolName} version=${toolVersion} method="source" \
    --fsl version="6.0.3" install_path="/opt/fsl" \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
