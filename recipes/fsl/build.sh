cb5cf9d104d2ba43ce991248148ebd6122432d18
Steffen <stebo85@web.de>
2020-07-02 12:11:12 +1000

    build fsl 6.0.1 in /opt so that it would work for mrtrix as a base image

--- recipes/fsl/build.sh
+++ recipes/fsl/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 export toolName='fsl'
-export toolVersion='6.0.3'
+export toolVersion='6.0.1'
 
 source ../main_setup.sh
 
@@ -14,6 +14,6 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="mkdir ${mountPointList}" \
    --${toolName} version=${toolVersion} \
    --env FSLOUTPUTTYPE=NIFTI_GZ \
-   --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/:/opt/${toolName}-${toolVersion}/fslpython/envs/fslpython/bin/ \
+   --env DEPLOY_PATH=/opt/${toolName}/bin/:/opt/${toolName}/fslpython/envs/fslpython/bin/ \
    --user=neuro \
   > ${imageName}.Dockerfile
