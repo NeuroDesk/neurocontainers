a43df0cb0409efd56b22504dbc7310cda984cfc9
Steffen <steffen.bollmann@cai.uq.edu.au>
2020-07-08 08:38:58 +1000

    freesurfer back to centos 6 - it might have caused the freeview core dump

--- recipes/freesurfer/build.sh
+++ recipes/freesurfer/build.sh
@@ -7,7 +7,7 @@ export toolVersion=7.1.0
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base centos:7 \
+   --base centos:6 \
    --pkg-manager yum \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
