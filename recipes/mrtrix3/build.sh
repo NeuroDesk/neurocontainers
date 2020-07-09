1bd277e28fb86d6eb84cb1d39fba9f1892ea15a6
Steffen <stebo85@web.de>
2020-07-09 14:14:24 +1000

    mrtrix3 with dbus-x11

--- recipes/mrtrix3/build.sh
+++ recipes/mrtrix3/build.sh
@@ -7,10 +7,12 @@ export toolVersion='3.0.0'
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base vnmd/fsl_6.0.1:20200702 \
+   --base ubuntu:16.04 \
    --pkg-manager apt \
    --${toolName} version=${toolVersion} method="source" \
    --ants version="2.3.4" \
+   --fsl version="6.0.1" \
+   --install dbus-x11 \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
    --user=neuro \
   > ${imageName}.Dockerfile
