b4e3214d5243d046f684379e1914105605e715aa
Aswin Narayanan <aswin@excelsol.com>
2020-07-18 02:33:31 +1000

    Refactor build.sh Separate docker and singularity build

---
+++ .github/workflows/build-singularity.sh
@@ -0,0 +1,31 @@
+
+if curl --output /dev/null --silent --head --fail "https://swift.rc.nectar.org.au:8888/v1/AUTH_d6165cc7b52841659ce8644df1884d5e/singularityImages/${IMAGENAMENAME}_${BUILDDATE}.sif"; then
+echo "${IMAGENAME}_${BUILDDATE}.sif exists"
+else
+echo "check space:"
+df -h
+
+echo "cleanup:"
+docker rmi $(docker image ls -aq)
+
+echo "check space:"
+df -h
+
+echo "build singularity container"
+singularity pull docker://$DOCKERHUB_ORG/$IMAGENAME:$BUILDDATE
+
+pip install python-swiftclient python-keystoneclient
+#configure swift
+export OS_AUTH_URL=https://keystone.rc.nectar.org.au:5000/v3/
+export OS_AUTH_TYPE=v3applicationcredential
+export OS_PROJECT_NAME="CAI_Container_Builder"
+export OS_USER_DOMAIN_NAME="Default"
+export OS_REGION_NAME="Melbourne"
+
+echo "attempting upload to swift ... "
+
+if [ "$GITHUB_REF" == "refs/heads/master" ]; then
+    swift upload singularityImages ${IMAGENAME}_${BUILDDATE}.sif --segment-size 1073741824
+fi
+fi
+fi
