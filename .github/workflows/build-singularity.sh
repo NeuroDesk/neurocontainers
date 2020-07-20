69aed772900b4b94d7e07ad2e8cc04f95942404d
Aswin Narayanan <aswin@excelsol.com>
2020-07-20 22:52:56 +1000

    Update build-singularity.sh

--- .github/workflows/build-singularity.sh
+++ .github/workflows/build-singularity.sh
@@ -2,28 +2,20 @@
 if curl --output /dev/null --silent --head --fail "https://swift.rc.nectar.org.au:8888/v1/AUTH_d6165cc7b52841659ce8644df1884d5e/singularityImages/${IMAGENAMENAME}_${BUILDDATE}.sif"; then
     echo "${IMAGENAME}_${BUILDDATE}.sif exists"
 else
-#     echo "check space:"
-#     df -h
-
-#     echo "cleanup:"
-#     docker rmi $(docker image ls -aq)
-
-#     echo "check space:"
-#     df -h
     echo "[DEBUG] BUILDDATE: $BUILDDATE"
     echo "[DEBUG] DOCKERHUB_ORG: $DOCKERHUB_ORG"
     REGISTRY=$(echo docker.pkg.github.com/$GITHUB_REPOSITORY | tr '[A-Z]' '[a-z]')
     echo "[DEBUG] REGISTRY: $REGISTRY"
     IMAGENAME=$1
     echo "[DEBUG] IMAGENAME: $IMAGENAME"
-    IMAGEID="vnmd/$IMAGENAME"
+    IMAGEID="$DOCKERHUB_ORG/$IMAGENAME"
     echo "[DEBUG] IMAGEID: $IMAGEID"
 
     echo "[DEBUG] Pulling latest build of singularity ..."
     docker pull $REGISTRY/singularity
     echo "[DEBUG] Build singularity container ..."
-    echo "[DEBUG] docker run -v /github/home:/home $REGISTRY/singularity build /home/${IMAGENAME}_${BUILDDATE}.sif docker://vnmd/$IMAGENAME"
-    docker run -v /github/home:/home $REGISTRY/singularity build "/home/${IMAGENAME}_${BUILDDATE}.sif" docker://vnmd/$IMAGENAME
+    echo "[DEBUG] docker run -v $HOME:/home $REGISTRY/singularity build /home/${IMAGENAME}_${BUILDDATE}.sif docker://$DOCKERHUB_ORG/$IMAGENAME"
+    docker run -v $HOME:/home $REGISTRY/singularity build "/home/${IMAGENAME}_${BUILDDATE}.sif" docker://$DOCKERHUB_ORG/$IMAGENAME
 
     echo "[DEBUG] Configure for SWIFT storage"
     pip install python-swiftclient python-keystoneclient
@@ -35,6 +27,6 @@ else
 
     echo "[DEBUG] Attempting upload to swift ..."
     if [ "$GITHUB_REF" == "refs/heads/master" ]; then
-        swift upload singularityImages /github/home/${IMAGENAME}_${BUILDDATE}.sif --segment-size 1073741824
+        swift upload singularityImages ${IMAGENAME}_${BUILDDATE}.sif --segment-size 1073741824
     fi
 fi
