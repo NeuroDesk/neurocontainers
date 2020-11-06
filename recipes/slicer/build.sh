bbfb53873f99910d5eed638d24bf9f0bb00c3328
Steffen Bollmann <stebo85@users.noreply.github.com>
2020-11-06 14:25:53 +1000

    added slicer

---
+++ recipes/slicer/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+# https://slicer.kitware.com/midas3/folder/274
+# export downloadLink='https://slicer.kitware.com/midas3/download/item/549121/Slicer-4.11.20200930-linux-amd64.tar.gz'
+export downloadLink='https://slicer.kitware.com/midas3/download?bitstream=1341035'
+export toolName='slicer'
+export toolVersion='4.11.20200930'
+
+if [ "$1" != "" ]; then
+    echo "Entering Debug mode"
+    export debug="true"
+fi
+
+source ../main_setup.sh
+
+neurodocker generate ${neurodocker_buildMode} \
+   --base ubuntu:20.04 \
+   --pkg-manager apt \
+   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+   --run="chmod +x /usr/bin/ll" \
+   --run="mkdir ${mountPointList}" \
+   --run="curl -fsSL --retry 5 ${downloadLink} | tar -xz -C /opt/ " \
+   --install libpulse-dev libnss3 libglu1-mesa \
+   --env DEPLOY_PATH=/opt/Slicer-${toolVersion}-linux-amd64/bin \
+   --env DEPLOY_BINS=Slicer \
+   --env PATH=/usr/bin:/opt/Slicer-${toolVersion}-linux-amd64/bin:/opt/Slicer-${toolVersion}-linux-amd64 \
+   --copy README.md /README.md \
+  > ${toolName}_${toolVersion}.Dockerfile
+
+if [ "$debug" = "true" ]; then
+   ./../main_build.sh
+fi
