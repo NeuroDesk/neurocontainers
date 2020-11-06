0081602e18cd613af9ba70f2aef8a497547d1d97
Steffen Bollmann <stebo85@users.noreply.github.com>
2020-11-06 16:21:26 +1000

    added mrtrix3tissue

---
+++ recipes/mrtrix3tissue/README.md
@@ -0,0 +1,6 @@
+MRtrix3Tissue is a fork of the MRtrix3 project. It adds capabilities for 3-Tissue CSD modelling and analysis to a complete version of MRtrix3. More information can be found on the official website at https://3Tissue.github.io.
+
+
+
+More documentation can be found here: https://3tissue.github.io/doc/general.html
+
---
+++ recipes/mrtrix3tissue/build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+export toolName='mrtrix3tissue'
+export toolVersion='5.2.8'
+
+if [ "$1" != "" ]; then
+    echo "Entering Debug mode"
+    export debug="true"
+fi
+
+source ../main_setup.sh
+
+if [ "$debug" = "true" ]; then
+   pip install --no-cache-dir https://github.com/NeuroDesk/neurodocker/tarball/add_MRtrix3tissue --upgrade
+fi
+
+neurodocker generate ${neurodocker_buildMode} \
+   --base docker.pkg.github.com/neurodesk/caid/fsl_6.0.3:20200905 \
+   --pkg-manager apt \
+   --${toolName} version=master method="source" \
+   --ants version="2.3.4" \
+   --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
+   --copy README.md /README.md \
+  > ${imageName}.Dockerfile
+
+if [ "$debug" = "true" ]; then
+   ./../main_build.sh
+fi
