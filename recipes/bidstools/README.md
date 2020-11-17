8b0cca63e8cc0664b275ceffcd6c9b22c44ee6ab
Steffen Bollmann <stebo85@users.noreply.github.com>
2020-11-18 09:57:04 +1000

    added bidstools

---
+++ recipes/bidstools/README.md
@@ -0,0 +1,27 @@
+
+
++++++++++++++++++++++ bidstools +++++++++++++++++++++++++
+Contains a collection of tools needed for DICOM to BIDS conversion
+
+Example:
+```
+dcm2niix
+bidsmapper
+bidscoiner
+bidseditor
+bidsparticipants
+bidstrainer
+deface
+dicomsort
+pydeface
+rawmapper
+```
+
+More documentation can be found here:
+https://bidscoin.readthedocs.io/en/latest/installation.html
+https://github.com/rordenlab/dcm2niix
+
+
+To run container outside of this environment: ml bidstools/1.0.0
++++++++++++++++++++++ bidstools +++++++++++++++++++++++++
+
---
+++ recipes/bidstools/build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+# this template file builds tools required for dicom conversion to bids
+export toolName='bidstools'
+export toolVersion='1.0.0'
+
+if [ "$1" != "" ]; then
+    echo "Entering Debug mode"
+    export debug="true"
+fi
+
+source ../main_setup.sh
+
+neurodocker generate ${neurodocker_buildMode} \
+   --base ubuntu:20.04 \
+   --pkg-manager apt \
+   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+   --run="chmod +x /usr/bin/ll" \
+   --run="mkdir ${mountPointList}" \
+   --miniconda use_env=base \
+              conda_install='python=3.6 traits' \
+              pip_install='bidscoin' \
+   --dcm2niix method=source version=latest \
+   --env DEPLOY_BINS=dcm2niix:bidsmapper:bidscoiner:bidseditor:bidsparticipants:bidstrainer:deface:dicomsort:pydeface:rawmapper  \
+   --copy README.md /README.md \
+  > ${toolName}_${toolVersion}.Dockerfile
+
+if [ "$debug" = "true" ]; then
+   ./../main_build.sh
+fi
