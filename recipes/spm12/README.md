95111f44d7001d988258bb5ae69516ff6b31a621
Steffen Bollmann <stebo85@users.noreply.github.com>
2020-11-18 07:52:11 +1000

    added spm12 standalone

---
+++ recipes/spm12/README.md
@@ -0,0 +1,15 @@
+
+
++++++++++++++++++++++ spm12 +++++++++++++++++++++++++
+SPM12 standalone with Matlab Compiler Runtime
+
+Example:
+```
+spm12_glnxa64
+```
+
+More documentation can be found here: https://www.fil.ion.ucl.ac.uk/spm/doc/
+
+To run container outside of this environment: ml spm12/r7219
++++++++++++++++++++++ spm12 +++++++++++++++++++++++++
+
---
+++ recipes/spm12/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+# this template file builds spm12
+export toolName='spm12'
+export toolVersion='r7219'
+
+if [ "$1" != "" ]; then
+    echo "Entering Debug mode"
+    export debug="true"
+fi
+
+source ../main_setup.sh
+
+neurodocker generate ${neurodocker_buildMode} \
+   --base ubuntu:16.04 \
+   --pkg-manager apt \
+   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+   --run="chmod +x /usr/bin/ll" \
+   --run="mkdir ${mountPointList}" \
+   --${toolName} version=${toolVersion} \
+   --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/ \
+   --copy README.md /README.md \
+  > ${toolName}_${toolVersion}.Dockerfile
+
+if [ "$debug" = "true" ]; then
+   ./../main_build.sh
+fi
