3f3e87f5fd276ddaaeb76380e5cb5588ad999cab
Steffen Bollmann <stebo85@users.noreply.github.com>
2020-11-19 18:25:19 +1000

    added datalad

---
+++ recipes/datalad/README.md
@@ -0,0 +1,15 @@
+
+
++++++++++++++++++++++ datalad +++++++++++++++++++++++++
+DataLad is a data management and distribution platform providing access to a wide range of data resources. Git for data using git-annex as backend. 
+
+Example:
+```
+datalad
+```
+
+More documentation can be found here: http://handbook.datalad.org/en/latest/
+
+To run container outside of this environment: ml datalad/0.13.3
++++++++++++++++++++++ datalad +++++++++++++++++++++++++
+
---
+++ recipes/datalad/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+# this template file builds itksnap and is then used as a docker base image for layer caching
+export toolName='datalad'
+export toolVersion='0.13.3'
+
+if [ "$1" != "" ]; then
+    echo "Entering Debug mode"
+    export debug="true"
+fi
+
+source ../main_setup.sh
+
+neurodocker generate ${neurodocker_buildMode} \
+   --base neurodebian:sid-non-free \
+   --pkg-manager apt \
+   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+   --run="chmod +x /usr/bin/ll" \
+   --run="mkdir ${mountPointList}" \
+   --install datalad datalad-container \
+   --env DEPLOY_BINS=datalad \
+   --copy README.md /README.md \
+  > ${toolName}_${toolVersion}.Dockerfile
+
+if [ "$debug" = "true" ]; then
+   ./../main_build.sh
+fi
