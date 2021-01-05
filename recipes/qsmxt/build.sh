81bbf582936336f9c3df1fa1db3061e183effde7
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-01-05 15:52:39 +1000

    try building qsmxt online

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -29,12 +29,21 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="unzip TGVQSM-plus.zip" \
    --workdir="/TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3" \
    --run="/miniconda2/bin/python setup.py install" \
-   --workdir="/opt/${toolName}-${toolVersion}" \
+   --workdir="/opt/tgvqsm-1.0.0" \
    --run="cp /miniconda2/bin/tgv_qsm ." \
    --workdir /opt \
-   --install git python3-tk python3-numpy python3-setuptools python3-pip python3-dev \
+   --install git python3-tk python3-numpy python3-setuptools python3-pip python3-dev julia zlib1g-dev libzstd1 \
    --run="git clone https://github.com/QSMxT/QSMxT" \
-   --run="pip3 install nipype[all]" \
+   --run="pip3 install nipype[all] bidscoin" \
+   --dcm2niix method=source version=latest \
+   --install apt_opts="--quiet" wget zip libgtk2.0-0 libgl1 libglib2.0 libglu1-mesa libsm6 libxrender1 libxt6 libxcomposite1 libfreetype6 libasound2 libfontconfig1 libxkbcommon0 libxcursor1 libxi6 libxrandr2 libxtst6 qt5-default libqt5svg5-dev wget libqt5opengl5-dev libqt5opengl5 libqt5gui5 libqt5core5a \
+   --workdir /opt/bru2 \
+   --run="wget https://github.com/neurolabusc/Bru2Nii/releases/download/v1.0.20180303/Bru2_Linux.zip" \
+   --run="unzip Bru2_Linux.zip" \
+   --fsl version=6.0.4 \
+   --minc version=1.9.17 \
+   --env PATH='$PATH':/opt/bru2 \
+   --env DEPLOY_BINS=dcm2niix:bidsmapper:bidscoiner:bidseditor:bidsparticipants:bidstrainer:deface:dicomsort:pydeface:rawmapper:Bru2:Bru2Nii  \
    --copy README.md /README.md \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/ \
   > ${imageName}.Dockerfile
