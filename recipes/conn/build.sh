ff1eec2e576ede549bc4a2400c12827be309c585
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-01-07 20:48:34 +1000

    try with libc6-dev

--- recipes/conn/build.sh
+++ recipes/conn/build.sh
@@ -18,9 +18,10 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
+   --install apt_opts="--quiet" libc6-dev \
    --run="curl -fsSL --retry 5 -o /tmp/conn18b_glnxa64.zip https://www.nitrc.org/frs/download.php/11120/conn18b_glnxa64.zip" \
    --run="unzip -q /tmp/conn18b_glnxa64.zip -d /opt/conn-18b/" \
-   --matlabmcr version=2019a method=binaries \
+   --matlabmcr version=2018a method=binaries \
    --env DEPLOY_BINS=conn \
    --env PATH=/opt/conn-18b/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
    --copy README.md /README.md \
