bea289533dca3404a2122fa113a7a4a7628a66a9
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-01-11 17:28:30 +1000

    disable caching for qsmxt

--- .github/workflows/build-docker-large.sh
+++ .github/workflows/build-docker-large.sh
@@ -10,23 +10,8 @@ echo "[DEBUG] IMAGENAME: $IMAGENAME"
 echo "[DEBUG] REGISTRY: $REGISTRY"
 echo "[DEBUG] IMAGEID: $IMAGEID"
 
-echo "[DEBUG] Pulling $IMAGEID"
-{
-  docker pull $IMAGEID \
-    && ROOTFS_CACHE=$(docker inspect --format='{{.RootFS}}' $IMAGEID)
-} || echo "$IMAGEID not found. Resuming build..."
-
 echo "[DEBUG] Docker build ..."
-docker build . --file ${IMAGENAME}.Dockerfile --tag $IMAGEID:$SHORT_SHA --cache-from $IMAGEID --label "GITHUB_REPOSITORY=$GITHUB_REPOSITORY" --label "GITHUB_SHA=$GITHUB_SHA"
-
-echo "[DEBUG] # Get image RootFS to check for changes ..."
-ROOTFS_NEW=$(docker inspect --format='{{.RootFS}}' $IMAGEID:$SHORT_SHA)
-
-# Tag and Push if new image RootFS differs from cached image
-if [ "$ROOTFS_NEW" = "$ROOTFS_CACHE" ]; then
-    echo "[DEBUG] Skipping push to registry. No changes found"
-else
-    echo "[DEBUG] Pushing to registry. Changes found"
+docker build . --file ${IMAGENAME}.Dockerfile --tag $IMAGEID:$SHORT_SHA --label "GITHUB_REPOSITORY=$GITHUB_REPOSITORY" --label "GITHUB_SHA=$GITHUB_SHA"
 
 if [ "$GITHUB_REF" == "refs/heads/master" ]; then
     # Push to GH Packages
@@ -42,5 +27,4 @@ if [ "$GITHUB_REF" == "refs/heads/master" ]; then
       docker push $DOCKERHUB_ORG/${IMAGENAME}:${BUILDDATE}
       docker push $DOCKERHUB_ORG/$IMAGENAME:latest
     fi
-  fi
 fi
