209294499ebf347c760727adf79f4cc37fc7b9c3
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-01-12 07:58:50 +1000

    build based on dev base image

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -12,31 +12,12 @@ fi
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base docker.pkg.github.com/neurodesk/caid/freesurfer_7.1.1:20201124 \
+   --base docker.pkg.github.com/neurodesk/caid/devbase_1.0.0:20210111 \
    --pkg-manager apt \
-   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
-   --run="chmod +x /usr/bin/ll" \
    --run="mkdir -p ${mountPointList}" \
-   --install apt_opts="--quiet" wget unzip gcc \
-   --run="wget https://repo.anaconda.com/miniconda/Miniconda2-4.6.14-Linux-x86_64.sh" \
-   --env PATH=/miniconda2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
-   --run="bash Miniconda2-4.6.14-Linux-x86_64.sh -b -p /miniconda2/" \
-   --run="/miniconda2/bin/conda install -c anaconda cython==0.25.2" \
-   --run="/miniconda2/bin/conda install numpy" \
-   --run="/miniconda2/bin/conda install pyparsing" \
-   --run="/miniconda2/bin/pip install scipy==0.17.1 nibabel==2.1.0" \
-   --run="wget http://www.neuroimaging.at/media/qsm/TGVQSM-plus.zip" \
-   --run="unzip TGVQSM-plus.zip" \
-   --workdir="/TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3" \
-   --run="/miniconda2/bin/python setup.py install" \
-   --workdir="/opt/tgvqsm-1.0.0" \
-   --run="cp /miniconda2/bin/tgv_qsm ." \
-   --fsl version=6.0.1 \
-   --minc version=1.9.17 \
-   --workdir /opt \
-   --install git python3-tk python3-numpy python3-setuptools python3-pip python3-dev julia zlib1g-dev libzstd1 graphviz \
-   --run="git clone https://github.com/QSMxT/QSMxT" \
    --run="pip3 install nipype[all] bidscoin" \
+   --workdir /opt \
+   --run="git clone https://github.com/QSMxT/QSMxT" \
    --dcm2niix method=source version=latest \
    --install apt_opts="--quiet" wget zip libgtk2.0-0 libgl1 libglib2.0 libglu1-mesa libsm6 libxrender1 libxt6 libxcomposite1 libfreetype6 libasound2 libfontconfig1 libxkbcommon0 libxcursor1 libxi6 libxrandr2 libxtst6 qt5-default libqt5svg5-dev wget libqt5opengl5-dev libqt5opengl5 libqt5gui5 libqt5core5a \
    --workdir /opt/bru2 \
