b1cae954d5fa3493bb2956e958d8a78235848ab8
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-01-22 13:03:28 +1000

    build based on latest base version

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -14,13 +14,13 @@ source ../main_setup.sh
 # ubuntu:18.04 
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base docker.pkg.github.com/neurodesk/caid/devbase_1.0.0:20210119 \
+   --base docker.pkg.github.com/neurodesk/caid/qsmxtbase_1.0.0:20210122 \
    --pkg-manager apt \
    --run="mkdir -p ${mountPointList}" \
    --workdir /opt \
    --run="git clone https://github.com/QSMxT/QSMxT" \
    --env PATH='$PATH':/opt/bru2 \
-   --env DEPLOY_BINS=dcm2niix:bidsmapper:bidscoiner:bidseditor:bidsparticipants:bidstrainer:deface:dicomsort:pydeface:rawmapper:Bru2:Bru2Nii:tgv_qsm:fslmaths:fslstats:mnc2nii:niii2mnc:tgv_qsm:recon-all:bidsmapper:bidscoiner:bet:julia:mri_convert:bestlinreg:mincresample:volcentre:norm:volpad:voliso:math:pik:blur:gennlxfm:xfmconcat:bestlinreg:nlpfit:xfmavg:xfminvert:resample:bigaverage:reshape:volsymm  \
+   --env DEPLOY_BINS=recon-all:dcm2niix:bidsmapper:bidscoiner:bidseditor:bidsparticipants:bidstrainer:deface:dicomsort:pydeface:rawmapper:Bru2:Bru2Nii:tgv_qsm:fslmaths:fslstats:mnc2nii:niii2mnc:tgv_qsm:recon-all:bidsmapper:bidscoiner:bet:julia:mri_convert:bestlinreg:mincresample:volcentre:norm:volpad:voliso:math:pik:blur:gennlxfm:xfmconcat:bestlinreg:nlpfit:xfmavg:xfminvert:resample:bigaverage:reshape:volsymm  \
    --run="cp /opt/QSMxT/README.md /README.md" \
   > ${imageName}.Dockerfile
 
