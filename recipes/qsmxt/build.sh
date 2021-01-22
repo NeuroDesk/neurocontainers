cfa4364fefca5ded6514b92c7f17918904012bd9
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-01-22 15:15:04 +1000

    try to add dicomifier

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -18,6 +18,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --pkg-manager apt \
    --run="mkdir -p ${mountPointList}" \
    --workdir /opt \
+   --run="conda install -c conda-forge dicomifier"
    --run="git clone https://github.com/QSMxT/QSMxT" \
    --env PATH='$PATH':/opt/bru2 \
    --env DEPLOY_BINS=recon-all:dcm2niix:bidsmapper:bidscoiner:bidseditor:bidsparticipants:bidstrainer:deface:dicomsort:pydeface:rawmapper:Bru2:Bru2Nii:tgv_qsm:fslmaths:fslstats:mnc2nii:niii2mnc:tgv_qsm:recon-all:bidsmapper:bidscoiner:bet:julia:mri_convert:bestlinreg:mincresample:volcentre:norm:volpad:voliso:math:pik:blur:gennlxfm:xfmconcat:bestlinreg:nlpfit:xfmavg:xfminvert:resample:bigaverage:reshape:volsymm  \
