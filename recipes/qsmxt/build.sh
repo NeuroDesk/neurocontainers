dbf7c4d9b01f22aa3b840f60cd43105d2d14a2d7
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-01-28 16:10:57 +1000

    try again after github actions incident

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -14,14 +14,15 @@ source ../main_setup.sh
 # ubuntu:18.04 
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base docker.pkg.github.com/neurodesk/caid/qsmxtbase_1.0.0:20210122 \
+   --base docker.pkg.github.com/neurodesk/caid/qsmxtbase_1.0.0:20210128 \
    --pkg-manager apt \
    --run="mkdir -p ${mountPointList}" \
    --workdir /opt \
    --run="conda install -c conda-forge dicomifier" \
    --run="git clone https://github.com/QSMxT/QSMxT" \
    --env PATH='$PATH':/opt/bru2 \
-   --env DEPLOY_BINS=recon-all:dcm2niix:bidsmapper:bidscoiner:bidseditor:bidsparticipants:bidstrainer:deface:dicomsort:pydeface:rawmapper:Bru2:Bru2Nii:tgv_qsm:fslmaths:fslstats:mnc2nii:niii2mnc:tgv_qsm:recon-all:bidsmapper:bidscoiner:bet:julia:mri_convert:bestlinreg:mincresample:volcentre:norm:volpad:voliso:math:pik:blur:gennlxfm:xfmconcat:bestlinreg:nlpfit:xfmavg:xfminvert:resample:bigaverage:reshape:volsymm  \
+   --env DEPLOY_PATH=/opt/minc-1.9.17/bin/:/opt/minc-1.9.17/volgenmodel-nipype/extra-scripts:/opt/minc-1.9.17/pipeline:/opt/fsl-6.0.4/bin/:/opt/freesurfer-7.1.1/bin/ \
+   --env DEPLOY_BINS=dcm2niix:bidsmapper:bidscoiner:bidseditor:bidsparticipants:bidstrainer:deface:dicomsort:pydeface:rawmapper:Bru2:Bru2Nii:tgv_qsm:julia  \
    --run="cp /opt/QSMxT/README.md /README.md" \
   > ${imageName}.Dockerfile
 
