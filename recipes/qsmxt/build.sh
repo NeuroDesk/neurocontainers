460b2282e0a5c5d05075d6247c36b550c7d78262
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-02-03 10:02:22 +1000

    dimension error solved by adding no resampling option

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -26,7 +26,6 @@ neurodocker generate ${neurodocker_buildMode} \
    --env DEPLOY_PATH=/opt/minc-1.9.17/bin/:/opt/minc-1.9.17/volgenmodel-nipype/extra-scripts:/opt/minc-1.9.17/pipeline:/opt/fsl-6.0.4/bin/:/opt/freesurfer-7.1.1/bin/ \
    --env DEPLOY_BINS=dcm2niix:bidsmapper:bidscoiner:bidseditor:bidsparticipants:bidstrainer:deface:dicomsort:pydeface:rawmapper:Bru2:Bru2Nii:tgv_qsm:julia  \
    --env PYTHONPATH=/opt/QSMxT \
-   --copy qsm_tgv_main.py /TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3/TGV_QSM \
    --run="cp /opt/QSMxT/README.md /README.md" \
   > ${imageName}.Dockerfile
 
