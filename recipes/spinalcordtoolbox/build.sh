226378fafb3266c2fe4eb8dc4469fbeda36a1719
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-02-22 17:14:36 +1000

    build in place instead of home

--- recipes/spinalcordtoolbox/build.sh
+++ recipes/spinalcordtoolbox/build.sh
@@ -14,16 +14,18 @@ source ../main_setup.sh
 neurodocker generate ${neurodocker_buildMode} \
    --base ubuntu:16.04 \
    --pkg-manager apt \
-   --install="gcc libmpich-dev python3-pyqt5" \
+   --install="gcc libmpich-dev python3-pyqt5 git curl bzip2 libglib2.0-0" \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
    --run="curl -fsSL --retry 5 https://github.com/neuropoly/spinalcordtoolbox/archive/${toolVersion}.tar.gz | tar -xz -C /opt/ " \
-   --user=${toolName} \
    --workdir="/opt/${toolName}-${toolVersion}" \
-   --run="yes | ./install_sct" \
-   --env DEPLOY_PATH=/home/${toolName}/sct_${toolVersion}/bin/ \
-   --env PATH=/home/${toolName}/sct_${toolVersion}/bin/:$PATH \
+   --run="chmod a+rwx /opt/${toolName}-${toolVersion}/spinalcordtoolbox" \
+   --run="chmod a+rwx /opt/${toolName}-${toolVersion}/" \
+   --user=${toolName} \
+   --run="yes | ./install_sct -i" \
+   --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
+   --env PATH=/opt/${toolName}-${toolVersion}/bin/:$PATH \
    --copy README.md /README.md \
   > ${toolName}_${toolVersion}.Dockerfile
 
