a427b9066251e3061246a262a64e8fd5c6c4df57
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-03-01 13:30:48 +1000

    expose SlicerSALT binary

--- recipes/slicersalt/build.sh
+++ recipes/slicersalt/build.sh
@@ -22,7 +22,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="curl -fsSL --retry 5 ${downloadLink} | tar -xz -C /opt/ " \
    --install libpulse-dev libnss3 libglu1-mesa libsm6 libxrender1 libxt6 libxcomposite1 libfreetype6 libasound2 libfontconfig1 libxkbcommon0 libxcursor1 libxi6 libxrandr2 libxtst6 qt5-default libqt5svg5-dev wget libqt5opengl5-dev libqt5opengl5 libqt5gui5 libqt5core5a \
    --env DEPLOY_PATH=/opt/SlicerSALT-${toolVersion}-linux-amd64/bin \
-   --env DEPLOY_BINS=SlicerSALTApp-real \
+   --env DEPLOY_BINS=SlicerSALTApp-real:SlicerSALT \
    --run="ln -s /opt/SlicerSALT-3.0.0-linux-amd64/lib/SlicerSALT-4.11/libgfortran.so /opt/SlicerSALT-3.0.0-linux-amd64/lib/SlicerSALT-4.11/libgfortran.so.3" \
    --env LD_LIBRARY_PATH=/opt/SlicerSALT-3.0.0-linux-amd64/lib/SlicerSALT-4.11:/opt/SlicerSALT-3.0.0-linux-amd64/lib/Python/lib:/opt/SlicerSALT-3.0.0-linux-amd64/lib/QtPlugins:/opt/SlicerSALT-3.0.0-linux-amd64/lib/Teem-1.12.0 \
    --env PATH=/usr/bin:/opt/SlicerSALT-${toolVersion}-linux-amd64/bin:/opt/SlicerSALT-${toolVersion}-linux-amd64 \
