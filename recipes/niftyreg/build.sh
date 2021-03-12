a7f50af07845bf675a100609a3d5e95bf814c439
aswinnarayanan <aswin@excelsol.com>
2021-03-12 12:08:43 +0000

    Add niftyreg

---
+++ recipes/niftyreg/build.sh
@@ -0,0 +1,34 @@
+# this template file builds itksnap and is then used as a docker base image for layer caching
+export toolName='niftyreg'
+export toolVersion='1.4'
+export commit_sha='83d8d1182ed4c227ce4764f1fdab3b1797eecd8d'
+
+if [ "$1" != "" ]; then
+    echo "Entering Debug mode"
+    export debug="true"
+fi
+
+source ../main_setup.sh
+
+neurodocker generate ${neurodocker_buildMode} \
+   --base ubuntu:20.04 \
+   --pkg-manager apt \
+   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+   --run="chmod +x /usr/bin/ll" \
+   --run="mkdir ${mountPointList}" \
+   --install apt_opts="--quiet" wget unzip cmake make g++ \
+   --workdir=/opt \
+   --run="wget --no-check-certificate https://github.com/CAIsr/niftyreg/archive/${commit_sha}.zip && unzip ${commit_sha}.zip && rm ${commit_sha}.zip && mv niftyreg-${commit_sha} src" \
+   --run="mkdir -p build ${toolName}-${toolVersion}" \
+   --run="cmake -S /opt/src -B /opt/build -D CMAKE_INSTALL_PREFIX=/opt/niftyreg" \
+   --run="cd /opt/build && make && make install" \
+   --workdir=/opt/${toolName}-${toolVersion}/ \
+   --env TOOLBOX_PATH=/opt/${toolName}-${toolVersion}/ \
+   --env PATH=/opt/${toolName}-${toolVersion}:${PATH} \
+   --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/ \
+   --copy README.md /README.md \
+  > ${toolName}_${toolVersion}.Dockerfile
+
+if [ "$debug" = "true" ]; then
+   ./../main_build.sh
+fi
