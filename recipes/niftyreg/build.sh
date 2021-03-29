3dd2f00e48c0e82bc4ad603f11b40bec5d465265
aswinnarayanan <aswinn89@gmail.com>
2021-03-29 14:56:39 +0000

    Add niftyreg 1.4.0

--- recipes/niftyreg/build.sh
+++ recipes/niftyreg/build.sh
@@ -1,6 +1,6 @@
 # this template file builds itksnap and is then used as a docker base image for layer caching
 export toolName='niftyreg'
-export toolVersion='2014.14.11'
+export toolVersion='1.4.0'
 export commit_sha='83d8d1182ed4c227ce4764f1fdab3b1797eecd8d'
 
 if [ "$1" != "" ]; then
@@ -13,20 +13,19 @@ source ../main_setup.sh
 neurodocker generate ${neurodocker_buildMode} \
    --base 'ubuntu:20.04' \
    --pkg-manager apt \
-   --install apt_opts="--quiet" wget unzip cmake make g++ \
+   --install apt_opts="--quiet" ca-certificates curl cmake make g++ \
    --workdir=/opt/builder/ \
-   --run="wget --no-check-certificate https://github.com/CAIsr/niftyreg/archive/${commit_sha}.zip" \
-   --run="unzip ${commit_sha}.zip && rm ${commit_sha}.zip && mv niftyreg-${commit_sha} src" \
-   --run="mkdir -p build install" \
-   --run="cmake -S src -B build -D CMAKE_INSTALL_PREFIX=/opt/install" \
+   --run="curl -fsSL --retry 5 https://github.com/KCL-BMEIS/niftyreg/archive/${commit_sha}.tar.gz | tar -xz -C ./" \
+   --run="mv ${toolName}-${commit_sha} src && mkdir build" \
+   --run="cmake -S src -B build -D CMAKE_INSTALL_PREFIX=/opt/${toolName}-${toolVersion}" \
    --run="cd build && make && make install" \
    --base 'ubuntu:20.04' \
-   --pkg-manager apt \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
-   --workdir=/opt/${toolName}-${toolVersion}/ \
-   --copy-from '0' /opt/builder/install/ /opt/${toolName}-${toolVersion}/ \
+   --copy-from '0' /opt/${toolName}-${toolVersion} /opt/${toolName}-${toolVersion} \
+   --workdir=/opt/${toolName}-${toolVersion} \
+   --install apt_opts="--quiet" libgomp1 \
    --env TOOLBOX_PATH=/opt/${toolName}-${toolVersion}/ \
    --env PATH=/opt/${toolName}-${toolVersion}:${PATH} \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
