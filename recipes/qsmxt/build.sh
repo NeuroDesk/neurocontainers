6338dc4c6d56b2a110df6cddb17fd1b5b09cad99
Oracle Public Cloud User <opc@gpu.subnet.vcn.oraclevcn.com>
2021-05-18 05:55:59 +0000

    removed freesurfer

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -23,11 +23,10 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="git clone --depth 1 --branch 210506 https://github.com/QSMxT/QSMxT" \
    --env PATH='$PATH':/opt/bru2 \
    --env PATH='$PATH':/opt/FastSurfer \
-   --env DEPLOY_PATH=/opt/fsl-6.0.4/bin/:/opt/freesurfer-6.0.1/bin/ \
+   --env DEPLOY_PATH=/opt/fsl-6.0.4/bin/
    --env DEPLOY_BINS=dcm2niix:bidsmapper:bidscoiner:bidseditor:bidsparticipants:bidstrainer:deface:dicomsort:pydeface:rawmapper:Bru2:Bru2Nii:tgv_qsm:julia  \
    --env PYTHONPATH=/opt/QSMxT:/TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3/TGV_QSM \
    --run="cp /opt/QSMxT/README.md /README.md" \
-   --run="sed '/freesurfer/d' /neurodocker/startup.sh -i" \
   > ${imageName}.Dockerfile
 
 if [ "$debug" = "true" ]; then
