40d0b674aa9a072e88309cb30a49e0c51f5c475c
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-06-14 16:08:40 +1000

    fixed installable models

--- recipes/spinalcordtoolbox/build.sh
+++ recipes/spinalcordtoolbox/build.sh
@@ -26,11 +26,12 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="yes | ./install_sct -i" \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
    --env PATH=/opt/${toolName}-${toolVersion}/bin/:$PATH \
-   --run="sct_deepseg -install-task seg_mice_gm-wm_dwi" \
+   --run="sct_deepseg -install-task seg_exvivo_gm-wm_t2" \
    --run="sct_deepseg -install-task seg_tumor-edema-cavity_t1-t2" \
    --run="sct_deepseg -install-task seg_tumor_t2" \
    --run="sct_deepseg -install-task seg_mice_gm" \
    --run="sct_deepseg -install-task seg_mice_sc" \
+   --run="sct_deepseg -install-task seg_sc_t2star" \
    --copy README.md /README.md \
   > ${toolName}_${toolVersion}.Dockerfile
 
