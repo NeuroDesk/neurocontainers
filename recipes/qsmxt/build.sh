9de52ab9ddf752bb1c673508ba22eab2dd5d9562
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-06-21 15:22:19 +1000

    qsmxt to 1.1.5 (fastsurfer bug)

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 export toolName='qsmxt'
-export toolVersion='1.1.4'
+export toolVersion='1.1.5'
 
 if [ "$1" != "" ]; then
     echo "Entering Debug mode"
@@ -24,7 +24,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="pip install niflow-nipype1-workflows" \
    --env PATH='$PATH':/opt/bru2 \
    --env PATH='$PATH':/opt/FastSurfer \
-   --env DEPLOY_PATH=/opt/fsl-6.0.4/bin/:/opt/ants-2.3.4/ \
+   --env DEPLOY_PATH=/opt/fsl-6.0.4/bin/:/opt/ants-2.3.4/:/opt/FastSurfer \
    --env DEPLOY_BINS=dcm2niix:bidsmapper:bidscoiner:bidseditor:bidsparticipants:bidstrainer:deface:dicomsort:pydeface:rawmapper:Bru2:Bru2Nii:tgv_qsm:julia  \
    --env PYTHONPATH=/opt/QSMxT:/TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3/TGV_QSM \
    --run="cp /opt/QSMxT/README.md /README.md" \
@@ -37,3 +37,4 @@ fi
 #seems to cause problems with singularity conversion?
    # --run="sed -i '/PS1=/c\PS1=\"${toolName}_${toolVersion}:\\\w # \"' /etc/bash.bashrc" \
    # --run="sed -i '/PS1=/c\PS1=\"${toolName}_${toolVersion}:\\\w # \"' /root/.bashrc" \
+   /opt/FastSurfer/run_fastsurfer.sh
