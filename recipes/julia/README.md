606a9c05b9b8b9227cf96f767439180f3bda0ea1
Steffen Bollmann <stebo85@users.noreply.github.com>
2021-06-23 08:56:13 +1000

    test new registry with julia package

--- recipes/julia/README.md
+++ recipes/julia/README.md
@@ -1,6 +1,6 @@
 
 
-+++++++++++++++++++++ julia/1.4.1 +++++++++++++++++++++++++
++++++++++++++++++++++ julia/toolVersion +++++++++++++++++++++++++
 Julia was designed from the beginning for high performance. Julia programs compile to efficient native code for multiple platforms via LLVM.
 
 
@@ -11,6 +11,6 @@ julia
 
 More documentation can be found here: https://docs.julialang.org/en/v1/
 
-To run container outside of this environment: ml julia/1.4.1
-+++++++++++++++++++++ julia/1.4.1 +++++++++++++++++++++++++
+To run container outside of this environment: ml julia/toolVersion
++++++++++++++++++++++ julia/toolVersion +++++++++++++++++++++++++
 
--- recipes/julia/build.sh
+++ recipes/julia/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 export toolName='julia'
-export toolVersion='1.4.1'
+export toolVersion='1.5.3'
 
 source ../main_setup.sh
 
@@ -12,8 +12,16 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="mkdir ${mountPointList}" \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
-   --install zlib1g-dev libzstd1 $toolName \
+   --install zlib1g-dev libzstd1 \
+   --workdir /opt \
+   --run="wget https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-${toolVersion}-linux-x86_64.tar.gz" \
+   --run="tar zxvf julia-${toolVersion}-linux-x86_64.tar.gz" \
+   --run="rm -rf julia-${toolVersion}-linux-x86_64.tar.gz" \
+   --env PATH='$PATH':/opt/julia-${toolVersion}/bin \
+   --run="julia -e 'using Pkg; Pkg.add(\"MriResearchTools\")'" \
    --env DEPLOY_BINS=julia \
-   --entrypoint /usr/bin/$toolName \
+   --entrypoint /opt/julia-${toolVersion}/bin \
    --copy README.md /README.md \
   > ${imageName}.Dockerfile
+
+sed -i 's/toolVersion/${toolVersion}/' README.md
