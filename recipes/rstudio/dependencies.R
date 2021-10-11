452d955c4ac00ca19bf1d459fbb04cb0c3933192
Oracle Public Cloud User <opc@neurodesk.subnet09021545.vcn09021545.oraclevcn.com>
2021-10-11 11:00:33 +0000

    and with all dependendies of #59

--- recipes/rstudio/dependencies.R
+++ recipes/rstudio/dependencies.R
@@ -3,3 +3,14 @@ if("devtools" %in% rownames(installed.packages()) == FALSE) {install.packages("d
 if("rmarkdown" %in% rownames(installed.packages()) == FALSE) {install.packages("rmarkdown")}
 if("plotly" %in% rownames(installed.packages()) == FALSE) {install.packages("plotly")}
 if("shiny" %in% rownames(installed.packages()) == FALSE) {install.packages("shiny")}
+if("tidyverse" %in% rownames(installed.packages()) == FALSE) {install.packages("tidyverse")}
+if("RColorBrewer" %in% rownames(installed.packages()) == FALSE) {install.packages("RColorBrewer")}
+if("paletteer" %in% rownames(installed.packages()) == FALSE) {install.packages("paletteer")}
+if("ggpubr" %in% rownames(installed.packages()) == FALSE) {install.packages("ggpubr")}
+if("ggthemes" %in% rownames(installed.packages()) == FALSE) {install.packages("ggthemes")}
+if("rbokeh" %in% rownames(installed.packages()) == FALSE) {install.packages("rbokeh")}
+if("RcppCNPy" %in% rownames(installed.packages()) == FALSE) {install.packages("RcppCNPy")}
+if("R.matlab" %in% rownames(installed.packages()) == FALSE) {install.packages("R.matlab")}
+if("caret" %in% rownames(installed.packages()) == FALSE) {install.packages("caret", dependencies = c("Depends", "Suggests"))}
+if("car" %in% rownames(installed.packages()) == FALSE) {install.packages("car")}
+if("BayesFactor" %in% rownames(installed.packages()) == FALSE) {install.packages("BayesFactor", dependencies = TRUE)}
