be0fdc0af88c092afd90fa3934414dcd3eb5b5e4
Oracle Public Cloud User <opc@neurodesk.subnet09021545.vcn09021545.oraclevcn.com>
2021-12-01 07:13:07 +0000

    windows version doesn't work yet

---
+++ recipes/physio/build.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+# this template file builds eeglab and is then used as a docker base image for layer caching
+export toolName='physio'
+export toolVersion='2020.0'
+# Don't forget to update version change in README.md!!!!!
+
+if [ "$1" != "" ]; then
+    echo "Entering Debug mode"
+    export debug="true"
+fi
+
+source ../main_setup.sh
+
+neurodocker generate ${neurodocker_buildMode} \
+   --base-image ubuntu:18.04 \
+   --pkg-manager apt \
+   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+   --run="chmod +x /usr/bin/ll" \
+   --run="mkdir ${mountPointList}" \
+   --install curl unzip ca-certificates openjdk-8-jre dbus-x11 \
+   --matlabmcr version=2020a install_path=/opt/MCR  \
+   --workdir /opt/${toolName}-${toolVersion}/ \
+   --run="curl https://objectstorage.us-ashburn-1.oraclecloud.com/p/b_NtFg0a37NZ-3nJfcTk_LSCadJUyN7IkhhVDB7pv8GGQ2e0brg8kYUnAwFfYb6N/n/sd63xuke79z3/b/neurodesk/o/spm12_standalone_with_physio.zip -o spm12_standalone_with_physio.zip \
+      && unzip spm12_standalone_with_physio.zip -d /opt/${toolName}-${toolVersion}/ \
+      && rm -rf spm12_standalone_with_physio.zip" \
+   --env XAPPLRESDIR=/opt/MCR/v98/x11/app-defaults \
+   --env PATH=/opt/${toolName}-${toolVersion}/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
+   --env DEPLOY_BINS=run_spm12.sh \
+   --copy README.md /README.md \
+  > ${imageName}.${neurodocker_buildExt}
+
+if [ "$debug" = "true" ]; then
+   ./../main_build.sh
+fi
