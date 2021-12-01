a1824312afe50628c96294c20c4047bc076a40df
Oracle Public Cloud User <opc@neurodesk.subnet09021545.vcn09021545.oraclevcn.com>
2021-12-01 10:01:39 +0000

    cleanup

--- recipes/mne/build.sh
+++ recipes/mne/build.sh
@@ -21,11 +21,11 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="mkdir ${mountPointList}" \
    --miniconda version=latest \
       conda_install="python=3 jupyter mne=${toolVersion} mne-bids mnelab nb_conda_kernels pytables h5py seaborn statsmodels pybv scikit-learn pyxdf pyEDFlib neurokit2" \
-   --install wget libnss3 gnupg libxkbfile1 libsecret-1-0 libgtk-3-0 libxss1 libgbm1 \
-   --run="wget -O vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'" \
-   --run="apt install ./vscode.deb"  \
-   --run="rm -rf ./vscode.deb" \
-   --install libxshmfence1 libasound2 \
+   --install wget libnss3 gnupg libxkbfile1 libsecret-1-0 libgtk-3-0 libxss1 libgbm1 libxshmfence1 libasound2 \
+   --run="wget -O vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' \
+      && apt install ./vscode.deb  \
+      && rm -rf ./vscode.deb" \
+   --env DONT_PROMPT_WSL_INSTALL=1 \
    --copy README.md /README.md \
    --user neuro \
  > ${imageName}.Dockerfile
@@ -34,4 +34,4 @@ if [ "$debug" = "true" ]; then
    ./../main_build.sh
 fi
 
-# PROBLEM: VSCODE does not run in singularity container!
+# vscode needs /run bind mounted to work!!!!
