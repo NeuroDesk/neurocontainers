f9d4741dfd2ae941a5d6c51e9746abe289ff3d0e
aswinnarayanan <aswinn89@gmail.com>
2021-12-03 00:52:44 +0000

    Switch template to datalad

--- recipes/template/build.sh
+++ recipes/template/build.sh
@@ -2,8 +2,8 @@
 set -e
 
 # this template file builds itksnap and is then used as a docker base image for layer caching
-export toolName='itksnap'
-export toolVersion='3.8.0'
+export toolName='datalad'
+export toolVersion='0.15.3'
 # Don't forget to update version change in README.md!!!!!
 
 if [ "$1" != "" ]; then
@@ -14,23 +14,17 @@ fi
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image ubuntu:16.04 \
+   --base-image neurodebian:sid-non-free \
+   --env DEBIAN_FRONTEND=noninteractive \
    --pkg-manager apt \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
-   --run="curl -o /example_data.zip https://www.nitrc.org/frs/download.php/750/MRI-crop.zip " \
-   --run="unzip /example_data.zip" \
-   --${toolName} version=${toolVersion} \
-   --entrypoint "/opt/${toolName}-${toolVersion}/bin/itksnap /MRIcrop-orig.gipl" \
-   --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
-   --user=neuro \
+   --install datalad datalad-container \
+   --env DEPLOY_BINS=datalad \
    --copy README.md /README.md \
-  > ${imageName}.${neurodocker_buildExt}
+  > ${toolName}_${toolVersion}.Dockerfile
 
 if [ "$debug" = "true" ]; then
    ./../main_build.sh
 fi
-
-
-   
