ccf3aa3fdab2eac3ced19181432025dc80abcb64
Oracle Public Cloud User <opc@neurodesk.subnet09021545.vcn09021545.oraclevcn.com>
2021-12-04 08:37:57 +0000

    fix permissions in ANTS 2.3.1

--- recipes/ants/build.sh
+++ recipes/ants/build.sh
@@ -5,6 +5,16 @@ export toolName='ants'
 export toolVersion='2.3.1'
 # Don't forget to update version change in README.md!!!!!
 
+if [ "$1" != "" ]; then
+    echo "Entering Debug mode"
+    export debug="true"
+fi
+
+if [ "$1" == "dev" ]; then
+    echo "Entering development mode"
+    export dev="true"
+fi
+
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
@@ -13,7 +23,8 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
-   --ants version=${toolVersion} \
+   --${toolName} version=${toolVersion} \
+   --run="chmod a+rx /opt/${toolName}-${toolVersion} -R" \
    --env DEPLOY_PATH=/opt/ants-${toolVersion}/ \
    --copy README.md /README.md \
   > ${imageName}.${neurodocker_buildExt}
