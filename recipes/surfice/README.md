a94b46b33807ad20213b37741f9482e4fea721f6
Oracle Public Cloud User <opc@neurodesk.subnet09021545.vcn09021545.oraclevcn.com>
2021-12-18 23:20:43 +0000

    add libqt5

---
+++ recipes/surfice/README.md
@@ -0,0 +1,21 @@
+
+----------------------------------
+## surfice/1.0.20210730 ##
+Surf Ice, a simple tool for visualizing connectome networks, tractography and statistical maps on top of anatomical brain images. It is closely related to MRIcroGL, but where MRIcroGL is a volume renderer; Surf Ice is a surface renderer
+
+
+Example:
+```
+surfice
+```
+
+More documentation can be found here: https://www.nitrc.org/plugins/mwiki/index.php/surfice:MainPage
+
+To run applications outside of this container: ml surfice/1.0.20210730
+
+Citation:
+```
+Rorden C, Brett M. Stereotaxic display of brain lesions. Behav Neurol. 2000;12(4):191-200. doi: 10.1155/2000/421719. PMID: 11568431.
+```
+
+----------------------------------
--- recipes/surfice/build.sh
+++ recipes/surfice/build.sh
@@ -2,8 +2,8 @@
 set -e
 
 # this template file builds itksnap and is then used as a docker base image for layer caching
-export toolName='mricrogl'
-export toolVersion='1.2.20211006' # https://github.com/rordenlab/MRIcroGL 
+export toolName='surfice'
+export toolVersion='1.0.20210730' # https://github.com/neurolabusc/surf-ice/releases 
 # Don't forget to update version change in README.md!!!!!
 
 if [ "$1" != "" ]; then
@@ -20,23 +20,18 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
-   --install wget unzip ca-certificates libgtk2.0-0 libqt5pas1 appmenu-gtk2-module libglu1-mesa python3 \
+   --install wget unzip ca-certificates libgtk2.0-0 libqt5pas1 appmenu-gtk2-module libglu1-mesa \
    --workdir /opt \
-   --run="wget --quiet -O MRIcroGL_linux.zip 'https://github.com/rordenlab/MRIcroGL/releases/download/v${toolVersion}/MRIcroGL_linux.zip' \
-      && unzip MRIcroGL_linux.zip  \
-      && rm -rf MRIcroGL_linux.zip" \
+   --run="wget --quiet -O surfice_linux.zip 'https://github.com/neurolabusc/surf-ice/releases/download/v${toolVersion}/surfice_linux.zip' \
+      && unzip surfice_linux.zip  \
+      && rm -rf surfice_linux.zip" \
    --run="wget --quiet -O libqt5pas1_2.9-0_amd64.deb 'https://github.com/davidbannon/libqt5pas/releases/download/v1.2.9/libqt5pas1_2.9-0_amd64.deb' \
       && apt install ./libqt5pas1_2.9-0_amd64.deb  \
       && rm -rf libqt5pas1_2.9-0_amd64.deb" \
-   --env PATH=/opt/MRIcroGL:/opt/MRIcroGL/Resources:$PATH \
-   --env DEPLOY_BINS=MRIcroGL:dcm2niix \
+   --env PATH=/opt/Surf_Ice:/opt/Surf_Ice/Resources:$PATH \
+   --env DEPLOY_BINS=surfice:surficeOld:surficeOld_qt5:surfice_qt5 \
    --copy README.md /README.md \
   > ${imageName}.${neurodocker_buildExt}
-
-   # --miniconda version=4.7.12.1 \
-   #          conda_install='python=3.6' \
-
-# explanation for miniconda version: this is the last version where python 3.6 doesn't create a conflict when installing
 
 if [ "$1" != "" ]; then
    ./../main_build.sh
