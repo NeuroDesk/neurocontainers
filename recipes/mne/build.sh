74708c7a017740e10830c91355ef0ed8a2f109f3
Oracle Public Cloud User <opc@neurodesk.subnet09021545.vcn09021545.oraclevcn.com>
2022-01-14 05:37:33 +0000

    pinned version builds and works

--- recipes/mne/build.sh
+++ recipes/mne/build.sh
@@ -20,9 +20,12 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
+   --copy mne-pinned.yml /opt/mne-pinned.yml \
    --miniconda version=latest \
-      conda_install="python=3 jupyter mne=${toolVersion} mne-bids mnelab nb_conda_kernels pytables h5py seaborn statsmodels pybv scikit-learn pyxdf pyEDFlib neurokit2" \
-   --install git apt-transport-https ca-certificates coreutils curl gnome-keyring gnupg libnotify4 wget libnss3 libxkbfile1 libsecret-1-0 libgtk-3-0 libxss1 libgbm1 libxshmfence1 libasound2 \
+      env_name=${toolName}-${toolVersion} \
+      env_exists=false \
+      yaml_file=/opt/mne-pinned.yml \
+   --install python-pyqt5 unzip git apt-transport-https ca-certificates coreutils curl gnome-keyring gnupg libnotify4 wget libnss3 libxkbfile1 libsecret-1-0 libgtk-3-0 libxss1 libgbm1 libxshmfence1 libasound2 \
    --run="wget -O vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' \
       && apt install ./vscode.deb  \
       && rm -rf ./vscode.deb" \
@@ -37,6 +40,9 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="chmod a+x /usr/local/sbin/code" \
    --user neuro \
  > ${imageName}.${neurodocker_buildExt}
+
+      # conda_install="python=3.8 jupyter mne=${toolVersion} mne-bids mnelab nb_conda_kernels pytables h5py seaborn statsmodels pybv scikit-learn pyxdf pyEDFlib neurokit2" \
+      # pip_install="osfclient" \
 
 
 if [ "$1" != "" ]; then
