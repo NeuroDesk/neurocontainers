308c1e739a82049a8f45291477e9c9c28da75209
Oracle Public Cloud User <opc@neurodesk.subnet09021545.vcn09021545.oraclevcn.com>
2022-01-31 23:03:15 +0000

    fixes now integrated in neurodocker

--- recipes/ants/build.sh
+++ recipes/ants/build.sh
@@ -12,8 +12,8 @@ fi
 
 source ../main_setup.sh
 
-yes | pip uninstall neurodocker
-pip install --no-cache-dir https://github.com/NeuroDesk/neurodocker/tarball/ants-add-scripts --upgrade
+# yes | pip uninstall neurodocker
+# pip install --no-cache-dir https://github.com/NeuroDesk/neurodocker/tarball/ants-add-scripts --upgrade
 
 neurodocker generate ${neurodocker_buildMode} \
    --base-image ubuntu:18.04 \
