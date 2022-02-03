763f6de9ca45fa09ea9d23d680d9d0e08f6d258e
Oracle Public Cloud User <opc@neurodesk.subnet09021545.vcn09021545.oraclevcn.com>
2022-02-03 07:40:19 +0000

    works :)

--- recipes/cat12/build.sh
+++ recipes/cat12/build.sh
@@ -17,8 +17,8 @@ fi
 source ../main_setup.sh
 
 # once this is working contribute this to neurodocker project:
-# yes | pip uninstall neurodocker
-# pip install --no-cache-dir https://github.com/NeuroDesk/neurodocker/tarball/xxx --upgrade
+yes | pip uninstall neurodocker
+pip install --no-cache-dir https://github.com/NeuroDesk/neurodocker/tarball/add-cat12 --upgrade
 
 export MATLAB_VERSION=R2017b
 export MCR_VERSION=v93
@@ -32,36 +32,16 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
-   --install wget unzip ca-certificates openjdk-8-jre dbus-x11 \
-   --env MATLAB_VERSION=$MATLAB_VERSION \
-   --env MCR_VERSION=$MCR_VERSION \
-   --env MCR_UPDATE=$MCR_UPDATE \
    --matlabmcr version=2017b install_path=/opt/mcr  \
-   --env CAT_VERSION=$CAT_VERSION \
-   --env CAT_REVISION=$toolVersion \
-   --env MCR_INHIBIT_CTF_LOCK=1 \
-   --env SPM_HTML_BROWSER=0 \
-   --env SPMROOT=/opt/spm${CAT_VERSION}/ \
-   --env MCRROOT=/opt/mcr/${MCR_VERSION} \
-   --env MCR_INHIBIT_CTF_LOCK=1v \
-   --run="wget --no-check-certificate --progress=bar:force -P /opt http://www.neuro.uni-jena.de/cat12/CAT${CAT_VERSION}_${CAT_REVISION}_${MATLAB_VERSION}_MCR_Linux.zip \
-      && unzip -q /opt/CAT${CAT_VERSION}_${CAT_REVISION}_${MATLAB_VERSION}_MCR_Linux.zip -d /opt \
-      && ln -s  /opt/CAT${CAT_VERSION}_${CAT_REVISION}_${MATLAB_VERSION}_MCR_Linux /opt/cat12 \
-      && rm -f /opt/CAT${CAT_VERSION}_${CAT_REVISION}_${MATLAB_VERSION}_MCR_Linux.zip" \
-   --miniconda \
-         version=latest \
-         conda_install='python=3.8 traits nipype numpy scipy h5py scikit-image' \
-         pip_install='osfclient' \
-   --env LD_LIBRARY_PATH=/opt/mcr/${MCR_VERSION}/runtime/glnxa64:/opt/mcr/${MCR_VERSION}/bin/glnxa64:/opt/mcr/${MCR_VERSION}/sys/os/glnxa64:/opt/mcr/${MCR_VERSION}/sys/opengl/lib/glnxa64:/opt/mcr/${MCR_VERSION}/extern/bin/glnxa64 \
-   --run="/opt/CAT${CAT_VERSION}_${CAT_REVISION}_${MATLAB_VERSION}_MCR_Linux/spm12 function exit \
-      && chmod +x /opt/CAT${CAT_VERSION}_${CAT_REVISION}_${MATLAB_VERSION}_MCR_Linux/*" \
-   --env XAPPLRESDIR=/opt/mcr/${MCR_VERSION}/x11/app-defaults \
+   --cat12 version=r1933_R2017b install_path=/opt/cat12 \
    --env DEPLOY_BINS=run_spm12.sh:spm12 \
-   --env PATH=/opt/CAT${CAT_VERSION}_${CAT_REVISION}_${MATLAB_VERSION}_MCR_Linux:/opt/miniconda-latest/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
    --copy README.md /README.md \
   > ${toolName}_${toolVersion}.${neurodocker_buildExt}
 
-      # && rm -rf /opt/CAT${CAT_VERSION}_${CAT_REVISION}_${MATLAB_VERSION}_MCR_Linux/spm12_mcr/home/ \
+   # --miniconda \
+   #       version=latest \
+   #       conda_install='python=3.8 traits nipype numpy scipy h5py scikit-image' \
+   #       pip_install='osfclient' \
 
 if [ "$1" != "" ]; then
    ./../main_build.sh
