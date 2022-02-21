c8426ae52487746ac6280884e1d148e5cfcdca6b
“stebo85@web.de” <opc@neurodesk.subnet09021545.vcn09021545.oraclevcn.com>
2022-02-21 04:41:39 +0000

    add evince

--- recipes/lcmodel/build.sh
+++ recipes/lcmodel/build.sh
@@ -19,10 +19,11 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
-   --install="curl libxft2 libxss1 libtk8.5 libnet-ifconfig-wrapper-perl vim nano unzip" \
+   --install="curl libxft2 libxss1 libtk8.5 libnet-ifconfig-wrapper-perl vim nano unzip evince" \
    --workdir=/opt/${toolName}-${toolVersion}/ \
-   --run="curl -o /opt/lcm-64.tar http://www.lcmodel.com/pub/LCModel/programs/lcm-64.tar" \
-   --run="tar xf /opt/lcm-64.tar" \
+   --run="curl -o /opt/lcm-64.tar http://www.lcmodel.com/pub/LCModel/programs/lcm-64.tar && \
+          tar xf /opt/lcm-64.tar && \
+          rm -rf /opt/lcm-64.tar" \
    --run="gunzip  -c  lcm-core.tar.gz  |  tar  xf  -" \
    --run="rm -rf lm-core.tar.gz" \
    --workdir=/opt/${toolName}-${toolVersion}/.lcmodel/basis-sets \
