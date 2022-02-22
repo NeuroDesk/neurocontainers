91879ffe9868e0ee2af15a7fed4a15490fc98433
“stebo85@web.de” <opc@neurodesk.subnet09021545.vcn09021545.oraclevcn.com>
2022-02-22 00:50:48 +0000

    updated installer

--- recipes/lcmodel/setup_lcmodel.sh
+++ recipes/lcmodel/setup_lcmodel.sh
@@ -4,7 +4,7 @@ set -e
 if [[ -d ~/.lcmodel/ ]]
 then
     echo ".lcmodel exists on your filesystem."
-    echo "Would you like to delete the local install and replace it with the new version? (y)"
+    echo "Would you like to delete the local install and replace it with the new version? (y/n)"
     read varname
     if [[ "$varname" = "y" ]]
     then
@@ -13,6 +13,8 @@ then
         echo "Setting up lcmodel..."
         cp /opt/lcmodel-6.3/.lcmodel/ ~/ -R
         echo "done."
+    else
+        echo "ok, not changing anything ..."
     fi
 else
     echo "Setting up lcmodel..."
