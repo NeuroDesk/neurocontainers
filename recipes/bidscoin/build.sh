4aaf515b9304cb53bd2ab3cc790620c320bd10c2
Oren Civier <orenciv@gmail.com>
2022-03-01 15:51:11 +0000

    Removed the specification of python version and traits

--- recipes/bidscoin/build.sh
+++ recipes/bidscoin/build.sh
@@ -20,18 +20,8 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
    --miniconda version=latest \
-      env_name=${toolName}-${toolVersion} \
-      env_exists=false \
-      conda_install='python=7.28.0 traits=6.2.0' \
-      pip_install="bidscoin" \
-   --dcm2niix method=source version=latest \
-   --install apt_opts="--quiet" wget zip libgl1 libglib2.0 libglu1-mesa libsm6 libxrender1 libxt6 libxcomposite1 libfreetype6 libasound2 libfontconfig1 libxkbcommon0 libxcursor1 libxi6 libxrandr2 libxtst6 qt5-default libqt5svg5-dev wget libqt5opengl5-dev libqt5opengl5 libqt5gui5 libqt5core5a \
-   --install libgtk2.0-0 \
-   --workdir /opt/bru2 \
-   --run="wget https://github.com/neurolabusc/Bru2Nii/releases/download/v1.0.20180303/Bru2_Linux.zip" \
-   --run="unzip Bru2_Linux.zip" \
-   --env PATH='$PATH':/opt/bru2 \
-   --env DEPLOY_BINS=dcm2niix:bidsmapper:bidscoiner:bidseditor:bidsparticipants:bidstrainer:deface:dicomsort:pydeface:rawmapper:Bru2:Bru2Nii  \
+              pip_install='bidscoin' \
+   --env DEPLOY_BINS=bidsmapper:bidscoiner:dicomsort:rawmapper:echocombine:deface:medeface:bidseditor:bidsparticipants \
    --copy README.md /README.md \
   > ${toolName}_${toolVersion}.Dockerfile
 
