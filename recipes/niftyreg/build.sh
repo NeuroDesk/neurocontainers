f46cb08168035349d4509fde8a480b92e82fd97a
“stebo85@web.de” <opc@neurodesk.subnet09021545.vcn09021545.oraclevcn.com>
2022-03-17 00:25:33 +0000

    updated niftyreg to work with latest neurodocker

--- recipes/niftyreg/build.sh
+++ recipes/niftyreg/build.sh
@@ -1,9 +1,9 @@
 # this template file builds itksnap and is then used as a docker base image for layer caching
 export toolName='niftyreg'
-export toolVersion='1.4.0'
+export toolVersion='1.4.0' #https://github.com/KCL-BMEIS/niftyreg
 # Don't forget to update version change in README.md!!!!!
 
-export commit_sha='83d8d1182ed4c227ce4764f1fdab3b1797eecd8d'
+export commit_sha='8ad2f11507ddedb09ed74a9bd97377b70532ee75'
 
 
 if [ "$1" != "" ]; then
@@ -16,24 +16,20 @@ source ../main_setup.sh
 neurodocker generate ${neurodocker_buildMode} \
    --base-image 'ubuntu:20.04' \
    --pkg-manager apt \
-   --install apt_opts="--quiet" ca-certificates curl cmake make g++ \
-   --workdir=/opt/builder/ \
+   --install libgomp1 ca-certificates curl cmake make g++ \
+   --workdir=/opt/${toolName}-${toolVersion} \
    --run="curl -fsSL --retry 5 https://github.com/KCL-BMEIS/niftyreg/archive/${commit_sha}.tar.gz | tar -xz -C ./" \
    --run="mv ${toolName}-${commit_sha} src && mkdir build" \
    --run="cmake -S src -B build -D CMAKE_INSTALL_PREFIX=/opt/${toolName}-${toolVersion}" \
    --run="cd build && make && make install" \
-   --base-image 'ubuntu:20.04' \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
-   --copy-from '0' /opt/${toolName}-${toolVersion} /opt/${toolName}-${toolVersion} \
-   --workdir=/opt/${toolName}-${toolVersion} \
-   --install apt_opts="--quiet" libgomp1 \
    --env TOOLBOX_PATH=/opt/${toolName}-${toolVersion}/ \
-   --env PATH=/opt/${toolName}-${toolVersion}:${PATH} \
+   --env PATH=/opt/${toolName}-${toolVersion}/bin:${PATH} \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
    --copy README.md /README.md \
-  > ${toolName}_${toolVersion}.Dockerfile
+> ${imageName}.${neurodocker_buildExt} 
 
 if [ "$1" != "" ]; then
    ./../main_build.sh
