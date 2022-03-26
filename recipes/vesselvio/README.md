4f8f0cb4eb2a43659ded719a185187b32554099b
Steffen Bollmann <stebo85@users.noreply.github.com>
2022-03-26 14:54:16 +1000

    started vesselvio

---
+++ recipes/vesselvio/README.md
@@ -0,0 +1,21 @@
+
+----------------------------------
+## vesselvio/1.1.1 ##
+VesselVio is an open-source application designed to allow researchers to analyze and visualize segmented vasculature datasets. The application is also compatible with annotated datasets and pre-constructed graphs made in other programs.
+
+Example:
+```
+cd /opt/vesselvio
+python VesselVio.py
+```
+
+More documentation can be found here: https://jacobbumgarner.github.io/VesselVio/
+
+To run applications outside of this container: ml vesselvio/1.1.1
+
+Citation:
+```
+https://doi.org/10.1016/j.crmeth.2022.100189
+```
+
+----------------------------------
---
+++ recipes/vesselvio/build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+
+export toolName='vesselvio'
+export toolVersion='1.1.1' #the version number cannot contain a "-" - try to use x.x.x notation always
+# Don't forget to update version change in README.md!!!!!
+# toolName or toolVersion CANNOT contain capital letters or dashes or underscores (Docker registry does not accept this!)
+
+if [ "$1" != "" ]; then
+    echo "Entering Debug mode"
+    export debug=$1
+fi
+
+source ../main_setup.sh
+
+###########################################################################################################################################
+# IF POSSIBLE, PLEASE DOCUMENT EACH ARGUMENT PROVIDED TO NEURODOCKER. USE THE `# your comment` NOTATION THAT ALLOWS MID-COMMAND COMMENTS
+# NOTE 1: THE QUOTES THAT ENCLOSE EACH COMMENT MUST BE BACKQUOTES (`). OTHER QUOTES WON'T WORK!
+# NOTE 2: THE BACKSLASH (\) AT THE END OF EACH LINE MUST FOLLOW THE COMMENT. A BACKSLASH BEFORE THE COMMENT WON'T WORK!
+##########################################################################################################################################
+neurodocker generate ${neurodocker_buildMode} \
+   --base-image neurodebian:sid-non-free                `# neurodebian makes it easy to install neuroimaging software, recommended as default` \
+   --env DEBIAN_FRONTEND=noninteractive                 `# this disables interactive questions during package installs` \
+   --pkg-manager apt                                    `# desired package manager, has to match the base image (e.g. debian needs apt; centos needs yum)` \
+   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll"   `# define the ll command to show detailed list including hidden files`  \
+   --run="chmod +x /usr/bin/ll"                         `# make ll command executable`  \
+   --run="mkdir ${mountPointList}"                      `# create folders for singularity bind points` \
+   --install python3-pyqt5 \
+   --run="curl -fsSL --retry 5 https://github.com/JacobBumgarner/VesselVio/archive/refs/tags/v${toolVersion}.tar.gz | tar -xz -C /opt/ " \
+   --miniconda version=latest \
+		pip_install='' \
+   --env DEPLOY_PATH=/opt/${toolName}-latest/           `# specify a path where ALL binary files will be exposed outside the container for the module system. Never expose a directory with system commands (like /bin/ /usr/bin ...)` \
+   --env DEPLOY_BINS=datalad:bidscoiner                 `# specify indiviual binaries (separated by :) on the PATH that should be exposed outside the container for the module system` \
+   --copy README.md /README.md                          `# include readme file in container` \
+  > ${imageName}.${neurodocker_buildExt}                `# LAST COMMENT; NOT FOLLOWED BY BACKSLASH!`
+
+
+if [ "$1" != "" ]; then
+   ./../main_build.sh
+fi
