80e7ad26018af316427f1fb55b69cd5e993d4154
“stebo85@web.de” <opc@neurodesk.subnet09021545.vcn09021545.oraclevcn.com>
2022-03-27 02:52:43 +0000

    change to debian:stable

--- recipes/vesselvio/build.sh
+++ recipes/vesselvio/build.sh
@@ -19,14 +19,12 @@ source ../main_setup.sh
 # NOTE 2: THE BACKSLASH (\) AT THE END OF EACH LINE MUST FOLLOW THE COMMENT. A BACKSLASH BEFORE THE COMMENT WON'T WORK!
 ##########################################################################################################################################
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image centos:8               `# neurodebian makes it easy to install neuroimaging software, recommended as default` \
+   --base-image debian:stable              `# neurodebian makes it easy to install neuroimaging software, recommended as default` \
    --env DEBIAN_FRONTEND=noninteractive                 `# this disables interactive questions during package installs` \
-   --pkg-manager yum                                    `# desired package manager, has to match the base image (e.g. debian needs apt; centos needs yum)` \
+   --pkg-manager apt                                    `# desired package manager, has to match the base image (e.g. debian needs apt; centos needs yum)` \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll"   `# define the ll command to show detailed list including hidden files`  \
    --run="chmod +x /usr/bin/ll"                         `# make ll command executable`  \
    --run="mkdir ${mountPointList}"                      `# create folders for singularity bind points` \
-   --copy fixCentos8.sh /opt/                           `# make centos8 work again` \
-   --run="bash /opt/fixCentos8.sh"                      `# make centos8 work again` \
    --install ca-certificates curl  `# install packages mesa is for swrast to work; the rest for QT5 xcb` \
    --workdir /opt/${toolName}-${toolVersion}/           `# create install directory` \
    --run="curl -fsSL --retry 5 https://github.com/JacobBumgarner/VesselVio/archive/refs/tags/v${toolVersion}.tar.gz | tar -xz -C /opt/${toolName}-${toolVersion} --strip-components 1" \
@@ -42,6 +40,9 @@ neurodocker generate ${neurodocker_buildMode} \
    # --run="pip install -r /opt/${toolName}-${toolVersion}/requirements.txt" \
 
 # mesa-dri-drivers libglvnd-glx libXrender fontconfig libxkbcommon-x11 gtk3 qt5-qtbase-gui python3-pyqt5-sip
+
+# 	libglvnd-glx qt5-qtbase-gui libXrender mesa-dri-drivers
+
 
 # debug QT problems with 
 # export QT_DEBUG_PLUGINS=1
