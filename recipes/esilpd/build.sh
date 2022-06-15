2efcc0998b4b3d55b6c1069a62a3b1bc607b2fc5
Kaxnn <52154094+Kaxnn@users.noreply.github.com>
2022-06-15 15:34:19 +1000

    Update build.sh

--- recipes/esilpd/build.sh
+++ recipes/esilpd/build.sh
@@ -23,21 +23,21 @@ source ../main_setup.sh
 
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image debian:10   \
+   --base-image debian:11   \
    --pkg-manager apt   \
    --env DEBIAN_FRONTEND=noninteractive  \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll"  \
    --run="mkdir -p ${mountPointList}" \
-   --install midori xdg-utils software-properties-common gcc-4.9 python3-pyqt5 unzip git apt-transport-https ca-certificates coreutils curl gnome-keyring gnupg libnotify4 wget libnss3 libxkbfile1 libsecret-1-0 libgtk-3-0 libxss1 libgbm1 libxshmfence1 libasound2 libglu1-mesa libgl1-mesa-dri mesa-utils libgl1-mesa-glx \
+   --install opts='--quiet' midori xdg-utils software-properties-common libstdc++6 gcc-10-base python3-pyqt5 unzip git apt-transport-https ca-certificates coreutils curl gnome-keyring gnupg libnotify4 wget libnss3 libxkbfile1 libsecret-1-0 libgtk-3-0  libgcc1  libc6 libxss1 libgbm1 libxshmfence1 libasound2 libglu1-mesa libgl1-mesa-dri mesa-utils libgl1-mesa-glx binutils \
+   --run="strings /usr/lib/x86_64-linux-gnu/libstdc++.so.6 | grep GLIBCXX "\
+   --run="wget -q https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/cuda-keyring_1.0-1_all.deb && dpkg -i cuda-keyring_1.0-1_all.deb && rm cuda-keyring_1.0-1_all.deb"\
+   --run="apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/7fa2af80.pub"\
+   --run="add-apt-repository 'deb https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/ /'"\
    --run="add-apt-repository contrib"\
-   --run="wget https://developer.download.nvidia.com/compute/cuda/repos/debian10/x86_64/cuda-keyring_1.0-1_all.deb && dpkg -i cuda-keyring_1.0-1_all.deb && rm cuda-keyring_1.0-1_all.deb"\
-   --run="apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/debian10/x86_64/3bf863cc.pub"\
-   --run="add-apt-repository 'deb https://developer.download.nvidia.com/compute/cuda/repos/debian10/x86_64/ /'"\
-   --run="apt-get --allow-releaseinfo-change update"\
-   --install cuda-11-3 nsight-compute-2021.2.2 \
-   --run="apt-get install --only-upgrade libstdc++6"\
-   --run="wget https://developer.download.nvidia.com/compute/redist/cudnn/v8.2.1/cudnn-11.3-linux-x64-v8.2.1.32.tgz" \
+   --install opts='--quiet' cuda-11-5 nsight-compute-2022.2.0 \
+   --run="add-apt-repository contrib && apt-get upgrade libstdc++6"\
+   --run="wget -q https://developer.download.nvidia.com/compute/redist/cudnn/v8.2.1/cudnn-11.3-linux-x64-v8.2.1.32.tgz" \
    --run="tar -xvf cudnn-11.3-linux-x64-v8.2.1.32.tgz && rm cudnn-11.3-linux-x64-v8.2.1.32.tgz"\
    --run="cp cuda/include/cudnn*.h /usr/local/cuda/include && cp -P cuda/lib64/libcudnn* /usr/local/cuda/lib64"\
    --run="chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*"\
@@ -45,13 +45,11 @@ neurodocker generate ${neurodocker_buildMode} \
         version=4.7.12 \
         env_name=base \
    --run="conda install -c conda-forge mamba=0.24.0 "\
-   --run="mamba create --override-channels --channel=conda-forge --name=${toolName}-${toolVersion} mne"\
+   --run="mamba create --override-channels --channel=conda-forge --name=${toolName}-${toolVersion} python=3.9 mne"\
    --run-bash=". activate ${toolName}-${toolVersion} && pip3 install --no-cache-dir torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113"\
-   --run-bash=". activate ${toolName}-${toolVersion} && pip3 install --no-cache-dir tensorflow==2.8.2 esinet osfclient scikit-image pybids seaborn argh joblib torchaudio"\
+   --run-bash=". activate ${toolName}-${toolVersion} && pip3 install --no-cache-dir osfclient nb_conda ipykernel scikit-image pybids seaborn argh joblib torchaudio odl[testing,show]"\
    --run="rm -rf ~/.cache/pip/*"\
-   --run="wget -O vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' \
-   && apt install ./vscode.deb  \
-   && rm -rf ./vscode.deb" \
+   --run="wget -q -O vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' && apt install ./vscode.deb && rm -rf ./vscode.deb" \
    --run=" code --extensions-dir=/opt/vscode-extensions --user-data-dir=/opt/vscode-data --install-extension ms-python.python \
     && code --extensions-dir=/opt/vscode-extensions --user-data-dir=/opt/vscode-data --install-extension ms-python.vscode-pylance \
     && code --extensions-dir=/opt/vscode-extensions --user-data-dir=/opt/vscode-data --install-extension ms-toolsai.jupyter \
