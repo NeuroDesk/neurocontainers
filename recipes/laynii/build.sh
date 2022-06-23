aa65a08494207af6c0a6800763103852e56ece51
Steffen Bollmann <s.bollmann@uq.edu.au>
2022-06-23 01:18:44 +0000

    working :)

--- recipes/laynii/build.sh
+++ recipes/laynii/build.sh
@@ -23,19 +23,19 @@ source ../main_setup.sh
 # NOTE 2: THE BACKSLASH (\) AT THE END OF EACH LINE MUST FOLLOW THE COMMENT. A BACKSLASH BEFORE THE COMMENT WON'T WORK!
 ##########################################################################################################################################
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image ubuntu:18.04                            `# any linux version will do` \                 
+   --base-image ubuntu:18.04                            `# any linux version will do` \
    --pkg-manager apt                                    `# not sure if I actually need this` \
    --env DEBIAN_FRONTEND=noninteractive                 `# this disables interactive questions during package installs` \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll"   `# define the ll command to show detailed list including hidden files`  \
    --run="chmod +x /usr/bin/ll"                         `# make ll command executable`  \
    --run="mkdir ${mountPointList}"                      `# create folders for singularity bind points` \
-   --install curl ca-certificates \
+   --install wget ca-certificates unzip \
    --workdir /opt/${toolName}-${toolVersion}/ \
-   --run="curl -o /laynii.zip https://github.com/layerfMRI/LAYNII/releases/download/v2.2.1/LayNii_v${toolVersion}_Linux64.zip " \
-   --run="unzip /laynii.zip" \
-   --${toolName} version=${toolVersion} \
+   --run="wget https://github.com/layerfMRI/LAYNII/releases/download/v2.2.1/LayNii_v${toolVersion}_Linux64.zip \
+            && unzip LayNii_v${toolVersion}_Linux64.zip \
+            && rm LayNii_v${toolVersion}_Linux64.zip" \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/ \
-   --env PATH=/opt/${toolName}-${toolVersion}/:${PATH} \
+   --env PATH='$PATH':/opt/${toolName}-${toolVersion}/ \
    --copy README.md /README.md                          `# include readme file in container` \
   > ${imageName}.${neurodocker_buildExt}                `# LAST COMMENT; NOT FOLLOWED BY BACKSLASH!`
 
