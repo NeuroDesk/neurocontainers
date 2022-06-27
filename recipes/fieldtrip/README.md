46e6bda55f4bd3d9e2a7f00ac8f3475e3435979d
David White <davidjwhite33@gmail.com>
2022-06-27 19:07:37 +1000

    Update fieldtrip version (#140)

--- recipes/fieldtrip/README.md
+++ recipes/fieldtrip/README.md
@@ -1,6 +1,6 @@
 
 ----------------------------------
-## fieldtrip/20211114 ##
+## fieldtrip/20220617 ##
 Standalone (compiled) version of FieldTrip with MATLAB runtime
 
 Example:
@@ -10,6 +10,8 @@ run_fieldtrip.sh /opt/MCR/v99 yourscript.m
 
 More documentation can be found here: https://www.fieldtriptoolbox.org/tutorial/
 
-To run applications outside of this container: ml fieldtrip/20211114
+To run applications outside of this container: ml fieldtrip/20220617
 
+Please cite the FieldTrip reference paper when you have used FieldTrip in your study.
+Robert Oostenveld, Pascal Fries, Eric Maris, and Jan-Mathijs Schoffelen. FieldTrip: Open Source Software for Advanced Analysis of MEG, EEG, and Invasive Electrophysiological Data. Computational Intelligence and Neuroscience, vol. 2011, Article ID 156869, 9 pages, 2011. doi:10.1155/2011/156869.
 ----------------------------------
--- recipes/fieldtrip/build.sh
+++ recipes/fieldtrip/build.sh
@@ -3,7 +3,7 @@ set -e
 
 # this template file builds fieldtrip and is then used as a docker base image for layer caching
 export toolName='fieldtrip'
-export toolVersion='20211114'
+export toolVersion='20220617'
 # Don't forget to update version change in README.md!!!!!
 
 if [ "$1" != "" ]; then
@@ -14,8 +14,6 @@ fi
 
 source ../main_setup.sh
 
-#yes | pip uninstall neurodocker
-#pip install --no-cache-dir https://github.com/NeuroDesk/neurodocker/tarball/update_mcr --upgrade
 
 
 neurodocker generate ${neurodocker_buildMode} \
@@ -27,7 +25,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --install curl unzip ca-certificates openjdk-8-jre dbus-x11 \
    --matlabmcr version=2020b install_path=/opt/MCR  \
    --workdir /opt/${toolName}-${toolVersion}/ \
-   --run="curl -fsSL --retry 5 https://objectstorage.us-ashburn-1.oraclecloud.com/p/b_NtFg0a37NZ-3nJfcTk_LSCadJUyN7IkhhVDB7pv8GGQ2e0brg8kYUnAwFfYb6N/n/sd63xuke79z3/b/neurodesk/o/fieldtrip20211114_mcr2020b.tar.gz \
+   --run="curl -fsSL --retry 5 https://objectstorage.us-ashburn-1.oraclecloud.com/p/b_NtFg0a37NZ-3nJfcTk_LSCadJUyN7IkhhVDB7pv8GGQ2e0brg8kYUnAwFfYb6N/n/sd63xuke79z3/b/neurodesk/o/fieldtrip20220617_mcr2020b.tar.gz \
       | tar -xz -C /opt/${toolName}-${toolVersion}/ --strip-components 1 \
       && chmod +x /opt/${toolName}-${toolVersion}/*" \
    --env PATH=/opt/${toolName}-${toolVersion}/:$PATH \
