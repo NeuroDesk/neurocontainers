b4280e23ffdf11ef95a393187f5dc4d5c7676faf
Ashley <a.stewart.au@gmail.com>
2022-07-27 18:15:56 +1000

    revert unnecessary changes

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -30,7 +30,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --env PATH='$PATH':/opt/bru2 \
    --env PATH='$PATH':/opt/FastSurfer \
    --env DEPLOY_PATH=/opt/fsl-6.0.4/bin/:/opt/ants-2.3.4/:/opt/FastSurfer \
-   --env DEPLOY_BINS=python:dcm2niix:bidsmapper:bidscoiner:bidseditor:bidsparticipants:bidstrainer:deface:dicomsort:pydeface:rawmapper:Bru2:Bru2Nii:tgv_qsm:julia  \
+   --env DEPLOY_BINS=dcm2niix:bidsmapper:bidscoiner:bidseditor:bidsparticipants:bidstrainer:deface:dicomsort:pydeface:rawmapper:Bru2:Bru2Nii:tgv_qsm:julia  \
    --env PYTHONPATH=/opt/QSMxT:/TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3/TGV_QSM \
    --copy README.md /README.md \
   > ${imageName}.${neurodocker_buildExt}
