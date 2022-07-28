bd624d29bc75d8cb7f897a73a5d068fe093ddf64
yexincheng <50694493+yexincheng@users.noreply.github.com>
2022-07-28 11:10:47 +1000

    update slicer 5.0.3 (#146)
    * Update build.sh
    
    update slicer to 5.0.3
    
    * Update README.md
    
    * Update build.sh
    
    install curl and change the install order
    
    * Update build.sh
    
    noninteractive mode
    
    * Update build.sh
    
    add ca-certificate

--- recipes/slicer/build.sh
+++ recipes/slicer/build.sh
@@ -3,9 +3,9 @@ set -e
 
 # https://slicer.kitware.com/midas3/folder/274
 # export downloadLink='https://slicer.kitware.com/midas3/download/item/549121/Slicer-4.11.20200930-linux-amd64.tar.gz'
-export downloadLink='https://slicer.kitware.com/midas3/download?bitstream=1341035'
+export downloadLink='https://download.slicer.org/bitstream/62cc52d2aa08d161a31c1af0'
 export toolName='slicer'
-export toolVersion='4.11.20200930'
+export toolVersion='5.0.3'
 # Don't forget to update version change in README.md!!!!!
 
 if [ "$1" != "" ]; then
@@ -18,11 +18,12 @@ source ../main_setup.sh
 neurodocker generate ${neurodocker_buildMode} \
    --base-image ubuntu:20.04 \
    --pkg-manager apt \
+   --env DEBIAN_FRONTEND=noninteractive \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
+   --install curl ca-certificates libpulse-dev libnss3 libglu1-mesa libsm6 libxrender1 libxt6 libxcomposite1 libfreetype6 libasound2 libfontconfig1 libxkbcommon0 libxcursor1 libxi6 libxrandr2 libxtst6 qt5-default libqt5svg5-dev wget libqt5opengl5-dev libqt5opengl5 libqt5gui5 libqt5core5a \
    --run="curl -fsSL --retry 5 ${downloadLink} | tar -xz -C /opt/ " \
-   --install libpulse-dev libnss3 libglu1-mesa libsm6 libxrender1 libxt6 libxcomposite1 libfreetype6 libasound2 libfontconfig1 libxkbcommon0 libxcursor1 libxi6 libxrandr2 libxtst6 qt5-default libqt5svg5-dev wget libqt5opengl5-dev libqt5opengl5 libqt5gui5 libqt5core5a \
    --env DEPLOY_PATH=/opt/Slicer-${toolVersion}-linux-amd64/bin \
    --env DEPLOY_BINS=Slicer \
    --env PATH=/usr/bin:/opt/Slicer-${toolVersion}-linux-amd64/bin:/opt/Slicer-${toolVersion}-linux-amd64 \
