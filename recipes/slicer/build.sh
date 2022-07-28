abee1e91d3e7c554e24d75d3e75dc755924d684b
yexincheng <50694493+yexincheng@users.noreply.github.com>
2022-07-28 10:56:53 +1000

    Update build.sh
    update slicer to 5.0.3

--- recipes/slicer/build.sh
+++ recipes/slicer/build.sh
@@ -3,9 +3,9 @@ set -e
 
 # https://slicer.kitware.com/midas3/folder/274
 # export downloadLink='https://slicer.kitware.com/midas3/download/item/549121/Slicer-4.11.20200930-linux-amd64.tar.gz'
-export downloadLink='https://slicer.kitware.com/midas3/download?bitstream=1341035'
+export downloadLink='https://download.slicer.org/bitstream/62cc52d2aa08d161a31c1af0'
 export toolName='slicer'
-export toolVersion='4.11.20200930'
+export toolVersion='5.0.3'
 # Don't forget to update version change in README.md!!!!!
 
 if [ "$1" != "" ]; then
