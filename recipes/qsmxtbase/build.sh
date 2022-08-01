2b475b3d9370dcf178d8887a92c904a347dec57b
Ashley Stewart <a.stewart.au@gmail.com>
2022-08-01 14:56:10 +1000

    qsmxtbase - dependency fixes

--- recipes/qsmxtbase/build.sh
+++ recipes/qsmxtbase/build.sh
@@ -55,7 +55,7 @@ yes | neurodocker generate ${neurodocker_buildMode} \
    --ants version=2.3.4 \
    --dcm2niix method=source version=003f0d19f1e57b0129c9dcf3e653f51ca3559028 \
    --miniconda version=4.7.12.1 \
-            conda_install='python=3.6 dicomifier scikit-sparse nibabel=2.5.1 pillow=6.2.0 traits=6.2.0 nipype=1.6.0 networkx=2.5 numpy=1.19.4 scipy=1.5.3 h5py=2.10.0 scikit-image=0.17.2' \
+            conda_install='python=3.6 numpy=1.19.5 h5py=3.1.0 nibabel=3.2.2 dicomifier=2.2.0 scikit-sparse=0.4.6 traits=6.2.0 nipype=1.6.1 scipy=1.5.3 scikit-image=0.17.2' \
    --run="conda install -c pytorch cpuonly "pytorch=1.2.0=py3.6_cpu_0" torchvision=0.4.0=py36_cpu" \
    --run="git clone https://github.com/Deep-MI/FastSurfer.git /opt/FastSurfer" \
    --env FASTSURFER_HOME=/opt/FastSurfer \
