931e4c81fb7d46b8f9be02e9550906d912c16003
yexincheng <50694493+yexincheng@users.noreply.github.com>
2022-08-04 14:44:03 +1000

    install miniconda
    rm python3-dev gcc python3-pip python3-setuptools

--- recipes/slicer/build.sh
+++ recipes/slicer/build.sh
@@ -22,7 +22,13 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
-   --install libopenslide0 python3-dev gcc python3-pip python3-setuptools curl ca-certificates libxdamage1 libpulse-dev libnss3 libglu1-mesa libsm6 libxrender1 libxt6 libxcomposite1 libfreetype6 libasound2 libfontconfig1 libxkbcommon0 libxcursor1 libxi6 libxrandr2 libxtst6 qt5-default libqt5svg5-dev wget libqt5opengl5-dev libqt5opengl5 libqt5gui5 libqt5core5a \
+   --install libopenslide0 curl ca-certificates libxdamage1 libpulse-dev libnss3 libglu1-mesa libsm6 libxrender1 libxt6 libxcomposite1 libfreetype6 libasound2 libfontconfig1 libxkbcommon0 libxcursor1 libxi6 libxrandr2 libxtst6 qt5-default libqt5svg5-dev wget libqt5opengl5-dev libqt5opengl5 libqt5gui5 libqt5core5a \
+   --env CONDA_DIR $HOME/miniconda3 \
+   --run="wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh" \
+   --run="chmod +x ~/miniconda.sh" \
+   --run="~/miniconda.sh -b -p $CONDA_DIR && rm ~/miniconda.sh" \
+   --env PATH=$CONDA_DIR/bin:$PATH \
+   --run="conda install pip" \
    --run="pip install monai" \
    --run="pip install h11==0.11" \
    --run="pip install monailabel" \
