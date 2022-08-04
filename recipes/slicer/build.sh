27bb36bdf31f1da7ec6673802017377880e3b069
yexincheng <50694493+yexincheng@users.noreply.github.com>
2022-08-04 15:28:43 +1000

    modify conda path

--- recipes/slicer/build.sh
+++ recipes/slicer/build.sh
@@ -23,11 +23,10 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
    --install libopenslide0 curl ca-certificates libxdamage1 libpulse-dev libnss3 libglu1-mesa libsm6 libxrender1 libxt6 libxcomposite1 libfreetype6 libasound2 libfontconfig1 libxkbcommon0 libxcursor1 libxi6 libxrandr2 libxtst6 qt5-default libqt5svg5-dev wget libqt5opengl5-dev libqt5opengl5 libqt5gui5 libqt5core5a \
-   --env CONDA_DIR=/miniconda3 \
    --run="wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh" \
    --run="chmod +x ~/miniconda.sh" \
-   --env PATH=$CONDA_DIR/bin:$PATH \
-   --run="~/miniconda.sh -b -p $CONDA_DIR && rm ~/miniconda.sh" \
+   --env PATH=/miniconda3/bin:$PATH \
+   --run="~/miniconda.sh -b -p /miniconda3/ && rm ~/miniconda.sh" \
    --run="echo $PATH"
    --run="conda install pip" \
    --run="pip install monai" \
