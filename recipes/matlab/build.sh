3fc1adc69dd99fcb892bd690773e72e8308e2bf8
Steffen Bollmann <s.bollmann@uq.edu.au>
2022-08-13 08:19:15 +0000

    create license directory

--- recipes/matlab/build.sh
+++ recipes/matlab/build.sh
@@ -47,7 +47,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="rm /usr/local/bin/matlab"			`# rm original matlab symbolic link` \
    --copy matlab /usr/local/bin/matlab `# replace original matlab with a script that sets MLM_LICENSE_FILE and then call matlab; license dir is set to ~/Downloads because there is where Firefox download the license to` \
    --run="chmod a+x /usr/local/bin/matlab"     		`# make matlab executables` \
-   --run="ln -s ~/Downloads /opt/matlab/R2022a/licenses"     		`# try to create a soft link to the homedirectory download folder` \
+   --run="mkdir /opt/matlab/R2022a/licenses"     		`# create license directory - this will later be bind-mounted to the homedirectory download folder` \
   > ${imageName}.${neurodocker_buildExt}                `# LAST COMMENT; NOT FOLLOWED BY BACKSLASH!`
 
 
