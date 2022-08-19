f86744220cdb8dddbfb390f183ded4237af8c4f8
aswinnarayanan <aswinn89@gmail.com>
2022-08-19 22:55:58 +0000

    Fix update-builders change

--- update-builders.sh
+++ update-builders.sh
@@ -8,7 +8,7 @@ for RECIPES in recipes/*/; do
             sed -i '/      - .github\/workflows\/free-up-space-list.txt/d' .github/workflows/$APPLICATION.yml
         fi
         if grep -m 1 -q "$APPLICATION" .github/workflows/self-hosted-list.txt; then
-            sed -i '/runs-on: ubuntu-latest/runs-on: self-hosted/d' .github/workflows/$APPLICATION.yml
+            sed -i 's/runs-on: ubuntu-latest/runs-on: self-hosted/g' .github/workflows/$APPLICATION.yml
         fi
     fi
 done
