06be7b06a69a041893d53e8fbd505673550c2f58
Steffen Bollmann <s.bollmann@uq.edu.au>
2022-09-03 05:35:48 +0000

    added monailabel plugin to slicer install

--- recipes/slicer/build.sh
+++ recipes/slicer/build.sh
@@ -5,7 +5,7 @@ set -e
 # export downloadLink='https://slicer.kitware.com/midas3/download/item/549121/Slicer-4.11.20200930-linux-amd64.tar.gz'
 export downloadLink='https://download.slicer.org/bitstream/62cc52d2aa08d161a31c1af0'
 export toolName='slicer'
-export toolVersion='5.0.3'
+export toolVersion='5.0.3' #When updating this version you also need to update the MONAILabel plugin version (line 31)!
 # Don't forget to update version change in README.md!!!!!
 
 if [ "$1" != "" ]; then
@@ -28,6 +28,9 @@ neurodocker generate ${neurodocker_buildMode} \
    --env PATH=/miniconda3/bin:$PATH \
    --run="pip install h11==0.11 monailabel" \
    --run="curl -fsSL --retry 5 ${downloadLink} | tar -xz -C /opt/ " \
+   --workdir /opt/Slicer-${toolVersion}-linux-amd64/NA-MIC/ \
+   --run="curl -fsSL --retry 5 https://objectstorage.us-ashburn-1.oraclecloud.com/p/b_NtFg0a37NZ-3nJfcTk_LSCadJUyN7IkhhVDB7pv8GGQ2e0brg8kYUnAwFfYb6N/n/sd63xuke79z3/b/neurodesk/o/MONAILabel30893.tar.gz \
+      | tar -xz -C /opt/Slicer-${toolVersion}-linux-amd64/NA-MIC/ --strip-components 1" \
    --env DEPLOY_PATH=/opt/Slicer-${toolVersion}-linux-amd64/bin \
    --env DEPLOY_BINS=Slicer \
    --env PATH=/miniconda3/bin:/usr/bin:/opt/Slicer-${toolVersion}-linux-amd64/bin:/opt/Slicer-${toolVersion}-linux-amd64 \
@@ -37,3 +40,9 @@ neurodocker generate ${neurodocker_buildMode} \
 if [ "$1" != "" ]; then
    ./../main_build.sh
 fi
+
+
+# /opt/Slicer-5.0.3-linux-amd64/NA-MIC/Extensions-30893
+
+# This would be more elegant or an overlay fs
+# singularity shell --bind /neurodesktop-storage/slicerplugins:/opt/Slicer-5.0.3-linux-amd64/NA-MIC/ /cvmfs/neurodesk.ardc.edu.au/containers/slicer_5.0.3_20220809/slicer_5.0.3_20220809.simg
