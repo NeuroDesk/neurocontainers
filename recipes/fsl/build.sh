7f4df6c414b535902b2e55a334eb3b644e20231c
Steffen Bollmann <s.bollmann@uq.edu.au>
2022-09-17 08:07:56 +0000

    should install cuda 10.2 and add to path and ld_library_path

--- recipes/fsl/build.sh
+++ recipes/fsl/build.sh
@@ -12,22 +12,35 @@ fi
 
 source ../main_setup.sh
 
-yes | pip uninstall neurodocker
-pip install --no-cache-dir https://github.com/NeuroDesk/neurodocker/tarball/update-fsl-to-6.0.5.1 --upgrade
-
 yes | neurodocker generate ${neurodocker_buildMode} \
-   --base-image ubuntu:16.04 \
+   --base-image ubuntu:18.04 \
    --pkg-manager apt \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
-   --install ltrace strace \
+   --install ca-certificates ltrace strace wget libxml2 gcc \
+   --run="wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run && \
+            sh cuda_10.2.89_440.33.01_linux.run --silent && \
+            rm cuda_10.2.89_440.33.01_linux.run && \
+            wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/patches/1/cuda_10.2.1_linux.run && \
+            sh cuda_10.2.1_linux.run --silent && \
+            rm cuda_10.2.1_linux.run && \
+            wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/patches/2/cuda_10.2.2_linux.run && \
+            sh cuda_10.2.2_linux.run --silent && \
+            rm cuda_10.2.2_linux.run" \
    --${toolName} version=${toolVersion} \
    --env FSLOUTPUTTYPE=NIFTI_GZ \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
    --env DEPLOY_BINS=fsleyes:fsl \
+   --env PATH='$PATH':/usr/local/cuda-10.2/bin \
+   --env LD_LIBRARY_PATH='$LD_LIBRARY_PATH':/usr/local/cuda-10.2/lib64 \
    --copy README.md /README.md \
   > ${imageName}.${neurodocker_buildExt}
+
+
+# 
+# 
+
 
    # --install ca-certificates wget python \
    # --workdir /opt \
