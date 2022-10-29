1838ba0021aad587330e5985f6de3cb57080ce29
Aswin Narayanan <aswinn89@gmail.com>
2022-10-29 21:53:12 +1000

    Update neurocontainer build process (#175)
    * Remove template from freeupspacelist
    
    * Remove depreciated set-couput commands
    
    * Update checkout and setup-python actions
    
    * Add inputs for manual workflow trigger
    
    * Read application from workflow input
    
    * Disable main steps for testing
    
    * Disable trigger on push (for testing)
    
    * Convert build job into matrix
    
    * Prep matrix
    
    * Check for changes to recipe using path filter
    
    * Limit to template recipe for testing
    
    * Remove checkout
    
    * Add push trigger
    
    * Disable inital fetch depth
    
    * Run path filter on push triggers only
    
    * Test commit for recipe change
    
    * Second test commit for yaml change
    
    * Use tj-actions changed file to find change list
    
    * Test file change
    
    * Print changed recipes
    
    * Test change
    
    * Checkout full git repo
    
    * Print only changed directories
    Test change
    
    * Use recipes relative path
    output json
    
    * Update path
    
    * Revert to check file changes
    
    * Debig github_workspace
    
    * Add debug step
    
    * Output changed dir
    
    * Add test folder under template
    
    * Generate json with changed recipes
    Filter duplicates and subdirs
    
    * Test additional apps
    
    * Add push trigger for recipe subdir changes
    
    * Use changed recipe list as matrix
    
    * Update job and step names
    
    * Test trigger
    
    * Add debug info
    
    * Convert json to string
    
    * Read output into matrix
    
    * Test output var in job2
    
    * Export output as raw compact json
    
    * Test two apps
    
    * Fix matrix application output
    
    * Fix matrix app echo
    
    * Add actual build steps
    
    * Add remainging build steps
    
    * Disabled update-builders.sh
    Added depracation notice to update-builders.sh
    
    * Delete legacy builders
    
    * Add documentation
    Disable remaining workflow for debugging
    
    * Rework build app workflow into separate workflows
    
    * FIxed requirements on build-app
    
    * Trigger build
    
    * Debug build-app
    
    * Setup needs for build-app
    
    * Test remaning build process
    
    * Test build two apps
    
    * Add manual build trigger
    
    * Reformat manual applist into json
    
    * Trim out spaces from manual app list
    
    * Remove spaces
    
    * Trigger auto-build
    
    * Add support for build-config.json
    Rename build-apps to builder
    Switch runners from ubuntu latest to 22.04
    
    * Fix builder.yml calls
    
    * Add debug info
    
    * Add more debug info
    
    * Fix an open bracket
    
    * Fix double quotes
    
    * Rename vars to apps_to_build for readability
    
    * Fix output apps_to_build name
    
    * Add auto-build check for build-config
    
    * Trigger autobuild
    
    * Generate valid json
    
    * Add check for empty matrix
    
    * Trigger autobuild
    
    * Add github quotes
    
    * Trigger autobuild
    
    * Add check for empty applist
    
    * Disable auto-build
    
    * Rename vars and jobs
    
    * Fix rename
    
    * Fix rename
    
    * Test again with auto-build enabled
    
    * Add debug info
    
    * Add more autobuild debug info
    Switch build-config.json values to boolean
    
    * Test build-config value as string
    
    * Add more debug info
    
    * Test full build for template
    
    * Fix mislocated double quotes
    
    * Add instruction for multiple app build
    In manual-build
    
    * Fix checks for build-config params
    
    * Add debug info for manual build
    
    * Fix missing var
    
    * Output json as json string
    
    * Remove fromJson for input app list
    
    * Add fromJson
    
    * Convert json to json string
    
    * Override to test json string
    
    * Fix manual build step id
    
    * Merge freeupspace and selfhosted apps into build-config.json
    
    * Free up space for micapipe and mrsiproc
    
    * Add depracation notice and placeholders for freeupspace and selfhost
    
    * Delete legacy builders added from merge
    
    * Update deprecation notices
    
    * Update deprecation notices

--- update-builders.sh
+++ update-builders.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-for RECIPES in recipes/*/; do
-    APPLICATION="$(basename $RECIPES /)"
-    if [ "$APPLICATION" != "template" ]; then
-        sed "s/template/$APPLICATION/g" <.github/workflows/template.yml >.github/workflows/$APPLICATION.yml
-        if ! grep -m 1 -q "$APPLICATION" .github/workflows/free-up-space-list.txt; then
-            sed -i '/      - .github\/workflows\/free-up-space-list.txt/d' .github/workflows/$APPLICATION.yml
-        fi
-        if grep -m 1 -q "$APPLICATION" .github/workflows/self-hosted-list.txt; then
-            sed -i 's/runs-on: ubuntu-latest/runs-on: self-hosted/g' .github/workflows/$APPLICATION.yml
-            sed -i '67,69d' .github/workflows/$APPLICATION.yml
-        fi
-    fi
-done
+# for RECIPES in recipes/*/; do
+#     APPLICATION="$(basename $RECIPES /)"
+#     if [ "$APPLICATION" != "template" ]; then
+#         sed "s/template/$APPLICATION/g" <.github/workflows/template.yml >.github/workflows/$APPLICATION.yml
+#         if ! grep -m 1 -q "$APPLICATION" .github/workflows/free-up-space-list.txt; then
+#             sed -i '/      - .github\/workflows\/free-up-space-list.txt/d' .github/workflows/$APPLICATION.yml
+#         fi
+#         if grep -m 1 -q "$APPLICATION" .github/workflows/self-hosted-list.txt; then
+#             sed -i 's/runs-on: ubuntu-latest/runs-on: self-hosted/g' .github/workflows/$APPLICATION.yml
+#             sed -i '67,69d' .github/workflows/$APPLICATION.yml
+#         fi
+#     fi
+# done
+
+echo "### Deprecation Notice ###
+
+- This is notice is to inform container building is now automated
+- update-builders.sh does not need to be run, and will be removed in a future update
+- The container building is now managed by .github/workflows/auto-build.yml & manual-build.yml
+- https://www.neurodesk.org/developers/new_tools/add_tool wil be up-to-date with the newprocess
+"
