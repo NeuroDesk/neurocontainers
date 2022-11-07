6de3feb2d964090bf2c017b360181a2fe837fac1
Steffen Bollmann <s.bollmann@uq.edu.au>
2022-11-07 04:26:05 +0000

    try setting build processes from neurodocker

--- recipes/mrtrix3/build.sh
+++ recipes/mrtrix3/build.sh
@@ -11,14 +11,12 @@ if [ "$1" != "" ]; then
     export debug=$1
 fi
 
-export NUMBER_OF_PROCESSORS=1
-
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
    --base-image vnmd/fsl_6.0.5.1:20221016 \
    --pkg-manager apt \
-   --${toolName} version=${toolVersion} method="source" \
+   --${toolName} version=${toolVersion} method="source" build_processes=1 \
    --ants version="2.3.4" \
    --install dbus-x11 less \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
