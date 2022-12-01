29c5a668410c5bf042d02a961feeba8f2e27bea5
Ashley <a.stewart.au@gmail.com>
2022-12-01 16:50:04 +1000

    QSMxT: Added QSM.jl to container

--- recipes/qsmxt/install_packages.jl
+++ recipes/qsmxt/install_packages.jl
@@ -1,4 +1,4 @@
 using Pkg
-packages = ["ArgParse", "MriResearchTools", "FFTW"]
+packages = ["ArgParse", "MriResearchTools", "FFTW", "QSM"]
 Pkg.add(packages)
 Pkg.add(url="https://github.com/korbinian90/RomeoApp.jl")
