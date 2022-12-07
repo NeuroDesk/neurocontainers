065a2c0755e39efe52d85a1f7b8466daa697dff1
Steffen Bollmann <s.bollmann@uq.edu.au>
2022-12-07 05:45:33 +0000

    updated test

--- recipes/afni/test.sh
+++ recipes/afni/test.sh
@@ -1,4 +1,7 @@
 afni_system_check.py -check_all
 
 #Test freesurfer functions
-\@SUMA_Make_Spec_FS -NIFTI -fspath /opt/freesurfer-7.3.2/subjects/bert/surf/ -sid bert
+cp /opt/freesurfer-7.3.2/subjects/bert ~/bert -r
+
+\@SUMA_Make_Spec_FS -NIFTI -fspath ~/bert/surf/ -sid bert
+
