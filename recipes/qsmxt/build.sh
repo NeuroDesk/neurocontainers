59e746b96c335663f8f60c8e535d621a9171bc67
Ashley Stewart <a.stewart.au@gmail.com>
2022-12-09 00:07:02 +0000

    QSMxT: Corrections to $PATH

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -37,7 +37,7 @@ neurodocker generate ${neurodocker_buildMode} \
       zip libgl1 libglib2.0 libglu1-mesa libsm6 libxrender1 libxt6 libxcomposite1 libfreetype6 \
       libasound2 libfontconfig1 libxkbcommon0 libxcursor1 libxi6 libxrandr2 libxtst6 qt5-default \
       libqt5svg5-dev wget libqt5opengl5-dev libqt5opengl5 libqt5gui5 libqt5core5a libsuitesparse-dev \
-   --env PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
+   --env PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
    --workdir="/opt/TGVQSM" \
    --run="wget https://repo.anaconda.com/miniconda/Miniconda2-4.6.14-Linux-x86_64.sh" \
    --run="bash Miniconda2-4.6.14-Linux-x86_64.sh -b -p miniconda2" \
@@ -48,17 +48,17 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="wget http://www.neuroimaging.at/media/qsm/TGVQSM-plus.zip" \
    --run="unzip TGVQSM-plus.zip && rm TGVQSM-plus.zip" \
    --workdir="/opt/TGVQSM/TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3" \
-   --copy setup.py /opt/TGVQSM/TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3 \
-   --env PYTHONPATH=/opt/TGVQSM/TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3/TGV_QSM \
+   --copy setup.py "/opt/TGVQSM/TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3" \
+   --env PYTHONPATH="/opt/TGVQSM/TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3/TGV_QSM" \
    --run="/opt/TGVQSM/miniconda2/bin/python setup.py install" \
    --workdir="/opt/TGVQSM/tgvqsm-1.0.0" \
    --run="cp /opt/TGVQSM/miniconda2/bin/tgv_qsm ." \
-   --env PATH='$PATH':/opt/TGVQSM/tgvqsm-1.0.0 \
+   --env PATH="${PATH}:/opt/TGVQSM/tgvqsm-1.0.0" \
    --workdir="/opt/bet2" \
    --run="git clone https://github.com/liangfu/bet2.git ." \
    --run="cmake . && make" \
    --run="ln -s /opt/bet2/bin/bet2 /bin/bet" \
-   --env SUBJECTS_DIR=/tmp \
+   --env SUBJECTS_DIR="/tmp" \
    --ants version=2.3.4 \
    --dcm2niix method=source version=003f0d19f1e57b0129c9dcf3e653f51ca3559028 \
    --miniconda version=4.7.12.1 conda_install='python=3.8' \
@@ -68,8 +68,8 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="git clone https://github.com/Deep-MI/FastSurfer.git /opt/FastSurfer" \
    --run="sed -i 's/cu113/cpu/g' /opt/FastSurfer/requirements.txt" \
    --run="pip install -r /opt/FastSurfer/requirements.txt" \
-   --env FASTSURFER_HOME=/opt/FastSurfer \
-   --env PATH='$PATH':/opt/FastSurfer \
+   --env FASTSURFER_HOME="/opt/FastSurfer" \
+   --env PATH="${PATH}:/opt/FastSurfer" \
    --copy test.sh /test.sh \
    --run="rm -rf /usr/bin/python3.8 && ln -s /opt/miniconda-latest/bin/python /usr/bin/python3.8" \
    --workdir="/opt" \
@@ -78,30 +78,30 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="python -c \"import cloudstor; cloudstor.cloudstor(url='https://cloudstor.aarnet.edu.au/plus/s/5OehmoRrTr9XlS5', password='').download('', 'nextqsm-weights.tar')\"" \
    --run="tar xf nextqsm-weights.tar -C /opt/nextqsm/checkpoints" \
    --run="rm nextqsm-weights.tar" \
-   --env PATH='$PATH':/opt/nextqsm/src_tensorflow \
+   --env PATH="${PATH}:/opt/nextqsm/src_tensorflow" \
    --workdir="/opt/bru2" \
    --run="wget https://github.com/neurolabusc/Bru2Nii/releases/download/v1.0.20180303/Bru2_Linux.zip" \
    --run="unzip Bru2_Linux.zip" \
-   --env PATH='$PATH':/opt/bru2 \
+   --env PATH="${PATH}:/opt/bru2" \
    --workdir="/opt" \
    --run="wget https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.1-linux-x86_64.tar.gz" \
    --run="tar zxvf julia-1.6.1-linux-x86_64.tar.gz" \
    --run="rm -rf julia-1.6.1-linux-x86_64.tar.gz" \
-   --env PATH='$PATH':/opt/julia-1.6.1/bin \
+   --env PATH="${PATH}:/opt/julia-1.6.1/bin" \
    --run="mkdir -p ${mountPointList}" \
    --workdir="/opt" \
    --run="git clone --depth 1 --branch v${toolVersion}  https://github.com/QSMxT/QSMxT" \
-   --copy install_packages.jl /opt \
+   --copy install_packages.jl "/opt" \
    --env JULIA_DEPOT_PATH="/opt/julia_depot" \
    --run="julia install_packages.jl" \
    --run="chmod -R 755 /opt/julia_depot/packages/RomeoApp" \
    --env JULIA_DEPOT_PATH="~/.julia:/opt/julia_depot" \
    --run="chmod +x /opt/QSMxT/*.py" \
    --run="chmod +x /opt/QSMxT/scripts/qsmxt_version.py" \
-   --env PATH='$PATH':/opt/QSMxT:/opt/QSMxT/scripts \
-   --env PYTHONPATH='$PYTHONPATH':/opt/QSMxT \
-   --env DEPLOY_PATH=/opt/ants-2.3.4/:/opt/FastSurfer:/opt/QSMxT:/opt/QSMxT/scripts \
-   --env DEPLOY_BINS=nipypecli:bet:dcm2niix:Bru2:Bru2Nii:tgv_qsm:julia:python3:python:pytest:predict_all.py:qsmxt_version.py:run_0_dicomSort.py:run_1_dicomConvert.py:run_1_niftiConvert.py:run_2_qsm.py:run_3_segment.py:run_4_template.py:run_5_analysis.py  \
+   --env PATH="${PATH}:/opt/QSMxT:/opt/QSMxT/scripts" \
+   --env PYTHONPATH="${PYTHONPATH}:/opt/QSMxT" \
+   --env DEPLOY_PATH="/opt/ants-2.3.4/:/opt/FastSurfer:/opt/QSMxT:/opt/QSMxT/scripts" \
+   --env DEPLOY_BINS="nipypecli:bet:dcm2niix:Bru2:Bru2Nii:tgv_qsm:julia:python3:python:pytest:predict_all.py:qsmxt_version.py:run_0_dicomSort.py:run_1_dicomConvert.py:run_1_niftiConvert.py:run_2_qsm.py:run_3_segment.py:run_4_template.py:run_5_analysis.py"  \
    --env LC_ALL="C.UTF-8" \
    --env LANG="C.UTF-8" \
    --copy README.md /README.md \
