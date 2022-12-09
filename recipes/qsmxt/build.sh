83f45f2e61e4aac8ac2af40b68b3de0e464cc022
Ashley Stewart <a.stewart.au@gmail.com>
2022-12-09 11:57:29 +1000

    QSMxT: Bugfix to PATH specification

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -53,7 +53,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="/opt/TGVQSM/miniconda2/bin/python setup.py install" \
    --workdir="/opt/TGVQSM/tgvqsm-1.0.0" \
    --run="cp /opt/TGVQSM/miniconda2/bin/tgv_qsm ." \
-   --env PATH="${PATH}:/opt/TGVQSM/tgvqsm-1.0.0" \
+   --env PATH="\${PATH}:/opt/TGVQSM/tgvqsm-1.0.0" \
    --workdir="/opt/bet2" \
    --run="git clone https://github.com/liangfu/bet2.git ." \
    --run="cmake . && make" \
@@ -69,7 +69,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="sed -i 's/cu113/cpu/g' /opt/FastSurfer/requirements.txt" \
    --run="pip install -r /opt/FastSurfer/requirements.txt" \
    --env FASTSURFER_HOME="/opt/FastSurfer" \
-   --env PATH="${PATH}:/opt/FastSurfer" \
+   --env PATH="\${PATH}:/opt/FastSurfer" \
    --copy test.sh /test.sh \
    --run="rm -rf /usr/bin/python3.8 && ln -s /opt/miniconda-latest/bin/python /usr/bin/python3.8" \
    --workdir="/opt" \
@@ -78,16 +78,16 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="python -c \"import cloudstor; cloudstor.cloudstor(url='https://cloudstor.aarnet.edu.au/plus/s/5OehmoRrTr9XlS5', password='').download('', 'nextqsm-weights.tar')\"" \
    --run="tar xf nextqsm-weights.tar -C /opt/nextqsm/checkpoints" \
    --run="rm nextqsm-weights.tar" \
-   --env PATH="${PATH}:/opt/nextqsm/src_tensorflow" \
+   --env PATH="\${PATH}:/opt/nextqsm/src_tensorflow" \
    --workdir="/opt/bru2" \
    --run="wget https://github.com/neurolabusc/Bru2Nii/releases/download/v1.0.20180303/Bru2_Linux.zip" \
    --run="unzip Bru2_Linux.zip" \
-   --env PATH="${PATH}:/opt/bru2" \
+   --env PATH="\${PATH}:/opt/bru2" \
    --workdir="/opt" \
    --run="wget https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.1-linux-x86_64.tar.gz" \
    --run="tar zxvf julia-1.6.1-linux-x86_64.tar.gz" \
    --run="rm -rf julia-1.6.1-linux-x86_64.tar.gz" \
-   --env PATH="${PATH}:/opt/julia-1.6.1/bin" \
+   --env PATH="\${PATH}:/opt/julia-1.6.1/bin" \
    --run="mkdir -p ${mountPointList}" \
    --workdir="/opt" \
    --run="git clone --depth 1 --branch v${toolVersion}  https://github.com/QSMxT/QSMxT" \
@@ -98,8 +98,8 @@ neurodocker generate ${neurodocker_buildMode} \
    --env JULIA_DEPOT_PATH="~/.julia:/opt/julia_depot" \
    --run="chmod +x /opt/QSMxT/*.py" \
    --run="chmod +x /opt/QSMxT/scripts/qsmxt_version.py" \
-   --env PATH="${PATH}:/opt/QSMxT:/opt/QSMxT/scripts" \
-   --env PYTHONPATH="${PYTHONPATH}:/opt/QSMxT" \
+   --env PATH="\${PATH}:/opt/QSMxT:/opt/QSMxT/scripts" \
+   --env PYTHONPATH="\${PYTHONPATH}:/opt/QSMxT" \
    --env DEPLOY_PATH="/opt/ants-2.3.4/:/opt/FastSurfer:/opt/QSMxT:/opt/QSMxT/scripts" \
    --env DEPLOY_BINS="nipypecli:bet:dcm2niix:Bru2:Bru2Nii:tgv_qsm:julia:python3:python:pytest:predict_all.py:qsmxt_version.py:run_0_dicomSort.py:run_1_dicomConvert.py:run_1_niftiConvert.py:run_2_qsm.py:run_3_segment.py:run_4_template.py:run_5_analysis.py"  \
    --env LC_ALL="C.UTF-8" \
