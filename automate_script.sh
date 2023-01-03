80a50187dd4c7b34e422a39e0736008ac17452b8
Thuy Dao <59903692+iishiishii@users.noreply.github.com>
2023-01-03 16:12:35 +1000

    Create automate script to extract terminal history Add python script to clean up command history

---
+++ automate_script.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+# Avoid duplicates
+echo 'HISTCONTROL=ignoredups:erasedups' >> ~/.bashrc 
+
+# After each command, append to the history file and reread it
+echo 'PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"' >> ~/.bashrc 
+
+source ~/.bashrc
+
+# Combine history from multiple terminals
+HISTFILE=~/.bash_history
+set -o history
+
+history | tee bash_history
+
+python3 extract.py
