73892e84dc8a68519b7e55a0914a4f917176cbc7
Ashley Stewart <a.stewart.au@gmail.com>
2023-02-04 07:38:13 +1000

    QSMxT: Fix FastSurfer to v1.0.1 due to breaking change in v2

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -23,10 +23,11 @@ source ../main_setup.sh
 # --qsmxt----------------------------------------------------------------------------------------------------
 # - 1.1.12: Combined qsmxt and qsmxtbase containers
 # - 1.1.13: https://github.com/QSMxT/QSMxT/releases/tag/v1.1.13
-# - 1.1.13 (dev update): Added RomeoApp to Julia for nextQSM testing; removed run_1_fixGEphaseFFTshift.py from DEPLOY_BINS
-# - 1.1.13 (dev update): Added NeXtQSM
+# - 1.1.13 (container update): Added RomeoApp to Julia for nextQSM testing; removed run_1_fixGEphaseFFTshift.py from DEPLOY_BINS
+# - 1.1.13 (container update): Added NeXtQSM
 # - 1.2.0: Major update; added QSM.jl; ROMEO unwrapping; Laplacian unwrapping; V-SHARP; RTS QSM; major pipeline refactor
 # - 1.3.0: Major update; added premade pipelines, interactive editor, PDF, TV, and fixed networkx version incompatibility
+# - 1.3.0 (container update): Fixed FastSurfer to v1.1.1 due to seeming slowness in v2
 
 neurodocker generate ${neurodocker_buildMode} \
    --base-image ubuntu:18.04 \
@@ -66,9 +67,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="pip install psutil datetime networkx==2.8.8 numpy h5py nibabel nilearn scikit-sparse traits nipype scipy scikit-image pydicom pytest osfclient seaborn" \
    --run="pip install cloudstor" \
    --run="pip install niflow-nipype1-workflows" \
-   --run="git clone https://github.com/Deep-MI/FastSurfer.git /opt/FastSurfer" \
-   --workdir="/opt/FastSurfer" \
-   --run="git checkout be92e4d568a7d937edeb81a6135164cd276a8885" \
+   --run="git clone --depth 1 --branch v1.1.1 https://github.com/Deep-MI/FastSurfer.git /opt/FastSurfer" \
    --run="sed -i 's/cu113/cpu/g' /opt/FastSurfer/requirements.txt" \
    --run="pip install -r /opt/FastSurfer/requirements.txt" \
    --env FASTSURFER_HOME="/opt/FastSurfer" \
