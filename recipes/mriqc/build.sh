325de8f3d3a68c4edf8088980c78ae5d07e01b17
Steffen Bollmann <s.bollmann@uq.edu.au>
2023-02-15 22:45:06 +0000

    updated mriqc docker repo

--- recipes/mriqc/build.sh
+++ recipes/mriqc/build.sh
@@ -3,7 +3,7 @@ set -e
 
 export toolName='mriqc'
 export toolVersion='22.0.6'
-# https://github.com/poldracklab/mriqc/releases
+# https://hub.docker.com/r/nipreps/mriqc/tags
 # Don't forget to update version change in README.md!!!!!
 
 
@@ -15,7 +15,7 @@ fi
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image poldracklab/${toolName}:$toolVersion \
+   --base-image nipreps/${toolName}:$toolVersion \
    --pkg-manager apt \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
