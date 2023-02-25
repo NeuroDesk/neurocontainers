0f3bb08164fc3c6d26f118db177f83647adc64db
Ashley Stewart <a.stewart.au@gmail.com>
2023-02-25 16:40:24 +1000

    QSMxT: v1.3.5 and combine steps to reduce disk space

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 export toolName='qsmxt'
-export toolVersion='1.3.4'
+export toolVersion='1.3.5'
 # Don't forget to update version change in README.md!!!!!
 
 if [ "$1" != "" ]; then
@@ -40,66 +40,68 @@ neurodocker generate ${neurodocker_buildMode} \
       libasound2 libfontconfig1 libxkbcommon0 libxcursor1 libxi6 libxrandr2 libxtst6 qt5-default \
       libqt5svg5-dev wget libqt5opengl5-dev libqt5opengl5 libqt5gui5 libqt5core5a libsuitesparse-dev \
    --env PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
+   --run="mkdir -p ${mountPointList}" \
    --workdir="/opt/TGVQSM" \
-   --run="wget https://repo.anaconda.com/miniconda/Miniconda2-4.6.14-Linux-x86_64.sh" \
-   --run="bash Miniconda2-4.6.14-Linux-x86_64.sh -b -p miniconda2" \
-   --run="miniconda2/bin/conda install -c anaconda cython==0.29.4" \
-   --run="miniconda2/bin/conda install numpy" \
-   --run="miniconda2/bin/conda install pyparsing" \
-   --run="miniconda2/bin/pip install scipy==0.17.1 nibabel==2.1.0" \
-   --run="wget http://www.neuroimaging.at/media/qsm/TGVQSM-plus.zip" \
-   --run="unzip TGVQSM-plus.zip && rm TGVQSM-plus.zip" \
-   --workdir="/opt/TGVQSM/TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3" \
+   --run="wget https://repo.anaconda.com/miniconda/Miniconda2-4.6.14-Linux-x86_64.sh \
+       && bash Miniconda2-4.6.14-Linux-x86_64.sh -b -p miniconda2 \
+       && miniconda2/bin/conda install -c anaconda cython==0.29.4 \
+       && miniconda2/bin/conda install numpy \
+       && miniconda2/bin/conda install pyparsing \
+       && miniconda2/bin/pip install scipy==0.17.1 nibabel==2.1.0 \
+       && miniconda2/bin/conda clean --all \
+       && rm Miniconda2-4.6.14-Linux-x86_64.sh" \
+   --run="wget http://www.neuroimaging.at/media/qsm/TGVQSM-plus.zip \
+       && unzip TGVQSM-plus.zip \
+       && rm TGVQSM-plus.zip" \
    --copy setup.py "/opt/TGVQSM/TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3" \
    --env PYTHONPATH="/opt/TGVQSM/TGVQSM-master-011045626121baa8bfdd6633929974c732ae35e3/TGV_QSM" \
-   --run="/opt/TGVQSM/miniconda2/bin/python setup.py install" \
-   --workdir="/opt/TGVQSM/tgvqsm-1.0.0" \
-   --run="cp /opt/TGVQSM/miniconda2/bin/tgv_qsm ." \
+   --run="/opt/TGVQSM/miniconda2/bin/python setup.py install \
+       && mkdir /opt/TGVQSM/tgvqsm-1.0.0/ \
+       && cp /opt/TGVQSM/miniconda2/bin/tgv_qsm /opt/TGVQSM/tgvqsm-1.0.0/" \
    --env PATH="\${PATH}:/opt/TGVQSM/tgvqsm-1.0.0" \
    --workdir="/opt/bet2" \
-   --run="git clone https://github.com/liangfu/bet2.git ." \
-   --run="cmake . && make" \
-   --run="ln -s /opt/bet2/bin/bet2 /bin/bet" \
+   --run="git clone https://github.com/liangfu/bet2.git . \
+       && cmake . && make \
+       && ln -s /opt/bet2/bin/bet2 /bin/bet" \
+   --workdir="/opt" \
    --env SUBJECTS_DIR="/tmp" \
    --ants version=2.3.4 \
    --dcm2niix method=source version=003f0d19f1e57b0129c9dcf3e653f51ca3559028 \
    --miniconda version=4.7.12.1 conda_install='python=3.8' \
-   --run="pip install psutil datetime networkx==2.8.8 numpy h5py nibabel nilearn scikit-sparse traits nipype scipy scikit-image pydicom pytest osfclient seaborn" \
-   --run="pip install cloudstor" \
-   --run="pip install niflow-nipype1-workflows" \
-   --run="git clone --depth 1 --branch v1.1.1 https://github.com/Deep-MI/FastSurfer.git /opt/FastSurfer" \
-   --run="sed -i 's/cu113/cpu/g' /opt/FastSurfer/requirements.txt" \
-   --run="pip install -r /opt/FastSurfer/requirements.txt" \
+   --run="rm -rf /usr/bin/python3.8 \
+       && ln -s /opt/miniconda-latest/bin/python /usr/bin/python3.8" \
+   --run="pip install psutil datetime networkx==2.8.8 numpy h5py nibabel nilearn scikit-sparse traits nipype scipy scikit-image pydicom pytest osfclient seaborn \
+       && pip install cloudstor \
+       && pip install niflow-nipype1-workflows \
+       && pip install tensorflow packaging" \
+   --run="git clone --depth 1 --branch v1.1.1 https://github.com/Deep-MI/FastSurfer.git /opt/FastSurfer \
+       && sed -i 's/cu113/cpu/g' /opt/FastSurfer/requirements.txt \
+       && pip install -r /opt/FastSurfer/requirements.txt" \
    --env FASTSURFER_HOME="/opt/FastSurfer" \
    --env PATH="\${PATH}:/opt/FastSurfer" \
    --copy test.sh /test.sh \
-   --run="rm -rf /usr/bin/python3.8 && ln -s /opt/miniconda-latest/bin/python /usr/bin/python3.8" \
-   --workdir="/opt" \
-   --run="pip install cloudstor tensorflow packaging" \
-   --run="git clone --depth 1 --branch v1.0.1 https://github.com/QSMxT/NeXtQSM /opt/nextqsm" \
-   --run="python -c \"import cloudstor; cloudstor.cloudstor(url='https://cloudstor.aarnet.edu.au/plus/s/5OehmoRrTr9XlS5', password='').download('', 'nextqsm-weights.tar')\"" \
-   --run="tar xf nextqsm-weights.tar -C /opt/nextqsm/checkpoints" \
-   --run="rm nextqsm-weights.tar" \
+   --run="git clone --depth 1 --branch v1.0.1 https://github.com/QSMxT/NeXtQSM /opt/nextqsm
+       && python -c \"import cloudstor; cloudstor.cloudstor(url='https://cloudstor.aarnet.edu.au/plus/s/5OehmoRrTr9XlS5', password='').download('', 'nextqsm-weights.tar')\" \
+       && tar xf nextqsm-weights.tar -C /opt/nextqsm/checkpoints \
+       && rm nextqsm-weights.tar" \
    --env PATH="\${PATH}:/opt/nextqsm/src_tensorflow" \
    --workdir="/opt/bru2" \
-   --run="wget https://github.com/neurolabusc/Bru2Nii/releases/download/v1.0.20180303/Bru2_Linux.zip" \
-   --run="unzip Bru2_Linux.zip" \
+   --run="wget https://github.com/neurolabusc/Bru2Nii/releases/download/v1.0.20180303/Bru2_Linux.zip \
+       && unzip Bru2_Linux.zip \
+       && rm Bru2_Linux.zip" \
    --env PATH="\${PATH}:/opt/bru2" \
    --workdir="/opt" \
-   --run="wget https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.1-linux-x86_64.tar.gz" \
-   --run="tar zxvf julia-1.6.1-linux-x86_64.tar.gz" \
-   --run="rm -rf julia-1.6.1-linux-x86_64.tar.gz" \
+   --run="wget https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.1-linux-x86_64.tar.gz \
+       && tar zxvf julia-1.6.1-linux-x86_64.tar.gz \
+       && rm -rf julia-1.6.1-linux-x86_64.tar.gz" \
    --env PATH="\${PATH}:/opt/julia-1.6.1/bin" \
-   --run="mkdir -p ${mountPointList}" \
    --workdir="/opt" \
-   --run="git clone --depth 1 --branch v${toolVersion}  https://github.com/QSMxT/QSMxT" \
    --copy install_packages.jl "/opt" \
    --env JULIA_DEPOT_PATH="/opt/julia_depot" \
-   --run="julia install_packages.jl" \
-   --run="chmod -R 755 /opt/julia_depot/packages/RomeoApp" \
+   --run="julia install_packages.jl \
+       && chmod -R 755 /opt/julia_depot/packages/RomeoApp" \
    --env JULIA_DEPOT_PATH="~/.julia:/opt/julia_depot" \
-   --run="chmod +x /opt/QSMxT/*.py" \
-   --run="chmod +x /opt/QSMxT/scripts/qsmxt_version.py" \
+   --run="git clone --depth 1 --branch v${toolVersion}  https://github.com/QSMxT/QSMxT" \
    --env PATH="\${PATH}:/opt/QSMxT:/opt/QSMxT/scripts" \
    --env PYTHONPATH="\${PYTHONPATH}:/opt/QSMxT" \
    --env DEPLOY_PATH="/opt/ants-2.3.4/:/opt/FastSurfer:/opt/QSMxT:/opt/QSMxT/scripts" \
