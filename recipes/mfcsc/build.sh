969e53dc3b6284bdeeec5e49594bac0c2224915c
Oren Civier <orenciv@gmail.com>
2023-03-05 08:07:06 +1100

    Fixed some bugs in the MCR Added not_in_mask_value argument Fixed usage message Fixed readme

--- recipes/mfcsc/build.sh
+++ recipes/mfcsc/build.sh
@@ -23,9 +23,8 @@ neurodocker generate ${neurodocker_buildMode} \
    --install curl unzip ca-certificates openjdk-8-jre dbus-x11 \
    --matlabmcr version=2020a install_path=/opt/MCR  \
    --workdir /opt/${toolName}-${toolVersion}/ \
-   --run="curl -fsSL --retry 5 https://swift.rc.nectar.org.au/v1/AUTH_dead991e1fa847e3afcca2d3a7041f5d/neurodesk/mfcsc1.0_mcr2020a.tar.gz \
-      | tar -xz -C /opt/${toolName}-${toolVersion}/" `# NOTICE: use access URL without pre-authorised token` \
-   `# --env XAPPLRESDIR=/opt/MCR/v98/x11/app-defaults` \
+   --run="curl -L -o mfcsc https://www.dropbox.com/s/qdz40cw7e9tjxc7/mfcsc?dl=0" `# download mfcsc executable from Oren's private Dropbox` \
+   --env XAPPLRESDIR=/opt/MCR/v98/x11/app-defaults \
    --run="chmod a+x /opt/${toolName}-${toolVersion}/*" `# give everybody permission to run because files are owned by rooot, and by default, only owner has execute permission` \
    --env PATH=/opt/${toolName}-${toolVersion}/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
    --env DEPLOY_BINS=mfcsc \
