f06f849e42f6ac722694db6ef4ea35849314fec6
Steffen Bollmann <stebo85@users.noreply.github.com>
2023-03-14 15:17:36 +1000

    added auto update of container so yum and apt can install things

--- interactive_builder/run_interactive_builder.sh
+++ interactive_builder/run_interactive_builder.sh
@@ -8,9 +8,17 @@ function build {
 
   read -p 'Enter tool name (all small caps): ' container_name && echo "export toolName='$container_name'" >> ./build.sh
   read -p 'Enter tool version (no underscores or dashes): ' container_version && echo "export toolVersion='$container_version'" >> ./build.sh
+  
+  echo " " >> ./build.sh
 
   echo -en "if [ \"\$1\" != \"\" ]; then\necho \"Entering Debug mode\"\nexport debug=\$1\nfi\n" >> ./build.sh
+  
+  echo " " >> ./build.sh
+  
   echo "source ../main_setup.sh" >> ./build.sh
+  echo " " >> ./build.sh
+  
+  
   echo "neurodocker generate \${neurodocker_buildMode} \\"   >> ./build.sh
   echo "--base-image $base_image \\" >> build.sh
   echo "--pkg-manager $package_manager \\" >> build.sh
@@ -45,6 +53,10 @@ read -p 'Enter base image (e.g.: centos:7, ubuntu:22.04, fedora:37): ' base_imag
 # base_image="ubuntu:22.04"
 # base_image="pytorch/pytorch:1.13.1-cuda11.6-cudnn8-runtime"
 
+
+read -p 'Enter package manager (apt/yum): ' package_manager
+# package_manager=apt
+
 echo "BootStrap: docker" > template
 echo "From: $base_image" >> template
 
@@ -62,11 +74,16 @@ echo "PS1='\u@neurodesk-builder:\w\$ '" >> template
 echo "EOF" >> template
 echo '    chmod 755 $CUSTOM_ENV' >> template
 
+if [ "$package_manager" = "apt" ]; then
+    echo 'apt update -y' >> template
+fi
 
-read -p 'Enter package manager (apt/yum): ' package_manager
-# package_manager=apt
+if [ "$package_manager" = "yum" ]; then
+    echo 'yum update -y' >> template
+fi
 
-sudo singularity build --sandbox container_${timestamp}.sif template
+
+sudo singularity build --fix-perms --sandbox container_${timestamp}.sif template
 xhost local:root #This enables root to open display to test graphical applications
 
 if [ "`lspci | grep -i nvidia`" ]
@@ -76,8 +93,10 @@ else
         gpu_option=" "
 fi
 
-
+echo "---------------------------------------------------------------"
 echo "Now get your tool to work and when done, type exit (or CTRL-D)"
+echo "---------------------------------------------------------------"
+
 
 sudo singularity --silent shell ${gpu_option} --bind ${HISTORY_FILE}:/root/.bash_history,/home/jovyan/Desktop:/root/Desktop --writable container_${timestamp}.sif
 
