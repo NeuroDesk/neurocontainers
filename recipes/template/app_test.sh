accc9d40e318dfdb2ab8c8e27c3823cbdf6014c8
Oren Civier <orenciv@gmail.com>
2023-03-31 12:30:45 +1100

    Fixed typos in app_test.sh

--- recipes/template/app_test.sh
+++ recipes/template/app_test.sh
@@ -15,7 +15,7 @@
 # Notide that any other stderr or stdout will be captured when running the script and provided to the administrator as part of the testing. That said,
 # given that many applications are tested, the administrator may not monitor the output and rely primarily on the exit code.
 #
-# The script must be a bash script, as it will be executed using 'bash -e /neurodesk/test.sh'. 
+# The script must be a bash script, as it will be executed using 'bash -e /neurodesk/app_test.sh'. 
 # The '-e' flag indicates to bash that if there is an error in the script, it should quit immediately with the non-zero exit code of the error rather than the bash default of continuing script execution after an error. This way errors in the script won't be overlooked during testing (the script continuing and returning zero exit code on termination, the administrator might not notice the error message).
 #
 # Notice that the script can also be executed by users directly by running /neurodesk/test.sh within the container. This can be used to double-check that the application works properly in the specific environment used by the user (although Singularity containers are supposed to run identically regardless of the execution environment, there are some excpetions).
