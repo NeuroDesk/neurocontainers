9addf0bb783d442a1d8731c861a15d45db0592db
Oren Civier <orenciv@gmail.com>
2023-03-31 12:30:14 +1100

    Fixed typos in app_version.sh

--- recipes/template/app_version.sh
+++ recipes/template/app_version.sh
@@ -15,10 +15,10 @@
 # Notide that any other stderr or stdout will be captured when running the script and provided to the administrator as part of the testing. That said,
 # given that many applications are tested, the administrator may not monitor the output and rely primarily on the exit code.
 #
-# The script must be a bash script, as it will be executed using 'bash -e /neurodesk/test.sh'. 
+# The script must be a bash script, as it will be executed using 'bash -e /neurodesk/app_version.sh'. 
 # The '-e' flag indicates to bash that if there is an error in the script, it should quit immediately with the non-zero exit code of the error rather than the bash default of continuing script execution after an error. This way errors in the script won't be overlooked during testing (the script continuing and returning zero exit code on termination, the administrator might not notice the error message).
 #
-# Notice that the script can also be executed by users directly by running /neurodesk/test.sh within the container. This can be used to double-check that the application works properly in the specific environment used by the user (although Singularity containers are supposed to run identically regardless of the execution environment, there are some excpetions).
+# Notice that the script can also be executed by users directly by running /neurodesk/app_version.sh within the container. This can be used to double-check that the application works properly in the specific environment used by the user (although Singularity containers are supposed to run identically regardless of the execution environment, there are some excpetions).
 #
 
 ############################################################
@@ -39,6 +39,6 @@ then
     a=1 # empty command, just so I'll have a then clause. Cannot run mfcsc without if, because then will exit without explaining what was the problem
 else
     exit_code=$?
-    echo 'version.sh: executing '"$EXEC"' returned an error. Return non-zero exit code' 1>&2
+    echo 'app_version.sh: executing '"$EXEC"' returned an error. Return non-zero exit code' 1>&2
     exit "$exit_code"
 fi
