2deda13616485ac1365f9e6f279eaf207781beff
Steffen Bollmann <s.bollmann@uq.edu.au>
2023-03-31 06:15:27 +0000

    add browser for authentication

--- recipes/globus/build.sh
+++ recipes/globus/build.sh
@@ -8,12 +8,12 @@ fi
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image fedora:37                            `# fedora ` \
+   --base-image ubuntu:22.04                            `# ubuntu ` \
    --pkg-manager yum                                    `# RECOMMENDED TO KEEP AS IS: desired package manager, has to match the base image (e.g. debian needs apt; centos needs yum)` \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll"   `# RECOMMENDED TO KEEP AS IS: define the ll command to show detailed list including hidden files`  \
    --run="chmod +x /usr/bin/ll"                         `# RECOMMENDED TO KEEP AS IS: make ll command executable`  \
    --run="mkdir -p ${mountPointList}"                   `# MANDATORY: create folders for singularity bind points` \
-   --install wget ca-certificates tk tcllib             `# RECOMMENDED: install system packages` \
+   --install wget ca-certificates tk tcllib midori           `# RECOMMENDED: install system packages` \
    --workdir /opt                                       `# install in opt` \
    --run="wget https://downloads.globus.org/globus-connect-personal/linux/stable/globusconnectpersonal-latest.tgz \
     && tar xzf globusconnectpersonal-latest.tgz \
