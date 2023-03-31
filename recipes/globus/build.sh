7a333e32f5ab77b7bec27fe1bbb2fb8cfd49db1c
Steffen Bollmann <s.bollmann@uq.edu.au>
2023-03-31 06:10:46 +0000

    build globus again

--- recipes/globus/build.sh
+++ recipes/globus/build.sh
@@ -8,9 +8,8 @@ fi
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image ubuntu:22.04                            `# ubuntu latest` \
-   --env DEBIAN_FRONTEND=noninteractive                 `# RECOMMENDED TO KEEP AS IS: this disables interactive questions during package installs` \
-   --pkg-manager apt                                    `# RECOMMENDED TO KEEP AS IS: desired package manager, has to match the base image (e.g. debian needs apt; centos needs yum)` \
+   --base-image fedora:37                            `# fedora ` \
+   --pkg-manager yum                                    `# RECOMMENDED TO KEEP AS IS: desired package manager, has to match the base image (e.g. debian needs apt; centos needs yum)` \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll"   `# RECOMMENDED TO KEEP AS IS: define the ll command to show detailed list including hidden files`  \
    --run="chmod +x /usr/bin/ll"                         `# RECOMMENDED TO KEEP AS IS: make ll command executable`  \
    --run="mkdir -p ${mountPointList}"                   `# MANDATORY: create folders for singularity bind points` \
@@ -20,6 +19,7 @@ neurodocker generate ${neurodocker_buildMode} \
     && tar xzf globusconnectpersonal-latest.tgz \
     && rm -rf globusconnectpersonal-latest.tgz"        `# download and unpack in opt` \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/ `# expose binaries` \
+   --env PATH='$PATH':/opt/${toolName}-${toolVersion}/ \
    --copy README.md /README.md                          `# MANDATORY: include readme file in container` \
   > ${imageName}.${neurodocker_buildExt}                `# THIS IS THE LAST COMMENT; NOT FOLLOWED BY BACKSLASH!`
   
