69a3f6323b917d7e9573b390efcf9d99533ed738
Steffen Bollmann <s.bollmann@uq.edu.au>
2023-03-31 05:28:00 +0000

    add globus client

---
+++ recipes/globus/build.sh
@@ -0,0 +1,30 @@
+export toolName='globus'
+export toolVersion='3.2.0' 
+
+if [ "$1" != "" ]; then
+    echo "Entering Debug mode"
+    export debug=$1
+fi
+source ../main_setup.sh
+
+neurodocker generate ${neurodocker_buildMode} \
+   --base-image neurodebian:sid-non-free                `# RECOMMENDED TO KEEP AS IS: neurodebian makes it easy to install neuroimaging software, recommended as default` \
+   --env DEBIAN_FRONTEND=noninteractive                 `# RECOMMENDED TO KEEP AS IS: this disables interactive questions during package installs` \
+   --pkg-manager apt                                    `# RECOMMENDED TO KEEP AS IS: desired package manager, has to match the base image (e.g. debian needs apt; centos needs yum)` \
+   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll"   `# RECOMMENDED TO KEEP AS IS: define the ll command to show detailed list including hidden files`  \
+   --run="chmod +x /usr/bin/ll"                         `# RECOMMENDED TO KEEP AS IS: make ll command executable`  \
+   --run="mkdir -p ${mountPointList}"                   `# MANDATORY: create folders for singularity bind points` \
+   --install wget ca-certificates tk tcllib             `# RECOMMENDED: install system packages` \
+   --workdir /opt                                       `# install in opt` \
+   --run="wget https://downloads.globus.org/globus-connect-personal/linux/stable/globusconnectpersonal-latest.tgz \
+    && tar xzf globusconnectpersonal-latest.tgz \
+    && rm -rf globusconnectpersonal-latest.tgz"        `# download and unpack in opt` \
+   --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/ `# expose binaries` \
+   --copy README.md /README.md                          `# MANDATORY: include readme file in container` \
+   --copy *.sh /neurodesk/                              `# MANDATORY: copy test scripts to /neurodesk folder - build.sh will be included as well, which is a good idea` \
+   --run="chmod +x /neurodesk/*.sh"                     `# MANDATORY: allow execution of all shell scripts in /neurodesk inside the cotainer` \
+  > ${imageName}.${neurodocker_buildExt}                `# THIS IS THE LAST COMMENT; NOT FOLLOWED BY BACKSLASH!`
+  
+if [ "$1" != "" ]; then
+   ./../main_build.sh
+fi
