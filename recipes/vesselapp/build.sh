6dbde2fb409105557424f3b3d6640ad4651d421d
Fernanda Ribeiro <33485379+felenitaribeiro@users.noreply.github.com>
2023-04-06 15:40:59 +1000

    Update build.sh
    manual labels container

--- recipes/vesselapp/build.sh
+++ recipes/vesselapp/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 export toolName='vesselapp'
-export toolVersion='0.6.0'
+export toolVersion='0.7.0'
 
 if [ "$1" != "" ]; then
     echo "Entering Debug mode"
@@ -19,7 +19,7 @@ neurodocker generate ${neurodocker_buildMode} \
 --run='git clone https://github.com/KMarshallX/vessel_code.git' \
 --workdir='/opt/vessel_code/saved_models' \
 --run='pip install osfclient matplotlib==3.6 nibabel==4.0.2 patchify==0.2.3 scikit-learn==1.1.1 antspyx==0.3.7 connected-components-3d==3.10.5' \
---run='osf -p jg7cr fetch /saved_models/Init_ep1000_lr1e3_tver_OM1' \
+--run='osf -p jg7cr fetch /saved_models/Init_ep1000_lr1e3_tver' \
 --copy README.md /README.md \
 --workdir='/opt/vessel_code/' \
 > ${toolName}_${toolVersion}.Dockerfile
