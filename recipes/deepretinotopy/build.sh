8032d5e66d0971930093ed02b108e03c62620e51
Fernanda Ribeiro <33485379+felenitaribeiro@users.noreply.github.com>
2023-04-13 14:04:54 +1000

    Update build.sh

--- recipes/deepretinotopy/build.sh
+++ recipes/deepretinotopy/build.sh
@@ -19,6 +19,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir ${mountPointList}" \
+   --install git \
    --miniconda version=py37_4.8.3 \
          conda_install='pytorch=1.6.0 torchvision=0.7.0 cudatoolkit=10.2 -c pytorch' \
          pip_install='packaging pandas seaborn nibabel torch-geometric==1.6.3 scikit-learn==0.22.2 scipy==1.1.0 matplotlib==3.2.1' \
