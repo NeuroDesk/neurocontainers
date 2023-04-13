4bf449015d726af320f0518879454f852d13cbf5
Steffen Bollmann <s.bollmann@uq.edu.au>
2023-04-13 04:24:50 +0000

    rebuild globus with libX and correct path

--- recipes/globus/build.sh
+++ recipes/globus/build.sh
@@ -13,13 +13,13 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll"   `# RECOMMENDED TO KEEP AS IS: define the ll command to show detailed list including hidden files`  \
    --run="chmod +x /usr/bin/ll"                         `# RECOMMENDED TO KEEP AS IS: make ll command executable`  \
    --run="mkdir -p ${mountPointList}"                   `# MANDATORY: create folders for singularity bind points` \
-   --install wget ca-certificates tk tcllib midori           `# RECOMMENDED: install system packages` \
+   --install wget ca-certificates tk tcllib midori  libXScrnSaver         `# RECOMMENDED: install system packages` \
    --workdir /opt                                       `# install in opt` \
    --run="wget https://downloads.globus.org/globus-connect-personal/linux/stable/globusconnectpersonal-latest.tgz \
     && tar xzf globusconnectpersonal-latest.tgz \
     && rm -rf globusconnectpersonal-latest.tgz"        `# download and unpack in opt` \
-   --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/ `# expose binaries` \
-   --env PATH='$PATH':/opt/${toolName}-${toolVersion}/ \
+   --env DEPLOY_PATH=/opt/globusconnectpersonal-${toolVersion}/ `# expose binaries` \
+   --env PATH='$PATH':/opt/globusconnectpersonal-${toolVersion}/ \
    --copy README.md /README.md                          `# MANDATORY: include readme file in container` \
   > ${imageName}.${neurodocker_buildExt}                `# THIS IS THE LAST COMMENT; NOT FOLLOWED BY BACKSLASH!`
   
