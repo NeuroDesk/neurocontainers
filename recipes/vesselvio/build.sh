080a66602838d112766cd29b81769083f6bcbd9e
Steffen Bollmann <s.bollmann@uq.edu.au>
2023-04-27 05:15:26 +0000

    build vesselvio in ubuntu2204

--- recipes/vesselvio/build.sh
+++ recipes/vesselvio/build.sh
@@ -18,21 +18,17 @@ source ../main_setup.sh
 # NOTE 2: THE BACKSLASH (\) AT THE END OF EACH LINE MUST FOLLOW THE COMMENT. A BACKSLASH BEFORE THE COMMENT WON'T WORK!
 ##########################################################################################################################################
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image centos:8              `# neurodebian makes it easy to install neuroimaging software, recommended as default` \
-   --copy fixCentos8.sh /fixCentos8.sh \
-   --run="bash /fixCentos8.sh" \
+   --base-image ubuntu:22.04              `# neurodebian makes it easy to install neuroimaging software, recommended as default` \
    --env DEBIAN_FRONTEND=noninteractive                 `# this disables interactive questions during package installs` \
-   --pkg-manager yum                                    `# desired package manager, has to match the base image (e.g. debian needs apt; centos needs yum)` \
+   --pkg-manager apt                                    `# desired package manager, has to match the base image (e.g. debian needs apt; centos needs yum)` \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll"   `# define the ll command to show detailed list including hidden files`  \
    --run="chmod +x /usr/bin/ll"                         `# make ll command executable`  \
    --run="mkdir ${mountPointList}"                      `# create folders for singularity bind points` \
-   --install ca-certificates curl libglvnd-glx qt5-qtbase-gui libXrender mesa-dri-drivers `# install packages mesa is for swrast to work; the rest for QT5 xcb` \
-   --workdir /opt/${toolName}-${toolVersion}/           `# create install directory` \
-   --run="curl -fsSL --retry 5 https://github.com/JacobBumgarner/VesselVio/archive/refs/tags/v${toolVersion}.tar.gz | tar -xz -C /opt/${toolName}-${toolVersion} --strip-components 1" \
+   --install ca-certificates curl libqt5gui5 `# install packages mesa is for swrast to work; the rest for QT5 xcb` \
    --miniconda version=latest \
       conda_install='python=3.8.8' \
-   --run="pip install -r requirements.txt" \
-   --env QT_DEBUG_PLUGINS=1 \
+   --run="git clone https://github.com/JacobBumgarner/VesselVio.git /opt/${toolName}-${toolVersion}/" \
+   --run="pip install -r /opt/${toolName}-${toolVersion}/requirements.txt" \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/    `# specify a path where ALL binary files will be exposed outside the container for the module system. Never expose a directory with system commands (like /bin/ /usr/bin ...)` \
    --copy README.md /README.md                           `# include readme file in container` \
    --env PATH='$PATH':/opt/${toolName}-${toolVersion}   `# set PATH` \
@@ -40,25 +36,8 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="chmod a+x /opt/${toolName}-${toolVersion}/vesselvio" \
   > ${imageName}.${neurodocker_buildExt}
 
-   # --run="pip install -r /opt/${toolName}-${toolVersion}/requirements.txt" \
-
-# centos7: mesa-dri-drivers libglvnd-glx libXrender fontconfig libxkbcommon-x11 gtk3 qt5-qtbase-gui python3-pyqt5-sip -> no errors, but doesn't start up
-# 	centos8: libglvnd-glx qt5-qtbase-gui libXrender mesa-dri-drivers -> no errors, but doesn't start up
-# debian:stable: libgthread-2.0.so.0 does not exist
-# ubuntu 22.04: "The plugin '/opt/miniconda-latest/lib/python3.8/site-packages/cv2/qt/plugins/platforms/libqxcb.so' uses incompatible Qt library. (5.15.0) [release]"  not a plugin
-# ubuntu:21.10: libgthread-2.0.so.0 does not exist
-# fedora35: libglvnd-glx, qt5-qtbase-gui (), libXrender
-
-# debug QT problems with 
-# export QT_DEBUG_PLUGINS=1
-# then run application and look for library not found errors
-
-# https://stackoverflow.com/questions/57362015/how-to-fix-could-not-load-the-qt-platform-plugin-xcb-in-even-though-it-was
-#There seems to be a conflict between cv2 (opencv-python) and QT5 that causes this error. I had to uninstall opencv-python and install opencv-python-headless to resolve the issue:
-
-# pip uninstall opencv-python
-# pip install opencv-python-headless
-
+   # --run="curl -fsSL --retry 5 https://github.com/JacobBumgarner/VesselVio/archive/refs/tags/v${toolVersion}.tar.gz | tar -xz -C /opt/${toolName}-${toolVersion} --strip-components 1" \
+   # --workdir /opt/${toolName}-${toolVersion}/           `# create install directory` \
 
 if [ "$1" != "" ]; then
    ./../main_build.sh
