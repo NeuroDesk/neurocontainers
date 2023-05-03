80013bc8aa118c7682c8dcf268356ec63ef3550d
Ashley <a.stewart.au@gmail.com>
2023-05-04 09:35:39 +1000

    Update build.sh
    QSMxT: Fix 'v' placement in toolVersion for consistency

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -2,11 +2,8 @@
 set -e
 
 export toolName='qsmxt'
-export toolVersion='v2.0.0'
+export toolVersion='2.0.0'
 # Don't forget to update version change in README.md!!!!!
-
-# ====== ENSURE THIS IS COMMENTED IF RELEASING A NEW VERSION ======
-#export toolVersion='master'
 
 if [ "$1" != "" ]; then
     echo "Entering Debug mode"
@@ -103,7 +100,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="julia install_packages.jl \
        && chmod -R 755 /opt/julia_depot/packages/RomeoApp" \
    --env JULIA_DEPOT_PATH="~/.julia:/opt/julia_depot" \
-   --run="git clone --depth 1 --branch ${toolVersion}  https://github.com/QSMxT/QSMxT" \
+   --run="git clone --depth 1 --branch v${toolVersion}  https://github.com/QSMxT/QSMxT" \
    --env PATH="\${PATH}:/opt/QSMxT:/opt/QSMxT/scripts" \
    --env PYTHONPATH="\${PYTHONPATH}:/opt/QSMxT" \
    --env DEPLOY_PATH="/opt/ants-2.3.4/:/opt/FastSurfer:/opt/QSMxT:/opt/QSMxT/scripts" \
