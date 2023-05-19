e029fd77368bd644b2329c23fac006ecee6bd8a4
Steffen Bollmann <stebo85@users.noreply.github.com>
2023-05-19 19:01:52 +1000

    Update build.sh

--- recipes/bidsapphcppipelines/build.sh
+++ recipes/bidsapphcppipelines/build.sh
@@ -22,7 +22,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --env PATH='$PATH':/ \
    --env DEPLOY_PATH=/usr/local/fsl/bin:/opt/workbench/bin_linux64:/usr/local/miniconda/bin:/opt/freesurfer/bin:/opt/freesurfer/fsfast/bin:/opt/freesurfer/tktools:/opt/freesurfer/mni/bin:/ \
    --copy README.md /README.md \
-   --install qt4-dev-tools libxss1 libxext6 libxft2 libjpeg62-dev libmng-dev \
+   --install qt4-dev-tools libxss1 libxext6 libxft2 libjpeg62-dev \
   > ${imageName}.${neurodocker_buildExt}
 
    # --entrypoint bash \ #needs to be moved up when debugging
