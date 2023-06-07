5884de8e5669c4d38dc255cb006437905e396a8d
Steffen Bollmann <s.bollmann@uq.edu.au>
2023-06-07 05:35:03 +0000

    switched to fedora base and set path

--- recipes/itksnap/build.sh
+++ recipes/itksnap/build.sh
@@ -12,20 +12,20 @@ fi
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image ubuntu:22.04 \
-   --pkg-manager apt \
+   --base-image fedora:38 \
+   --pkg-manager yum \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir -p ${mountPointList}" \
-   --install curl ca-certificates unzip libqt5gui5 libopengl0 \
+   --install curl ca-certificates unzip dbus-libs libX11 libglvnd-glx libglvnd-opengl libxkbcommon libglvnd-egl fontconfig \
    --run="curl -fsSL -o /example_data.zip https://www.nitrc.org/frs/download.php/750/MRI-crop.zip  \
          && unzip /example_data.zip \
          && rm /example_data.zip" \
    --workdir /opt/${toolName}-${toolVersion} \
    --run="curl -fsSL --retry 5 https://ixpeering.dl.sourceforge.net/project/itk-snap/itk-snap/4.0.1/itksnap-4.0.1-20230320-Linux-gcc64.tar.gz | tar -xz --strip-components=1 -C /opt/${toolName}-${toolVersion}" \
+   --env PATH='$PATH':/opt/${toolName}-${toolVersion}/bin/ \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
    --env DEPLOY_ENV_TEST='$CONTAINER_PATH'/opt/fsl-${toolVersion} \
-   --user=neuro \
    --copy README.md /README.md \
   > ${imageName}.${neurodocker_buildExt}
    # --entrypoint "/opt/${toolName}-${toolVersion}/bin/itksnap /MRIcrop-orig.gipl" \
