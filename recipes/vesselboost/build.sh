a76919eb5193bf1b9748fc0775dc64072e3e19a6
Steffen Bollmann <s.bollmann@uq.edu.au>
2023-06-26 22:35:54 +0000

    fix paths

--- recipes/vesselboost/build.sh
+++ recipes/vesselboost/build.sh
@@ -17,7 +17,7 @@ neurodocker generate ${neurodocker_buildMode} \
 --run='git clone https://github.com/KMarshallX/vessel_code.git' \
 --workdir='/opt/vessel_code/' \
 --run='pip install -r requirements.txt ' \
---workdir='/vessel_code/saved_models' \
+--workdir='/opt/vessel_code/saved_models' \
 --run='osf -p jg7cr fetch /saved_models/Init_ep1000_lr1e3_tver' \
 --copy README.md /README.md \
 > ${toolName}_${toolVersion}.Dockerfile 
