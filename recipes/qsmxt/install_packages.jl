3ffd7bc284c58306ced804cf7b6c3b2e2cca7f0c
Ashley Stewart <a.stewart.au@gmail.com>
2023-07-19 16:05:25 +1000

    QSMxT: Added CLEARSWI

--- recipes/qsmxt/install_packages.jl
+++ recipes/qsmxt/install_packages.jl
@@ -2,3 +2,4 @@ using Pkg
 packages = ["ArgParse", "MriResearchTools", "FFTW", "QSM"]
 Pkg.add(packages)
 Pkg.add(url="https://github.com/korbinian90/RomeoApp.jl")
+Pkg.add(url="https://github.com/korbinian90/CLEARSWI.jl")
