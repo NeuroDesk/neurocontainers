6cf85585297d8d6c7c19e912fa66453790df033d
Steffen Bollmann <s.bollmann@uq.edu.au>
2023-07-20 04:13:34 +0000

    added mitkdiffusion

---
+++ recipes/mitkdiffusion/README.md
@@ -0,0 +1,20 @@
+
+----------------------------------
+## mitkdiffusion/toolVersion ##
+The MITK Diffusion application offers a selection of image analysis algorithms for the processing of diffusion-weighted MR images. 
+
+Example:
+```
+MitkDiffusion.sh
+```
+
+More documentation can be found here: https://github.com/MIC-DKFZ/MITK-Diffusion
+
+To make the executables and scripts inside this container transparently available in the command line of environments where Neurocommand is installed: ml mitkdiffusion/toolVersion
+
+Citation:
+```
+Fritzsche, Klaus H., Peter F. Neher, Ignaz Reicht, Thomas van Bruggen, Caspar Goch, Marco Reisert, Marco Nolden, et al. “MITK Diffusion Imaging.” Methods of Information in Medicine 51, no. 5 (2012): 441.
+```
+
+----------------------------------
---
+++ recipes/mitkdiffusion/build.sh
@@ -0,0 +1,30 @@
+export toolName='mitkdiffusion'
+export toolVersion='1.2.0' 
+# http://www.mitk.org/download/diffusion/nightly/MITK-Diffusion_ubuntu-20.04_NoPython.tar.gz.html
+
+if [ "$1" != "" ]; then
+    echo "Entering Debug mode"
+    export debug=$1
+fi
+source ../main_setup.sh
+
+neurodocker generate ${neurodocker_buildMode} \
+   --base-image ubuntu:20.04 \
+   --env DEBIAN_FRONTEND=noninteractive \
+   --pkg-manager apt \
+   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+   --run="chmod +x /usr/bin/ll" \
+   --run="mkdir -p ${mountPointList}" \
+   --install libgomp1 libasound2 libnss3 libqt5gui5 libxi6 wget ca-certificates libxtst6 libxcomposite1 libxdamage1 libxcursor1 \
+   --workdir /opt \
+   --run='wget https://www.mitk.org/download/diffusion/nightly/MITK-Diffusion_ubuntu-20.04_2023.07.20_a754b053_32d7d08a_NoPython.tar.gz \
+         && tar xfz MITK-Diffusion_ubuntu-20.04_2023.07.20_a754b053_32d7d08a_NoPython.tar.gz \
+         && rm -rf MITK-Diffusion_ubuntu-20.04_2023.07.20_a754b053_32d7d08a_NoPython.tar.gz' \
+   --env PATH='$PATH':/opt/MITK-Diffusion-2018.09.99-linux-x86_64 \
+   --env DEPLOY_PATH=/opt/MITK-Diffusion-2018.09.99-linux-x86_64 \
+   --copy README.md /README.md \
+  > ${imageName}.${neurodocker_buildExt}
+  
+if [ "$1" != "" ]; then
+   ./../main_build.sh
+fi
