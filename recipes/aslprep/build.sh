8a4a30950c262158d4fb540d76b774bd162703df
Steffen Bollmann <stebo85@users.noreply.github.com>
2023-07-28 17:54:55 +1000

    Add -p

--- recipes/aslprep/build.sh
+++ recipes/aslprep/build.sh
@@ -19,7 +19,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --pkg-manager apt \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
-   --run="mkdir ${mountPointList}" \
+   --run="mkdir -p ${mountPointList}" \
    --env DEPLOY_BINS=aslprep \
    --copy README.md /README.md \
   > ${imageName}.${neurodocker_buildExt}
