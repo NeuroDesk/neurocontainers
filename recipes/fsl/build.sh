a71fd4176aff7ea943cb7483dacbb771a2c5d6a6
Steffen Bollmann <s.bollmann@uq.edu.au>
2023-08-09 02:48:31 +0000

    update fsl

--- recipes/fsl/build.sh
+++ recipes/fsl/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 export toolName='fsl'
-export toolVersion='6.0.6.4'
+export toolVersion='6.0.7.1'
 
 if [ "$1" != "" ]; then
     echo "Entering Debug mode"
@@ -11,6 +11,11 @@ fi
 
 source ../main_setup.sh
 
+echo "installing development repository of neurodocker:"
+yes | pip uninstall neurodocker
+pip install --no-cache-dir https://github.com/NeuroDesk/neurodocker/tarball/add-fsl-6.0.7.1 --upgrade
+
+
 yes | neurodocker generate ${neurodocker_buildMode} \
    --base-image ubuntu:20.04 \
    --pkg-manager apt \
@@ -18,8 +23,7 @@ yes | neurodocker generate ${neurodocker_buildMode} \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir -p ${mountPointList}" \
-   --install git ca-certificates ltrace strace wget libxml2 gcc build-essential \
-   --install nvidia-cuda-toolkit \
+   --install git ca-certificates ltrace strace wget libxml2 gcc build-essential nvidia-cuda-toolkit \
    --${toolName} version=${toolVersion} \
    --env FSLOUTPUTTYPE=NIFTI_GZ \
    --install locales \
@@ -28,7 +32,6 @@ yes | neurodocker generate ${neurodocker_buildMode} \
    --env LANGUAGE=en_US:en \
    --env LC_ALL=en_US.UTF-8 \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
-   --env DEPLOY_BINS=fsleyes:fsl \
    --env DEPLOY_ENV_FSLDIR=BASEPATH/opt/fsl-${toolVersion} \
    --run="cp /opt/fsl-${toolVersion}/bin/eddy_cuda10.2 /opt/fsl-${toolVersion}/bin/eddy_cuda" \
    --copy eddy /opt/fsl-${toolVersion}/bin/eddy \
@@ -41,4 +44,4 @@ if [ "$1" != "" ]; then
 fi
 
 #CUDA SETUP
-#FSL 6.0.6.4 only brings eddy_cuda10.2 -> so Ubuntu 20.04 could work because it brings nvidia-cuda-toolkit_10.1.243-3_amd64.deb or 22.04 with nvidia-cuda-toolkit_11.5.1-1ubuntu1_amd64.deb
+#FSL 6.0.6.4 only brings eddy_cuda10.2 -> so Ubuntu 20.04 could work because it brings nvidia-cuda-toolkit_10.1.243-3_amd64.deb or 22.04 with nvidia-cuda-toolkit_11.5.1-1ubuntu1_amd64.deb -> needed to hack eddy executable to make this work
