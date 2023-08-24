12d3e1ce15c5991401ebc7f69f8dcc1cc27e7e5f
Ashley Stewart <a.stewart.au@gmail.com>
2023-08-24 15:35:02 +1000

    QSMxT: v4.0.0 - fix Julia dependencies

--- recipes/qsmxt/install_packages.jl
+++ recipes/qsmxt/install_packages.jl
@@ -1,5 +1,6 @@
 using Pkg
-packages = ["ArgParse", "MriResearchTools", "FFTW", "QSM"]
+packages = ["ArgParse", "FFTW", "QSM"]
 Pkg.add(packages)
-Pkg.add(url="https://github.com/korbinian90/RomeoApp.jl")
-Pkg.add(url="https://github.com/korbinian90/CLEARSWI.jl")
+Pkg.add(Pkg.PackageSpec(name="MriResearchTools", version=v"2.2.0"))
+Pkg.add(Pkg.PackageSpec(url="https://github.com/korbinian90/RomeoApp.jl"))
+Pkg.add(Pkg.PackageSpec(name="CLEARSWI", version=v"1.0.0"))
