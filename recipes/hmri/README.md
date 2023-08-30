d8aebd1f604376e660585d312273ecdf96c26c45
Steffen Bollmann <s.bollmann@uq.edu.au>
2023-08-30 07:16:18 +0000

    this might work already

---
+++ recipes/hmri/README.md
@@ -0,0 +1,38 @@
+
+----------------------------------
+## cat12/0.6.0 ##
+SPM12 standalone with CAT12 toolbox in Matlab Compiler Runtime
+
+Example:
+```
+run_spm12.sh /opt/mcr/v93/
+```
+
+Example with nipype:
+```
+osf -p bt4ez fetch TOMCAT_DIB/sub-01/ses-01_7T/anat/sub-01_ses-01_7T_T1w_defaced.nii.gz /neurodesktop-storage/sub-01_ses-01_7T_T1w_defaced.nii.gz
+
+gunzip /neurodesktop-storage/sub-01_ses-01_7T_T1w_defaced.nii.gz
+
+python
+```
+
+in python run:
+```
+import nipype.interfaces.spm as spm
+
+matlab_cmd = '/opt/cat12/run_spm12.sh /opt/mcr/v93/ script'
+spm.SPMCommand.set_mlab_paths(matlab_cmd=matlab_cmd, use_mcr=True)
+
+import nipype.interfaces.cat12 as cat12
+
+cat = cat12.CAT12Segment(in_files='/neurodesktop-storage/sub-01_ses-01_7T_T1w_defaced.nii')
+cat.run() 
+
+```
+
+More documentation can be found here: http://141.35.69.218/cat12/CAT12-Manual.pdf
+
+To run container outside of this environment: ml cat12/0.6.0
+
+----------------------------------
---
+++ recipes/hmri/build.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+set -e
+
+export toolName='hmri'
+export toolVersion='0.6.0'
+
+if [ "$1" != "" ]; then
+    echo "Entering Debug mode"
+    export debug=$1
+fi
+
+source ../main_setup.sh
+
+export MATLAB_VERSION=2023a
+export MCR_VERSION=R2023a
+export MCR_UPDATE=9
+export SPM_VERSION=12
+export SPM_REVISION=7771
+
+neurodocker generate ${neurodocker_buildMode} \
+   --base-image ubuntu:22.04 \
+   --pkg-manager apt \
+   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+   --run="chmod +x /usr/bin/ll" \
+   --run="mkdir -p ${mountPointList}" \
+   --install wget unzip ca-certificates openjdk-8-jre dbus-x11 \
+   --matlabmcr version=2023a install_path=/opt/mcr  \
+   --run="wget --no-check-certificate --progress=bar:force -P /opt https://objectstorage.us-ashburn-1.oraclecloud.com/n/sd63xuke79z3/b/neurodesk/o/hMRI_toolbox-v${toolVersion}-SPM${SPM_VERSION}v${SPM_REVISION}.zip \
+      && unzip -q /opt/hMRI_toolbox-v${toolVersion}-SPM${SPM_VERSION}v${SPM_REVISION}.zip -d /opt \
+      && rm -f /opt/hMRI_toolbox-v${toolVersion}-SPM${SPM_VERSION}v${SPM_REVISION}.zip" \
+   --env SPM_VERSION=$SPM_VERSION \
+   --env SPM_REVISION=r$SPM_REVISION \
+   --env MCR_INHIBIT_CTF_LOCK=1 \
+   --env SPM_HTML_BROWSER=0 \
+   --env MATLAB_VERSION=R$MATLAB_VERSION \
+   --env MCR_VERSION=$MCR_VERSION \
+   --env MCR_UPDATE=$MCR_UPDATE \
+   --env LD_LIBRARY_PATH=/opt/mcr/${MCR_VERSION}/runtime/glnxa64:/opt/mcr/${MCR_VERSION}/bin/glnxa64:/opt/mcr/${MCR_VERSION}/sys/os/glnxa64:/opt/mcr/${MCR_VERSION}/sys/opengl/lib/glnxa64:/opt/mcr/${MCR_VERSION}/extern/bin/glnxa64 \
+   --env XAPPLRESDIR=/opt/mcr/${MCR_VERSION}/x11/app-defaults \
+   --env DEPLOY_BINS=run_spm12.sh:spm12 \
+   --run="/opt/standalone/spm${SPM_VERSION} function exit \
+      && chmod +x /opt/standalone /*" \
+   --env PATH='$PATH':/opt/standalone:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
+   --env DEPLOY_ENV_FORCE_SPMMCR="1" \
+   --copy README.md /README.md \
+  > ${toolName}_${toolVersion}.${neurodocker_buildExt}
+
+
+
+if [ "$1" != "" ]; then
+   ./../main_build.sh
+fi
