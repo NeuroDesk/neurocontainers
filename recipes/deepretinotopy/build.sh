f90fb9bd7d4b0c1900bd75901d5825314974034f
Fernanda Ribeiro <33485379+felenitaribeiro@users.noreply.github.com>
2023-09-21 14:28:18 +1000

    Update build.sh
    changes in package version

--- recipes/deepretinotopy/build.sh
+++ recipes/deepretinotopy/build.sh
@@ -19,7 +19,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --install git \
    --miniconda version=py37_4.8.3 \
          conda_install='pytorch=1.6.0 torchvision=0.7.0 cudatoolkit=10.2 -c pytorch' \
-         pip_install='packaging pandas seaborn nibabel torch-geometric==1.6.3 scikit-learn==0.22.2 scipy==1.1.0 matplotlib==3.2.1' \
+         pip_install='packaging pandas==1.1.5 seaborn==0.11.1 nibabel==3.2.1 torch-geometric==1.6.3 scikit-learn==0.22.2 scipy==1.5.4 matplotlib==3.3.4' \
    --run='pip install --no-index torch-sparse -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html' \
    --run='pip install --no-index torch-scatter -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html' \
    --run='pip install --no-index torch-cluster -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html' \
