1e73c8a403630e329f92eb1f07edb7b54bb7f118
Ashley Stewart <a.stewart.au@gmail.com>
2023-09-29 15:55:52 +1000

    QSMxT: Updated QSMTGV.jl

--- recipes/qsmxt/install_packages.jl
+++ recipes/qsmxt/install_packages.jl
@@ -3,5 +3,5 @@ packages = ["ArgParse", "FFTW", "QSM"]
 Pkg.add(packages)
 Pkg.add(Pkg.PackageSpec(name="MriResearchTools", version=v"2.2.0"))
 Pkg.add(Pkg.PackageSpec(url="https://github.com/korbinian90/RomeoApp.jl"))
-Pkg.add(Pkg.PackageSpec(url="https://github.com/korbinian90/QuantitativeSusceptibilityMappingTGV.jl", rev="3cbe735cfda787cc64610aa97056972cf2c07d1f"))
+Pkg.add(Pkg.PackageSpec(url="https://github.com/korbinian90/QuantitativeSusceptibilityMappingTGV.jl", rev="3195658975f91f73958b2d14791d995caa4b7eff"))
 Pkg.add(Pkg.PackageSpec(name="CLEARSWI", version=v"1.0.0"))
