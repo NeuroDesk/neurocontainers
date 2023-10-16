1fe270190cdbb046e60de5500fbc7fff2df4c38a
Ashley Stewart <a.stewart.au@gmail.com>
2023-10-16 16:28:00 +1000

    QSMxT: Rebuild with Julia TGV v0.2.0

--- recipes/qsmxt/install_packages.jl
+++ recipes/qsmxt/install_packages.jl
@@ -2,6 +2,6 @@ using Pkg
 packages = ["ArgParse", "FFTW", "QSM"]
 Pkg.add(packages)
 Pkg.add(Pkg.PackageSpec(name="MriResearchTools", version=v"2.2.0"))
+Pkg.add(Pkg.PackageSpec(name="QuantitativeSusceptibilityMappingTGV", version=v"0.2.1"))
 Pkg.add(Pkg.PackageSpec(url="https://github.com/korbinian90/RomeoApp.jl"))
-Pkg.add(Pkg.PackageSpec(url="https://github.com/korbinian90/QuantitativeSusceptibilityMappingTGV.jl", rev="3195658975f91f73958b2d14791d995caa4b7eff"))
 Pkg.add(Pkg.PackageSpec(name="CLEARSWI", version=v"1.0.0"))
