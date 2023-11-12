f6d768581deb4a57d12960fadba39d8d5e6924c1
Neurodesk User <user@neurodesk.github.io>
2023-11-12 18:39:35 +0000

    Updated to use libpython packages

--- recipes/hnncore/build.sh
+++ recipes/hnncore/build.sh
@@ -14,7 +14,7 @@ neurodocker generate ${neurodocker_buildMode} \
     --base-image ubuntu:22.04 \
     --pkg-manager apt \
     --env DEBIAN_FRONTEND=noninteractive \
-    --install python3.9 python3-pip python3.9-dev build-essential openmpi-bin openmpi-doc libopenmpi-dev \
+    --install python3.9 python3-pip libpython3.9 libpython3.9-dev build-essential openmpi-bin openmpi-doc libopenmpi-dev \
     --run 'pip install numpy scipy matplotlib NEURON' \
     --run 'pip install ipywidgets voila scikit-learn joblib mpi4py psutil' \
     --run 'pip install hnn_core hnn_core[opt] hnn_core[gui]' \
