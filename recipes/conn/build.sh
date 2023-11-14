1cb5c36c94832af66e9f20bdea8834d387ce2208
Steffen Bollmann <s.bollmann@uq.edu.au>
2023-11-14 23:03:42 +0000

    update conn toolbox

--- recipes/conn/build.sh
+++ recipes/conn/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 export toolName='conn'
-export toolVersion='21a'
+export toolVersion='22a'
 # https://www.nitrc.org/frs/?group_id=279
 
 if [ "$1" != "" ]; then
@@ -18,10 +18,10 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir -p ${mountPointList}" \
-   --matlabmcr version=2021a method=binaries \
+   --matlabmcr version=2022a method=binaries \
    --workdir /opt/${toolName}-${toolVersion}/ \
    --install wget \
-   --run="wget --no-check-certificate --progress=bar:force -P /opt/${toolName}-${toolVersion}/ https://www.nitrc.org/frs/download.php/12424/conn${toolVersion}_glnxa64.zip \
+   --run="wget --no-check-certificate --progress=bar:force -P /opt/${toolName}-${toolVersion}/ https://www.nitrc.org/frs/download.php/13733/conn${toolVersion}_glnxa64.zip \
       && unzip -q conn${toolVersion}_glnxa64.zip -d /opt/${toolName}-${toolVersion}/ \
       && rm -f conn${toolVersion}_glnxa64.zip" \
    --env DEPLOY_BINS=${toolName} \
