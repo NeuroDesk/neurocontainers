7f9974f1cbf2bf8c5734aa0825fb40fa115681f7
Steffen Bollmann <s.bollmann@uq.edu.au>
2024-01-08 03:18:17 +0000

    update itksnap and add wayland support

--- recipes/itksnap/build.sh
+++ recipes/itksnap/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 export toolName='itksnap'
-export toolVersion='4.0.1'
+export toolVersion='4.0.2'
 
 if [ "$1" != "" ]; then
     echo "Entering Debug mode"
@@ -17,17 +17,14 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir -p ${mountPointList}" \
-   --install curl ca-certificates unzip libqt5gui5 libopengl0 \
+   --install curl ca-certificates unzip libqt5gui5 libopengl0 qtwayland5 \
    --run="curl -fsSL -o /example_data.zip https://www.nitrc.org/frs/download.php/750/MRI-crop.zip  \
          && unzip /example_data.zip \
          && rm /example_data.zip" \
    --workdir /opt/${toolName}-${toolVersion} \
-   --run="curl -fsSL --retry 5 https://ixpeering.dl.sourceforge.net/project/itk-snap/itk-snap/4.0.1/itksnap-4.0.1-20230320-Linux-gcc64.tar.gz | tar -xz --strip-components=1 -C /opt/${toolName}-${toolVersion}" \
+   --run="curl -fsSL --retry 5 https://ixpeering.dl.sourceforge.net/project/itk-snap/itk-snap/${toolVersion}/itksnap-${toolVersion}-Linux-gcc64.tar.gz | tar -xz --strip-components=1 -C /opt/${toolName}-${toolVersion}" \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
    --env PATH='$PATH':/opt/${toolName}-${toolVersion}/bin/ \
-   --env DEPLOY_ENV_FORCE_SPMMCR="1" \
-   --env DEPLOY_ENV_FSLDIR=BASEPATH/opt/fsl-test \
-   --env DEPLOY_ENV_SPMMCRCMD="BASEPATH/opt/spm12/run_spm12.sh BASEPATH/opt/mcr/v97/ script" \
    --copy README.md /README.md \
   > ${imageName}.${neurodocker_buildExt}
    # --entrypoint "/opt/${toolName}-${toolVersion}/bin/itksnap /MRIcrop-orig.gipl" \
---
+++ recipes/itksnap/test.sh
@@ -0,0 +1,2 @@
+export QT_QPA_PLATFORM=wayland
+itksnap
