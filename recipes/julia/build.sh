2d3dd7e21ced54ac5451566738f819411b28e5d7
Korbinian Eckstein <korbinian90@gmail.com>
2024-02-16 16:07:11 +0100

    julia 1.10.1

--- recipes/julia/build.sh
+++ recipes/julia/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 export toolName='julia'
-export toolVersion='1.6.1'
+export toolVersion='1.10.1'
 # Don't forget to update version change in README.md!!!!!
 
 if [ "$1" != "" ]; then
@@ -13,20 +13,12 @@ fi
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image ubuntu:20.04 \
+   --base-image julia:${toolVersion} \
    --pkg-manager apt \
    --run="mkdir -p ${mountPointList}" \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
-   --install zlib1g-dev libzstd1 wget \
-   --workdir /opt \
-   --run="wget https://julialang-s3.julialang.org/bin/linux/x64/${toolVersion:0:3}/julia-${toolVersion}-linux-x86_64.tar.gz" \
-   --run="tar zxvf julia-${toolVersion}-linux-x86_64.tar.gz" \
-   --run="rm -rf julia-${toolVersion}-linux-x86_64.tar.gz" \
-   --env PATH='$PATH':/opt/julia-${toolVersion}/bin \
-   --run="julia -e 'using Pkg; Pkg.add(\"MriResearchTools\")'" \
    --env DEPLOY_BINS=julia \
-   --entrypoint /opt/julia-${toolVersion}/bin \
    --copy README.md /README.md \
   > ${imageName}.${neurodocker_buildExt}
 
