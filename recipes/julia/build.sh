362322ccd7c81af182cc68d4d87f6de4c903bd2b
Korbinian Eckstein <korbinian90@gmail.com>
2024-02-27 12:32:15 +0100

    base on build.sh from code

--- recipes/julia/build.sh
+++ recipes/julia/build.sh
@@ -3,24 +3,48 @@ set -e
 
 export toolName='julia'
 export toolVersion='1.10.1'
+export releaseVersion='1.10'
 # Don't forget to update version change in README.md!!!!!
 
 if [ "$1" != "" ]; then
-    echo "Entering Debug mode"
-    export debug=$1
+   echo "Entering Debug mode"
+   export debug=$1
 fi
 
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image julia:${toolVersion} \
+   --base-image debian \
    --pkg-manager apt \
-   --run="mkdir -p ${mountPointList}" \
+   --env DEBIAN_FRONTEND=noninteractive \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
-   --env DEPLOY_BINS=julia \
+   --run="mkdir -p ${mountPointList}" \
+   --install xdg-utils unzip git apt-transport-https ca-certificates coreutils \
+      curl gnome-keyring gnupg libnotify4 wget libnss3 libxkbfile1 libsecret-1-0 libgtk-3-0 libxss1 libgbm1 libxshmfence1 libasound2 \
+      lmod less nano tree \
+      gcc graphviz libzstd1 zlib1g-dev zip build-essential uuid-dev libgpgme-dev libseccomp-dev pkg-config \
+   --run="wget -O vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' \
+      && apt install ./vscode.deb  \
+      && rm -rf ./vscode.deb" \
+   --workdir /opt \
+   --run="wget https://julialang-s3.julialang.org/bin/linux/x64/${releaseVersion}/julia-${toolVersion}-linux-x86_64.tar.gz \
+      && tar zxvf julia-${toolVersion}-linux-x86_64.tar.gz \
+      && rm -rf julia-${toolVersion}-linux-x86_64.tar.gz \
+      && ln -s /opt/julia-${toolVersion} /opt/julia-latest" \
+   --run="code --extensions-dir=/opt/vscode-extensions --user-data-dir=/opt/vscode-data --install-extension julialang.language-julia \
+      && code --extensions-dir=/opt/vscode-extensions --user-data-dir=/opt/vscode-data --install-extension KorbinianEckstein.niivue \
+      && rm -rf /opt/vscode-data/CachedExtensionVSIXs/" \
+   --env PATH='$PATH':/opt/julia-${toolVersion}/bin \
    --copy README.md /README.md \
-  > ${imageName}.${neurodocker_buildExt}
+   --copy code /usr/local/sbin/ \
+   --run="chmod a+x /usr/local/sbin/code" \
+   --run="chmod a+rwx /opt/vscode-extensions/ -R" \
+   --run="chmod a+rwx /opt/vscode-data -R" \
+   --env DEPLOY_BINS=code \
+   --copy module.sh /usr/share/ \
+   --user neuro \
+   >${imageName}.${neurodocker_buildExt}
 
 if [ "$1" != "" ]; then
    ./../main_build.sh
