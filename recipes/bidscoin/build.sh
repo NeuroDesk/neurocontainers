a0666454e9db01c63f4d48ef110b7e2b84ca0de3
Marcel Zwiers <m.zwiers@donders.ru.nl>
2024-03-29 16:49:06 +0100

    Upgrade to 4.3.2

--- recipes/bidscoin/build.sh
+++ recipes/bidscoin/build.sh
@@ -3,7 +3,7 @@ set -e
 
 # this template file builds tools required for dicom conversion to bids
 export toolName='bidscoin'
-export toolVersion='4.3.1'    # Don't forget to update version change in README.md!!!!!
+export toolVersion='4.3.2'    # Don't forget to update version change in README.md!!!!!
 
 if [ "$1" != "" ]; then
     echo "Entering Debug mode"
