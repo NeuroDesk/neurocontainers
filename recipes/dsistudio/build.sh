0bfde71de0b7c811b25fd9427dbbd63458f5f63a
karl-koschutnig <52704390+karl-koschutnig@users.noreply.github.com>
2024-04-26 00:49:48 +0200

    Update build.sh (#665)
    Update dsistudio to 2024.04.

--- recipes/dsistudio/build.sh
+++ recipes/dsistudio/build.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 set -e
-
-# Check latest version here
-# https://github.com/frankyeh/DSI-Studio/releases
 export toolName='dsistudio'
-export toolVersion='2020.10'
-# Don't forget to update version change in README.md!!!!!
-
+export toolVersion='2024.04'
+ 
 if [ "$1" != "" ]; then
-    echo "Entering Debug mode"
-    export debug=$1
+echo "Entering Debug mode"
+export debug=$1
 fi
-
+ 
 source ../main_setup.sh
-
+ 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image dsistudio/dsistudio:latest \
-   --pkg-manager apt \
-   --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
-   --run="chmod +x /usr/bin/ll" \
-   --run="mkdir -p ${mountPointList}" \
-   --env DEPLOY_BINS=dsi_studio \
-   --copy README.md /README.md \
-  > ${toolName}_${toolVersion}.Dockerfile
-
-if [ "$1" != "" ]; then
-   ./../main_build.sh
+--base-image ubuntu:22.04 \
+--pkg-manager apt \
+--env DEBIAN_FRONTEND=noninteractive \
+--run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+--run="chmod +x /usr/bin/ll \
+--run="mkdir -p ${mountPointList}" \
+--install opts=--quiet  \
+--run='wget https://github.com/frankyeh/DSI-Studio/releases/download/2023.12.06/dsi_studio_ubuntu2204.zip' \
+--run='unzip dsi_studio_ubuntu2204.zip' \
+--run='rm dsi_studio_ubuntu2204.zip ' \
+--copy README.md /README.md \
+> ${toolName}_${toolVersion}.Dockerfile 
+if [ "$1" != "" ]; then 
+./../main_build.sh 
 fi
