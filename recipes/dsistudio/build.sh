395224f0632adbf873b495fb7ae5a5e28e5d44bc
Steffen Bollmann <s.bollmann@uq.edu.au>
2024-04-25 23:06:22 +0000

    update dsistudio

--- recipes/dsistudio/build.sh
+++ recipes/dsistudio/build.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 set -e
 export toolName='dsistudio'
-export toolVersion='2024.04'
- 
+export toolVersion='chen-2024-04-19'
+# Check latest version here
+# https://hub.docker.com/r/dsistudio/dsistudio/tags
+
+
 if [ "$1" != "" ]; then
-echo "Entering Debug mode"
-export debug=$1
+    echo "Entering Debug mode"
+    export debug=$1
 fi
  
 source ../main_setup.sh
  
 neurodocker generate ${neurodocker_buildMode} \
---base-image ubuntu:22.04 \
---pkg-manager apt \
---env DEBIAN_FRONTEND=noninteractive \
---run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
---run="chmod +x /usr/bin/ll \
---run="mkdir -p ${mountPointList}" \
---install opts=--quiet  \
---run='wget https://github.com/frankyeh/DSI-Studio/releases/download/2023.12.06/dsi_studio_ubuntu2204.zip' \
---run='unzip dsi_studio_ubuntu2204.zip' \
---run='rm dsi_studio_ubuntu2204.zip ' \
---copy README.md /README.md \
-> ${toolName}_${toolVersion}.Dockerfile 
+    --base-image dsistudio/dsistudio:$toolVersion \
+    --pkg-manager apt \
+    --env DEBIAN_FRONTEND=noninteractive \
+    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+    --run="chmod +x /usr/bin/ll" \
+    --run="mkdir -p ${mountPointList}" \
+    --env DEPLOY_BINS=dsi_studio \
+    --copy README.md /README.md \
+    > ${toolName}_${toolVersion}.Dockerfile 
+
+
 if [ "$1" != "" ]; then 
 ./../main_build.sh 
 fi
