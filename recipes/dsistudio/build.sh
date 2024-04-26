c5f21b33cd1df6c346b4bbbdb55a4322d74aeed2
Steffen Bollmann <s.bollmann@uq.edu.au>
2024-04-26 04:30:38 +0000

    replaces dashes with dots

--- recipes/dsistudio/build.sh
+++ recipes/dsistudio/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 export toolName='dsistudio'
-export toolVersion='chen-2024-04-19'
-# Check latest version here
+export toolVersion='chen.2024.04.19'
+# Check latest version here - replace dashes with dots
 # https://hub.docker.com/r/dsistudio/dsistudio/tags
 
 
@@ -12,9 +12,11 @@ if [ "$1" != "" ]; then
 fi
  
 source ../main_setup.sh
- 
+
+dockerHubName=`echo $toolVersion | tr '.' '-'`
+
 neurodocker generate ${neurodocker_buildMode} \
-    --base-image dsistudio/dsistudio:$toolVersion \
+    --base-image dsistudio/dsistudio:$dockerHubName \
     --pkg-manager apt \
     --env DEBIAN_FRONTEND=noninteractive \
     --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
