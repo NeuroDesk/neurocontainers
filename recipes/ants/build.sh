ed42cb84080ac03d310d10978bb7d39f23413423
Scott Love <12942961+scott-love@users.noreply.github.com>
2024-04-26 21:27:15 +0200

    Update ants build.sh
    update to version 2.5.1

--- recipes/ants/build.sh
+++ recipes/ants/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 export toolName='ants'
-export toolVersion='2.4.2' # https://github.com/ANTsX/ANTs/releases
+export toolVersion='2.5.1' # https://github.com/ANTsX/ANTs/releases
 # Don't forget to update version change in README.md!!!!!
 
 if [ "$1" != "" ]; then
