27294a13235e237795829e06a51ed9e366848e76
Steffen Bollmann <s.bollmann@uq.edu.au>
2024-05-03 06:26:25 +0000

    add upload to AWS

--- .github/workflows/build-docker.sh
+++ .github/workflows/build-docker.sh
@@ -85,6 +85,9 @@ fi
 echo "[DEBUG] Attempting upload to Nectar Object Storage ..."
 rclone copy --progress $IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg nectar:/neurodesk/temporary-builds-new
 
+echo "[DEBUG] Attempting upload to AWS Object Storage ..."
+rclone copy --progress $IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg aws-neurocontainers:/neurocontainers/temporary-builds-new
+
 if curl --output /dev/null --silent --head --fail "https://object-store.rc.nectar.org.au/v1/AUTH_dead991e1fa847e3afcca2d3a7041f5d/neurodesk/temporary-builds-new/${IMAGENAME}_${BUILDDATE}.simg"; then
     echo "[DEBUG] ${IMAGENAME}_${BUILDDATE}.simg was freshly build and exists now :)"
     echo "[DEBUG] cleaning up $IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg"
