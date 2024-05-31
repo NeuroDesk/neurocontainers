06a5d876d398605d086f23f605bb0ce9a24eec48
felenitaribeiro <felenita.ribeiro@gmail.com>
2024-05-31 16:55:55 +1000

    use deepretinotopy_1.0.2 as base image

--- recipes/deepretinotopy/build.sh
+++ recipes/deepretinotopy/build.sh
@@ -15,19 +15,12 @@ fi
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image ghcr.io/neurodesk/freesurfer_7.3.2:20230216 \
+   --base-image ghcr.io/neurodesk/deepretinotopy_1.0.2:20231223 \
    --pkg-manager yum \
    --install git \
-   --miniconda version=py37_4.8.3 \
-         conda_install='pytorch=1.6.0 torchvision=0.7.0 cudatoolkit=10.2 -c pytorch' \
-         pip_install='packaging pip==23.3.2 future==0.18.3 packaging==23.2 pyparsing==3.1.1 pandas==1.1.5 seaborn==0.11.1 nibabel==3.2.1 torch-geometric==1.6.3 scikit-learn==0.22.2 scipy==1.5.4 matplotlib==3.3.4 osfclient==0.0.5' \
-   --run='pip install --no-index torch-sparse -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html' \
-   --run='pip install --no-index torch-scatter -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html' \
-   --run='pip install --no-index torch-cluster -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html' \
-   --run='pip install --no-index torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html' \
-   --run='git clone https://github.com/felenitaribeiro/nilearn.git' \
    --run='python -c "import torch" 2>/dev/null || { echo "Failed to import module"; exit 1; }' \
    --workdir=/opt \
+   --run='rm -rf deepRetinotopy_TheToolbox' \
    --run='git clone https://github.com/felenitaribeiro/deepRetinotopy_TheToolbox.git && \
        cd deepRetinotopy_TheToolbox && \
        git checkout 7411bed5c7a25f5beb165699f38f759b3adc90b6' \
@@ -41,3 +34,13 @@ neurodocker generate ${neurodocker_buildMode} \
 if [ "$1" != "" ]; then
    ./../main_build.sh
 fi
+
+# Previously installed in the base image
+#    --miniconda version=py37_4.8.3 \
+#          conda_install='pytorch=1.6.0 torchvision=0.7.0 cudatoolkit=10.2 -c pytorch' \
+#          pip_install='packaging pip==23.3.2 future==0.18.3 packaging==23.2 pyparsing==3.1.1 pandas==1.1.5 seaborn==0.11.1 nibabel==3.2.1 torch-geometric==1.6.3 scikit-learn==0.22.2 scipy==1.5.4 matplotlib==3.3.4 osfclient==0.0.5' \
+#    --run='pip install --no-index torch-sparse -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html' \
+#    --run='pip install --no-index torch-scatter -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html' \
+#    --run='pip install --no-index torch-cluster -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html' \
+#    --run='pip install --no-index torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.6.0+cu102.html' \
+#    --run='git clone https://github.com/felenitaribeiro/nilearn.git' \
