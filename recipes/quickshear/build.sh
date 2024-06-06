d3ecbbcba76245256a6d8af54c9ad861bdbbbc5f
Steffen Bollmann <s.bollmann@uq.edu.au>
2024-06-06 06:32:07 +0000

    quickshear with synthstrip

--- recipes/quickshear/build.sh
+++ recipes/quickshear/build.sh
@@ -8,7 +8,7 @@ fi
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image ubuntu:22.04 \
+   --base-image freesurfer/synthstrip:1.6 \
    --env DEBIAN_FRONTEND=noninteractive \
    --pkg-manager apt \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
@@ -16,14 +16,11 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="mkdir -p ${mountPointList}" \
    --workdir /opt \
    --install wget git curl ca-certificates python3 python3-pip \
-   --run="pip install quickshear=1.2.0" \
-   --run="curl -o robex.tar.gz https://www.nitrc.org/frs/download.php/5994/ROBEXv12.linux64.tar.gz//?i_agree=1&download_now=1" \
+   --run="pip install quickshear==1.2.0" \
    --env PATH='$PATH':/opt/${toolName}-${toolVersion}/bin \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
    --copy README.md /README.md \
-   --copy test.sh /test.sh \
-   --run="bash /test.sh" \
-   --copy license.txt /license.txt                          `# MANDATORY: include license file in container` \
+   --entrypoint bash \
   > ${imageName}.${neurodocker_buildExt}
   
 if [ "$1" != "" ]; then
