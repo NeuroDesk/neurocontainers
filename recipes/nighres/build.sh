a49d3a900a6635180758d4a84e7e47adefb12cbe
Steffen Bollmann <s.bollmann@uq.edu.au>
2024-06-10 16:04:58 +0000

    try nighres

--- recipes/nighres/build.sh
+++ recipes/nighres/build.sh
@@ -14,7 +14,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir -p ${mountPointList}" \
-   --install git python-is-python3 python3-pip curl locales wget \
+   --install git python-is-python3 python3-pip curl locales wget build-essential \
    --run='curl https://download.java.net/java/GA/jdk20.0.1/b4887098932d415489976708ad6d1a4b/9/GPL/openjdk-20.0.1_linux-x64_bin.tar.gz | tar -zx -C /opt --transform='s/jdk-20.0.1/jdk-temurin-20.0.1/'' \
    --env JCC_JDK=/opt/jdk-temurin-20.0.1 \
    --env JAVAHOME=/opt/jdk-temurin-20.0.1 \
