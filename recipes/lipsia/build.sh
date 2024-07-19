8d4bed9627d91c34b81c064755579f5530e79ef8
Gabriele Lohmann <lohmann@tuebingen.mpg.de>
2024-07-19 15:30:56 +0200

    first build attempt

--- recipes/lipsia/build.sh
+++ recipes/lipsia/build.sh
@@ -15,14 +15,16 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir -p ${mountPointList}" \
    --workdir /opt \
-   --install wget git curl ca-certificates unzip  \
-   --env PATH='$PATH':/opt/${toolName}-${toolVersion}/bin \
-   --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
+   --install wget git curl ca-certificates unzip build-essential libgsl0-dev libboost-dev zlib1g-dev git lsb-release libopenblas-dev \
+   --run="git clone --depth 1 --branch ${toolVersion} https://github.com/lipsia-fmri/lipsia.git \ 
+    	&& cd lipsia && bash -c \"source lipsia-setup.sh && cd src && make\"" \
+   --env PATH='$PATH':/opt/${toolName}/bin \
+   --env LD_LIBRARY_PATH='$LD_LIBRARY_PATH':/opt/${toolName}/lib \
+   --env DEPLOY_PATH=/opt/${toolName}/bin/ \
    --copy README.md /README.md \
-   --copy test.sh /test.sh \
-   --run="bash /test.sh" \
   > ${imageName}.${neurodocker_buildExt}
   
+
 if [ "$1" != "" ]; then
    ./../main_build.sh
 fi
