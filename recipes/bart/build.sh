f495d8eea41d5422df2223a52155171972cd82e1
Dario Bosch <dario.bosch@tuebingen.mpg.de>
2024-07-22 17:49:30 +0200

    BART now compiled with GPU support

--- recipes/bart/build.sh
+++ recipes/bart/build.sh
@@ -13,7 +13,7 @@ fi
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image ubuntu:24.04 \
+   --base-image docker.io/nvidia/cuda:12.5.1-devel-ubuntu24.04 \
    --pkg-manager apt \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
@@ -22,7 +22,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --workdir=/opt/${toolName}-${toolVersion}/ \
    --run="curl -fsSL --retry 5 https://github.com/mrirecon/bart/archive/v${toolVersion}.tar.gz \
       | tar -xz -C /opt/${toolName}-${toolVersion}/ --strip-components 1" \
-   --run="make" \
+   --run="CUDA_BASE=/usr/local/cuda/ CUDA_LIB=lib64 CUDA=1 make" \
    --env TOOLBOX_PATH=/opt/${toolName}-${toolVersion}/ \
    --env PATH=/opt/${toolName}-${toolVersion}:${PATH} \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/ \
