20878c8850b2403ac522f99bc48f9d9a69672d8a
Ashley Stewart <a.stewart.au@gmail.com>
2024-08-06 17:27:55 +1000

    QSMxT: Add missing \

--- recipes/qsmxt/build.sh
+++ recipes/qsmxt/build.sh
@@ -86,7 +86,7 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="wget https://nodejs.org/dist/v14.17.0/node-v14.17.0-linux-x64.tar.xz \
        && tar xf node-v14.17.0-linux-x64.tar.xz \
        && rm node-v14.17.0-linux-x64.tar.xz \
-       && QSMxT-UI/setup.sh"
+       && QSMxT-UI/setup.sh" \
    --env PATH="\${PATH}:/opt/node-v14.17.0-linux-x64/bin" \
    --env DEPLOY_PATH="/opt/ants-2.4.3/:/opt/FastSurfer:/opt/QSMxT-UI" \
    --env DEPLOY_BINS="nipypecli:bet:dcm2niix:Bru2:Bru2Nii:tgv_qsm:julia:python3:python:pytest:predict_all.py:qsmxt:qsmxt-gui:dicom-sort:dicom-convert:nifti-convert"  \
