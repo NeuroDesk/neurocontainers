003792cb1cc102ad3eaf1627c413aaa9b02290e4
Joshua Scarsbrook <j.scarsbrook@uq.edu.au>
2024-08-23 19:58:21 +1000

    Fix qMRLab build.sh
    - Made `toolName` lowercase.
    - Added `DEPLOY_BINS` and the standard `ll` command and `mountPointList`.

--- recipes/qmrlab/build.sh
+++ recipes/qmrlab/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-export toolName='qMRLab'
+export toolName='qmrlab'
 export toolVersion='2.4.2'
 
 if [ "$1" != "" ]; then
@@ -14,17 +14,21 @@ source ../main_setup.sh
 # Use Singlrity/Apptainer to run TinyRange to generate the rootfs.
 singularity run docker://ghcr.io/tinyrange/tinyrange:stable build qMRLab.star:qmr_lab_root -o qmr_lab.tar
 
-# MAke sure the automaticcly included template doesn't add to the final layer.
-neurodocker generate docker \
+# Make sure the automaticcly included template doesn't add to the final layer.
+neurodocker generate ${neurodocker_buildMode} \
     --base-image ubuntu \
     --pkg-manager apt \
     --base-image scratch \
     --add qmr_lab.tar . \
     --run "/init -run-scripts /.pkg/scripts.json" \
+    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
+    --run="chmod +x /usr/bin/ll" \
+    --run="mkdir -p ${mountPointList}" \
     --run "cd /qMRLab-2.4.2;octave --exec \"qMRLabVer\"" \
     --copy qMRLab.sh /usr/bin/qMRLab \
     --entrypoint "/usr/bin/qMRLab" \
-    > ${imageName}.${neurodocker_buildExt}
+    --env DEPLOY_BINS=qMRLab \
+ > ${imageName}.${neurodocker_buildExt}
 
 if [ "$1" != "" ]; then
    ./../main_build.sh
