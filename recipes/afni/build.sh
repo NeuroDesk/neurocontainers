069637c15a46eb06ae6c509449e9baeb490f86bf
Steffen Bollmann <s.bollmann@uq.edu.au>
2024-09-25 23:52:14 +0000

    try afni rebuild

--- recipes/afni/build.sh
+++ recipes/afni/build.sh
@@ -19,6 +19,12 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir -p ${mountPointList}" \
    --env DEPLOY_PATH=/opt/afni-latest/ \
+   --run="wget --quiet https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/7.4.1/freesurfer_ubuntu18-7.4.1_amd64.deb \
+            && apt-get update -qq\
+            && apt-get install -y ./freesurfer_ubuntu18-7.4.1_amd64.deb \
+            && ln -s /usr/local/freesurfer/7.4.1-1/ /opt/freesurfer-7.4.1 \
+            && rm -rf freesurfer_ubuntu18-7.4.1_amd64.deb \
+            && rm -rf /var/lib/apt/lists/*" \
    --install software-properties-common \
    --run="sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9"\
    --run="sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'" \
@@ -30,16 +36,31 @@ neurodocker generate ${neurodocker_buildMode} \
    --copy test.tgz /opt/test.tgz \
    --copy dependencies.R /opt \
    --run="Rscript /opt/dependencies.R" \
-   --run="wget --quiet https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/7.4.1/freesurfer_ubuntu18-7.4.1_amd64.deb \
-            && apt install -y ./freesurfer_ubuntu18-7.4.1_amd64.deb \
-            && ln -s /usr/local/freesurfer/7.4.1-1/ /opt/freesurfer-7.4.1 \
-            && rm -rf freesurfer_ubuntu18-7.4.1_amd64.deb" \
    --env PATH='$PATH':/opt/freesurfer-7.4.1/tktools:/opt/freesurfer-7.4.1/bin:/opt/freesurfer-7.4.1/fsfast/bin:/opt/freesurfer-7.4.1/mni/bin \
    --env FREESURFER_HOME="/opt/freesurfer-7.4.1" \
    --env SUBJECTS_DIR="~/freesurfer-subjects-dir" \
    --copy license.txt /opt/freesurfer-7.4.1/license.txt \
    --workdir /opt \
 > ${imageName}.${neurodocker_buildExt}
+
+
+# 594.2 The following packages have unmet dependencies:
+# 594.2  freesurfer : Depends: language-pack-en but it is not installable
+# 594.2               Depends: gettext but it is not installable
+# 594.2               Depends: xterm but it is not installable
+# 594.2               Depends: csh but it is not installable
+# 594.2               Depends: xorg but it is not installable
+# 594.2               Depends: xorg-dev but it is not installable
+# 594.2               Depends: xserver-xorg-video-intel but it is not installable
+# 594.2               Depends: libxcb-icccm4 (>= 0.4.1) but it is not installable
+# 594.2               Depends: libxcb-image0 (>= 0.2.1) but it is not installable
+# 594.2               Depends: libxcb-keysyms1 (>= 0.4.0) but it is not installable
+# 594.2               Depends: libxcb-render-util0 but it is not installable
+# 594.2               Depends: libxcb-util1 (>= 0.4.0) but it is not installable
+# 594.2               Depends: libxcb-xinerama0 but it is not installable
+# 594.2               Depends: libxcb-xinput0 (>= 1.13) but it is not installable
+# 594.2               Depends: libxcb-xkb1 but it is not installable
+# 594.2               Depends: libxkbcommon-x11-0 (>= 0.5.0) but it is not installable
 
 # This hack is needed for images which set the user to non-root, because neurodocker needs startup scripts that run as root.
 sed -i '/^FROM/a USER root' ${imageName}.${neurodocker_buildExt}
