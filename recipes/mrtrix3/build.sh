0a70edcd6a1b7b882ec5eff1d1e728cc8fba4f24
Neurodesk Github Action Runner <mail.neurodesk@gmail.com>
2024-09-30 05:21:50 +0000

    add workshop data

--- recipes/mrtrix3/build.sh
+++ recipes/mrtrix3/build.sh
@@ -27,11 +27,16 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="wget https://object-store.rc.nectar.org.au/v1/AUTH_dead991e1fa847e3afcca2d3a7041f5d/build/acpcdetect_V2.1_LinuxCentOS6.7.tar.gz \
       && tar zxvf acpcdetect_V2.1_LinuxCentOS6.7.tar.gz \
       && rm -rf acpcdetect_V2.1_LinuxCentOS6.7.tar.gz" \
+   --workdir /opt/workshop-data \
+   --run "curl https://osf.io/2mzbn/download \
+         && curl https://osf.io/razk2/download \
+         && curl https://osf.io/qx29y/download \
+         && cat DICOM_dwifslpreproc-*.tar | tar -xi \
+         && rm -f DICOM_dwifslpreproc-*.tar" \
    --env ARTHOME=/opt/acpcdetect_V2.1/ \
    --env PATH='$PATH':/opt/acpcdetect_V2.1/bin \
    --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/:/opt/acpcdetect_V2.1/bin \
    --copy README.md /README.md \
-   --user=neuro \
   > ${imageName}.${neurodocker_buildExt}
 
 if [ "$1" != "" ]; then
