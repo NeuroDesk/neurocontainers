ebe8dfab92ec2273b8ba3e91136cb3b82ff09853
Neurodesk Github Action Runner <mail.neurodesk@gmail.com>
2024-10-03 04:04:21 +0000

    rebuild samsrfx

--- recipes/samsrfx/build.sh
+++ recipes/samsrfx/build.sh
@@ -12,14 +12,14 @@ fi
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image ubuntu:24.04 \
+   --base-image ubuntu:22.04 \
    --pkg-manager apt \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir -p ${mountPointList}" \
    --matlabmcr version=2023b method=binaries \
-   --workdir /opt/${toolName}-${toolVersion}/ \
    --install wget openjdk-8-jre dbus-x11 libgbm-dev \
+   --workdir /opt/${toolName}-${toolVersion}/ \
    --run="wget --no-check-certificate --progress=bar:force -P /opt/${toolName}-${toolVersion}/ https://object-store.rc.nectar.org.au/v1/AUTH_dead991e1fa847e3afcca2d3a7041f5d/build/samsrf_241003.zip \
       && unzip -q samsrf_241003.zip -d /opt/${toolName}-${toolVersion}/ \
       && chmod a+x /opt/${toolName}-${toolVersion}/samsrf/SamSrfX \
