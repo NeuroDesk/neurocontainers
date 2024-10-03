3a66bba8b1777a2eb172b4613f83cb7fa5441c08
Marcel Zwiers <m.zwiers@donders.ru.nl>
2024-10-03 12:46:34 +0200

    Upgrade to 4.4.0, i.e. switch from Qt5 to Qt6

--- recipes/bidscoin/test.sh
+++ recipes/bidscoin/test.sh
@@ -1,4 +1,4 @@
  export QT_DEBUG_PLUGINS=1
  bidscoin -t
  bidseditor myproject/bids
- # sudo docker run --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix  bidscoin_4.3.0:20240219 bidscoin -t
+ # sudo docker run --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix  bidscoin_4.4.0:20241002 bidscoin -t
