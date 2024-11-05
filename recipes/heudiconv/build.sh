e05a421faaafc3f66f4dbe6508d4338e213ad377
Neurodesk Github Action Runner <mail.neurodesk@gmail.com>
2024-11-05 06:21:22 +0000

    add heudiconv

--- recipes/heudiconv/build.sh
+++ recipes/heudiconv/build.sh
@@ -1,26 +1,24 @@
-export toolName='template'
-export toolVersion='1.0' 
+export toolName='heudiconv'
+export toolVersion='1.3.1' 
+# https://hub.docker.com/r/nipy/heudiconv/tags
 
 if [ "$1" != "" ]; then
     echo "Entering Debug mode"
     export debug=$1
 fi
+
 source ../main_setup.sh --reinstall_neurodocker=false
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image neurodebian:sid-non-free \
+   --base-image nipy/heudiconv:$toolVersion \
    --env DEBIAN_FRONTEND=noninteractive \
    --pkg-manager apt \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir -p ${mountPointList}" \
-   --workdir /opt \
-   --install wget git curl ca-certificates unzip \
-   --env PATH='$PATH':/opt/${toolName}-${toolVersion}/bin \
-   --env DEPLOY_PATH=/opt/${toolName}-${toolVersion}/bin/ \
+   --env DEPLOY_BINS=heudiconv \
    --copy README.md /README.md \
    --copy test.sh /test.sh \
-   --run="bash /test.sh" \
   > ${imageName}.${neurodocker_buildExt}
   
 if [ "$1" != "" ]; then
