c192030c0ffbe461b5299aa3dd347278097111d7
Neurodesk Github Action Runner <mail.neurodesk@gmail.com>
2024-12-04 02:37:59 +0000

    force upgrade

--- recipes/nighres/build.sh
+++ recipes/nighres/build.sh
@@ -8,12 +8,13 @@ fi
 source ../main_setup.sh
 
 neurodocker generate ${neurodocker_buildMode} \
-   --base-image ubuntu:24.04 \
+   --base-image ubuntu:22.04 \
    --env DEBIAN_FRONTEND=noninteractive \
    --pkg-manager apt \
    --run="printf '#!/bin/bash\nls -la' > /usr/bin/ll" \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir -p ${mountPointList}" \
+   --run="apt-get update && apt upgrade" \
    --install opts="--quiet" git python-is-python3 python3-pip curl locales wget build-essential \
    --run='curl https://download.java.net/java/GA/jdk20.0.1/b4887098932d415489976708ad6d1a4b/9/GPL/openjdk-20.0.1_linux-x64_bin.tar.gz | tar -zx -C /opt --transform='s/jdk-20.0.1/jdk-temurin-20.0.1/'' \
    --env JCC_JDK=/opt/jdk-temurin-20.0.1 \
