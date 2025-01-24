e8a06ff3aa111fe9cc4e2aa1acbe843ca29681a5
Joshua Scarsbrook <jscarsbrook@gmail.com>
2025-01-24 13:28:18 +1000

    added voreen 5.3.0

---
+++ recipes/voreen/build.sh
@@ -0,0 +1,31 @@
+export toolName='voreen'
+export toolVersion='5.3.0'
+
+if [ "$1" != "" ]; then
+    echo "Entering Debug mode"
+    export debug=$1
+fi
+
+source ../main_setup.sh
+
+neurodocker generate ${neurodocker_buildMode} \
+--base-image ubuntu:24.04 \
+--pkg-manager apt \
+--run="mkdir -p ${mountPointList}" \
+--env DEBIAN_FRONTEND=noninteractive \
+--install build-essential cmake libglew-dev libboost-all-dev libdevil-dev qtbase5-dev libqt5svg5-dev wget \
+--workdir='/root' \
+--run='wget https://www.uni-muenster.de/imperia/md/content/voreen/release/voreen-src-$toolVersion-unix.tar.gz && \
+    tar -xf voreen-src-$toolVersion-unix.tar.gz' \
+--run='cd /root/voreen-src-unix-nightly && \
+    mkdir build;cd build &&
+    cmake .. &&
+    make -j &&
+    make install' \
+--env DEPLOY_BINS=voreenve:voreentool \
+--copy README.md /README.md \
+> ${toolName}_${toolVersion}.Dockerfile 
+
+if [ "$1" != "" ]; then
+   ./../main_build.sh
+fi
