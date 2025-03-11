42c395b5f11b132cd5ee7bedc09502e91245db07
Neurodesk Github Action Runner <mail.neurodesk@gmail.com>
2025-03-11 23:30:03 +0000

    name objects correctly in temp folder instead of overwriting the temp folder

--- .github/workflows/build-docker.sh
+++ .github/workflows/build-docker.sh
@@ -98,7 +98,7 @@ then
     echo "[DEBUG] Installing AWS CLI"
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && sudo ./aws/install && rm -rf aws awscliv2.zip
 fi
-time aws s3 cp $IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg s3://neurocontainers/temporary-builds-new
+time aws s3 cp $IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg s3://neurocontainers/temporary-builds-new/${IMAGENAME}_${BUILDDATE}.simg
 echo "[DEBUG] Done with uploading to AWS Object Storage!"
 
 if curl --output /dev/null --silent --head --fail "https://neurocontainers.neurodesk.org/temporary-builds-new/${IMAGENAME}_${BUILDDATE}.simg"; then
