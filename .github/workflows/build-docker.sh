cff18e9b90c31fedc2ccfb0dd1180d5280fbfd56
Neurodesk Github Action Runner <mail.neurodesk@gmail.com>
2025-04-29 13:03:58 +0000

    refactor build workflows into individual steps

--- .github/workflows/build-docker.sh
+++ .github/workflows/build-docker.sh
@@ -63,43 +63,9 @@ else
   echo "[DEBUG] Skipping push to registry. Not on main branch"
 fi
 
-# Build singularity container and upload to cache to speed up testing of images:
-
-# install apptainer if no singularity executable is available
-if ! command -v singularity &>/dev/null; then
-  #This prevents the sometimes stuck apt process from stopping the build
-  if [ -f "/var/lib/apt/lists/lock" ]; then
-    sudo rm -f /var/lib/apt/lists/lock
-    sudo rm -f /var/cache/apt/archives/lock
-    sudo rm -f /var/lib/dpkg/lock*
-  fi
-
-  sudo apt-get install -y software-properties-common
-  sudo add-apt-repository -y ppa:apptainer/ppa
-  sudo apt-get update
-  sudo apt-get install -y apptainer
-fi
-
-export IMAGE_HOME="/storage/tmp"
-
-if [ -d "$IMAGE_HOME" ]; then
-  echo "[DEBUG] $IMAGE_HOME exists"
-else
-  echo "[DEBUG] $IMAGE_HOME does not exist. Creating ..."
-  sudo mkdir -p $IMAGE_HOME
-  sudo chmod a+rwx $IMAGE_HOME
-fi
-
-if [ -f "$IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg" ]; then
-  rm -rf $IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg
-fi
-echo "[DEBUG] building singularity image from docker image:"
-time singularity build "$IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg" docker-daemon://$IMAGEID:$SHORT_SHA
-echo "[DEBUG] done building singularity image from docker image!"
 
 
-echo "[DEBUG] Attempting upload to Nectar Object Storage:"
-time rclone copy $IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg nectar:/neurodesk/temporary-builds-new
-echo "[DEBUG] Done with uploading to Nectar Object Storage!"
+
+
 
 
